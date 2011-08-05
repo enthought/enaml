fb63f1e88159cbd4d159bf485faf29fa9ec6775f
David Wyde <david.wyde@gmail.com>
2011-08-05 14:12:15 -0500

    Add a setup.py script.

---
+++ setup.py
@@ -0,0 +1,21 @@
+# Copyright (c) 2011 by Enthought, Inc.
+# All rights reserved.
+
+from distutils.core import setup
+
+
+setup(name = 'traitsml',
+    version='0.1',
+    author = 'Enthought, Inc',
+    author_email = 'info@enthought.com',
+    url = 'https://github.com/enthought/traitsml',
+    description = 'language for building user interfaces with Traits',
+    long_description = open('README.rst').read(),
+    requires = ['traits', 'PySide', 'ply'],
+    packages = [
+        'traitsml',
+        'traitsml.parsing',
+        'traitsml.pyside',
+        'traitsml.view_elements',
+    ],
+)
