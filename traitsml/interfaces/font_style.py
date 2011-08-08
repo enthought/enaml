59c3c1a6bae5d0c9cfb5eb77fd4c0ffb5ed4054d
David Wyde <david.wyde@gmail.com>
2011-08-08 15:58:29 -0500

    Add a FontStyle class.

---
+++ traitsml/interfaces/font_style.py
@@ -0,0 +1,31 @@
+from traits.api import (HasTraits, Int, Float, Bool, Str, Tuple, List,
+        Enum)
+
+from ..constants import Color
+
+
+class FontStyle(HasTraits):
+    """Style text, similarly to Cascading Style Sheets."""
+    
+    # A List of font families: the first available one will be used.
+    families = List
+    
+    # The size of a font, in pixels (px).
+    size = Float
+    
+    # The foreground color of a font.
+    fg_color = Tuple(Color.DEFAULT)
+    
+    # The background color of a font.
+    bg_color = Tuple(Color.DEFAULT)
+    
+    # Specify if a font is bold.
+    bold = Bool(False)
+    
+    # Determine whether a font is italicized.
+    italic = Bool(False)
+    
+    # A line below, through, or above text.
+    text_decoration = Enum('underline', 'strike', 'overline')
+    
+    
