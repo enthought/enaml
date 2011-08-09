c2ab0daeba203d3f45dd9e280985ab70a1e58707
David Wyde <david.wyde@gmail.com>
2011-08-09 11:30:38 -0500

    Add image interface class.

---
+++ traitsml/interfaces/i_image.py
@@ -0,0 +1,21 @@
+from traits.api import 
+
+from .i_element import IElement
+
+
+class IImage(IElement):
+    """A widget for displaying images."""
+    
+    # A value that `loader` knows how to convert into image data;
+    # for example, a path name.
+    value = Any
+
+    # A Callable to convert `value` into a 3D numpy array of RGB values:
+    # unsigned 8-bit integers with dimensions of width x height x 3.
+    loader = Callable
+    
+    # The width of an image (in pixels).
+    width = Int
+    
+    # The height of an image (in pixels).
+    height = Int
