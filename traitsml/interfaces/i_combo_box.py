e6b5931a928be2e5baead3d0d87ff1a24c538d53
David Wyde <david.wyde@gmail.com>
2011-08-09 17:06:45 -0500

    Add a combo box interface.

---
+++ traitsml/interfaces/i_combo_box.py
@@ -0,0 +1,22 @@
+from traits.api import List, Any, Event, Bool
+
+from .i_element import IElement
+
+
+class IComboBox(IElement):
+    """A dropdown list, from which one item can be selected at a time."""
+
+    # The available choices for this combo box.
+    items = List
+
+    # The current selection.
+    value = Any
+
+    # Visible items: just the current selection (False), or a choice (True).
+    open = Bool
+    
+    # The event fired when a new value is selected.
+    selected = Event
+
+    # The event fired when a combo box is clicked.
+    clicked = Event
