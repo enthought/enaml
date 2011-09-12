3c892357a127ba39a135beb97a65703cb6ce2f25
Ioannis Tziakos <ioannist@enthought.com>
2011-09-12 18:29:26 +0100

    add .. todo:: directive

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -97,6 +97,8 @@ pygments_style = 'sphinx'
 # A list of ignored prefixes for module index sorting.
 #modindex_common_prefix = []
 
+todo_include_todos = True
+
 
 # -- Options for HTML output ---------------------------------------------------
 
