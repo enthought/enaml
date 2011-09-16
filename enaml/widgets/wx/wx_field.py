0c0ce7220a76f8ea43430cc9362217538dc12130
David Wyde <david.wyde@gmail.com>
2011-09-16 14:00:10 -0500

    Update WXField to use the new Control interface.

--- enaml/widgets/wx/wx_field.py
+++ enaml/widgets/wx/wx_field.py
@@ -67,11 +67,11 @@ class WXField(WXLineEdit):
         try:
             value = from_string(text)
         except Exception as e:
-            parent._exception = e
-            parent._error = True
+            parent.exception = e
+            parent.error = True
         else:
-            parent._exception = None
-            parent._error = False
+            parent.exception = None
+            parent.error = False
             parent.value = value
     
     def sync_text(self):
@@ -86,10 +86,10 @@ class WXField(WXLineEdit):
         try:
             text = to_string(value)
         except Exception as e:
-            parent._exception = e
-            parent._error = True
+            parent.exception = e
+            parent.error = True
         else:
-            parent._exception = None
-            parent._error = False
+            parent.exception = None
+            parent.error = False
             parent.text = text
 
