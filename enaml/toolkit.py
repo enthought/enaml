adfdbf1302124eefb8a80fcd619f763416dbf744
Corran Webster <cwebster@enthought.com>
2011-09-16 17:54:08 -0500

    Implement Qt CheckGroup

--- enaml/toolkit.py
+++ enaml/toolkit.py
@@ -208,6 +208,7 @@ def qt_toolkit():
         'SpinBox': ctors.QtSpinBoxCtor,
         'EnableCanvas': ctors.QtEnableCanvasCtor,
         'TableView': ctors.QtTableViewCtor,
+        'CheckGroup': ctors.QtCheckGroupCtor,
     }
     
     utils = {}
