58b294adb278581811243cf164ef767a9bf65507
itziakos <ioannist@enthought.com>
2011-09-20 20:58:02 +0100

    add spin box widget sample

---
+++ docs/source/examples/spin_box.py
@@ -0,0 +1,27 @@
+#------------------------------------------------------------------------------
+#  Copyright (c) 2011, Enthought, Inc.
+#  All rights reserved.
+#------------------------------------------------------------------------------
+from cStringIO import StringIO
+
+from enaml.factory import EnamlFactory
+
+enml = """
+from enaml.enums import TickPosition
+
+Window:
+    title = "VGroup example"
+    Panel:
+        VGroup:
+            SpinBox:
+                special_value_text = "Auto"
+                step = 2
+                low = -20
+                high = 20
+"""
+
+fact = EnamlFactory(StringIO(enml))
+
+view = fact()
+
+view.show()
