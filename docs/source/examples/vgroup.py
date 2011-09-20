22ddb57573d5b5f907afe43fc5fc37b750aec120
itziakos <ioannist@enthought.com>
2011-09-20 20:58:34 +0100

    add VGroup widget sample

---
+++ docs/source/examples/vgroup.py
@@ -0,0 +1,34 @@
+#------------------------------------------------------------------------------
+#  Copyright (c) 2011, Enthought, Inc.
+#  All rights reserved.
+#------------------------------------------------------------------------------
+from cStringIO import StringIO
+
+from enaml.factory import EnamlFactory
+
+enml = """
+from enaml.enums import TickPosition
+
+Window:
+    title = "VGroup example"
+    Panel:
+        VGroup:
+            PushButton:
+                style.cls = "stretch"
+                text = 'b1'
+            PushButton:
+                style.cls = "stretch"
+                text = 'b2'
+            PushButton:
+                style.cls = "stretch"
+                text = 'b3'
+            PushButton:
+                style.cls = "stretch"
+                text = 'b4'
+"""
+
+fact = EnamlFactory(StringIO(enml))
+
+view = fact()
+
+view.show()
