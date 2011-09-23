9f49fa8cacfb8dca46b4c04b6bdb24922c3bddcc
itziakos <ioannist@enthought.com>
2011-09-23 16:53:41 +0100

    update label test to use the new EnamlTestCase class

--- enaml/widgets/wx/tests/test_wx_label.py
+++ enaml/widgets/wx/tests/test_wx_label.py
@@ -3,65 +3,52 @@
 #  All rights reserved.
 #------------------------------------------------------------------------------
 # -*- coding: utf-8 -*-
+
 import unittest
 import sys
-import weakref
+from enaml.widgets.wx.tests.utility import EnamlTestCase
 
-import wx
-
-from enaml.widgets.wx.wx_label import WXLabel
-
-
-class WidgetParent(object):
-    """Mock parent clas"""
-
-    def __init__(self):
-
-        self.app = wx.App(0)
-        self.widget = wx.Frame(None)
-
-    def __call__(self):
-        return weakref.ref(self)
-
-
-class testWXLabel(unittest.TestCase):
-    """Testsuite for WXLabel
-
-    The widget is tested in isolation without the traitaml boilerplate
+class testWXLabel(EnamlTestCase):
+    """ Testsuite for WXLabel
     """
 
-    def setUp(self):
-        # setup an empty application
-        parent = WidgetParent()
-
-        # setup widgets
-        self.label = WXLabel()
-        self.label.parent_ref = parent()
-        self.label.create_widget()
-        self.label.init_attributes()
-
-        self.label.text = 'test label'
+    enaml = \
+"""
+Window:
+    Panel:
+        VGroup:
+            Label:
+                name = 'tested_widget'
+                text = 'test label'
+"""
 
     def test_initial_contents(self):
         """Check the text value of the WXLabel"""
 
-        self.assertEqual('test label', self.label.widget.GetLabel(),
+        enaml_widget = self.get_widget('tested_widget')
+        widget = enaml_widget.toolkit_widget()
+
+        self.assertEqual('test label', widget.GetLabel(),
             "The widget's label should be {0}".format('test label'))
-        self.assertEqual(self.label.text, self.label.widget.GetLabel(),
+        self.assertEqual(enaml_widget.text, widget.GetLabel(),
             "The text attribute does not agree with the widget's label")
 
     @unittest.skipIf(sys.platform.startswith("win"), "Coredump in Windows")
     def test_label_change(self):
         """Test changing the label of a WXLabel"""
 
-        new_label = 'new_label'
-        self.label.text = new_label
+        enaml_widget = self.get_widget('tested_widget')
+        widget = enaml_widget.toolkit_widget()
 
-        self.assertEqual(new_label, self.label.widget.GetLabel(),
+        new_label = 'new_label'
+        enaml_widget.text = new_label
+
+        self.assertEqual(new_label, widget.GetLabel(),
             "The widget's label should be {0}".format(new_label))
-        self.assertEqual(self.label.text, self.label.widget.GetLabel(),
+        self.assertEqual(enaml_widget.text, widget.GetLabel(),
             "The text attribute does not agree with the widget's label")
 
 
 if __name__ == '__main__':
+    import unittest
     unittest.main()
