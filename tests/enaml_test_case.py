ff8a0d9bb29306fa2a0bf3cbc66bac83a57019da
David Wyde <david.wyde@gmail.com>
2011-09-24 16:27:05 -0500

    Add a new test base class, finding widgets by `id` instead of `name`.

---
+++ tests/enaml_test_case.py
@@ -0,0 +1,33 @@
+import unittest
+from cStringIO import StringIO
+
+from enaml.factory import EnamlFactory
+
+
+class EnamlTestCase(unittest.TestCase):
+    
+    def setUp(self):
+        """ Parse Enaml source, stored in `self.enaml`, and store the view. """
+        
+        events = []
+        fact = EnamlFactory(StringIO(self.enaml))
+        view = fact(events=events)
+        
+        # Lay widgets out, but don't display them to screen.
+        view._apply_style_sheet()
+        view.window.layout()
+        
+        self.namespace = view.ns
+        self.view = view
+        self.events = events
+    
+    def widget_by_id(self, widget_id):
+        """ Find an item in the view's namespace with a given id.
+        
+        Raises
+        ------
+        AttributeError if no such name exists.
+
+        """
+        return getattr(self.namespace, widget_id)
+        
