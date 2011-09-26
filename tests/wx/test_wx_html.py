4aa0075c08ca1151771231b8f7f8f1ee5ecaf3b4
David Wyde <dwyde@enthought.com>
2011-09-26 10:38:53 -0500

    Add Html tests.

---
+++ tests/wx/test_wx_html.py
@@ -0,0 +1,19 @@
+#------------------------------------------------------------------------------
+#  Copyright (c) 2011, Enthought, Inc.
+#  All rights reserved.
+#------------------------------------------------------------------------------
+from ..common import html
+from enaml.toolkit import wx_toolkit
+
+
+class TestWxHtml(html.TestHtml):
+    """ WXHtml tests. """
+
+    toolkit = wx_toolkit()
+
+    def get_source(self, widget):
+        """ Get the source of an Html widget.
+        
+        """
+        return widget.ToText()
+        
