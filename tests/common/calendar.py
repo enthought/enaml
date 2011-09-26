17871474c34c26cd0da2ccd735c599fcc53c9e7b
David Wyde <dwyde@enthought.com>
2011-09-26 15:19:13 -0500

    Add calendar tests. Neither backend constrains min and max dates properly.

---
+++ tests/common/calendar.py
@@ -0,0 +1,94 @@
+#------------------------------------------------------------------------------
+#  Copyright (c) 2011, Enthought, Inc.
+#  All rights reserved.
+#------------------------------------------------------------------------------
+from datetime import date
+
+from .enaml_test_case import EnamlTestCase
+
+
+class TestCalendar(EnamlTestCase):
+    """ Logic for testing calendars. """
+
+    enaml = """
+import datetime
+Window:
+    Panel:
+        VGroup:
+            Calendar cal:
+                date = datetime.date(1970, 2, 1)
+                minimum_date = datetime.date(1970, 1, 1)
+                maximum_date = datetime.date(1970, 2, 15)
+                selected >> events.append('selected')
+                activated >> events.append('activated')
+"""
+
+    def setUp(self):
+        """ Set up calendar tests.
+
+        """
+        super(TestCalendar, self).setUp()
+        component = self.widget_by_id('cal')
+        self.widget = component.toolkit_widget()
+        self.component = component
+
+    def test_initial_value(self):
+        """ Test the initial attributes of the calendar.
+
+        """
+        widget = self.widget
+        component = self.component
+
+        self.assertEqual(self.get_date(widget), component.date)
+        self.assertEqual(self.get_minimum_date(widget), component.minimum_date)
+        self.assertEqual(self.get_maximum_date(widget), component.maximum_date)
+
+    def test_selected_fired(self):
+        """ Test that the 'selected' event fires properly.
+        
+        """
+        self.select_date(self.widget, date(1970, 2, 1))
+        self.assertEqual(self.events, ['selected'])
+    
+    def test_activated_fired(self):
+        """ Test that the 'activated' event fires properly.
+        
+        """
+        self.activate_date(self.widget, date(1970, 2, 1))
+        self.assertEqual(self.events, ['activated'])
+
+    def test_initial_too_early(self):
+        """ Check that an invalid date is corrected (value below the minimum).
+        
+        """
+        self.enaml = """
+import datetime
+Window:
+    Panel:
+        VGroup:
+            Calendar cal:
+                date = datetime.date(1980, 1, 1)
+                minimum_date = datetime.date(1990, 1, 1)
+                maximum_date = datetime.date(2000, 1, 1)
+"""
+        self.setUp()
+        component = self.component
+        self.assertEqual(component.date, component.minimum_date)
+
+    def test_initial_too_late(self):
+        """ Check that an invalid date is corrected (value above the maximum).
+        
+        """
+        self.enaml = """
+import datetime
+Window:
+    Panel:
+        VGroup:
+            Calendar cal:
+                date = datetime.date(2010, 1, 1)
+                minimum_date = datetime.date(1990, 1, 1)
+                maximum_date = datetime.date(2000, 1, 1)
+"""
+        self.setUp()
+        component = self.component
+        self.assertEqual(component.date, component.maximum_date)
---
+++ tests/wx/test_wx_calendar.py
@@ -0,0 +1,50 @@
+#------------------------------------------------------------------------------
+#  Copyright (c) 2011, Enthought, Inc.
+#  All rights reserved.
+#------------------------------------------------------------------------------
+import wx.calendar
+
+from ..common import calendar
+from enaml.toolkit import wx_toolkit
+
+
+class TestWxCalendar(calendar.TestCalendar):
+    """ WXCalendar tests. """
+
+    toolkit = wx_toolkit()
+
+    def get_date(self, widget):
+        """ Get a calendar's active date.
+
+        """
+        return widget.PyGetDate()
+
+    def get_minimum_date(self, widget):
+        """ Get a calendar's minimum date attribute.
+
+        """
+        return widget.PyGetLowerDateLimit()
+
+    def get_maximum_date(self, widget):
+        """ Get a calendar's maximum date attribute.
+        
+        """
+        return widget.PyGetUpperDateLimit()
+
+    def activate_date(self, widget, date):
+        """ Fire an event to indicate that a date was activated.
+        
+        """
+        cal_event = wx.calendar.EVT_CALENDAR
+        event = wx.calendar.CalendarEvent(widget, cal_event.typeId)
+        event.PySetDate(date)
+        widget.GetEventHandler().ProcessEvent(event)
+        
+    def select_date(self, widget, date):
+        """ Fire an event to indicate that a date was selected.
+        
+        """
+        cal_select_event = wx.calendar.EVT_CALENDAR_SEL_CHANGED
+        event = wx.calendar.CalendarEvent(widget, cal_select_event.typeId)
+        event.PySetDate(date)
+        widget.GetEventHandler().ProcessEvent(event)
