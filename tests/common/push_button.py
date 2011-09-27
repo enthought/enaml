daa908142f35ed1f88230cee93e9497855bc4dcd
itziakos <ioannist@enthought.com>
2011-09-27 22:22:00 +0100

    Update push button test
    - split release test into to independent cases
    - add checks for the `down` attribute

--- tests/common/push_button.py
+++ tests/common/push_button.py
@@ -53,6 +53,7 @@ Window:
         self.button_pressed()
 
         events = self.events
+        self.assertTrue(self.component.down)
         self.assertIn('pressed', events)
         self.assertNotIn('released', events)
         self.assertNotIn('clicked', events)
@@ -77,9 +78,17 @@ Window:
         self.button_released()
         self.assertEqual(events, [])
 
-        # Check when normal sequence of events takes place
+    def test_press_release_sequence(self):
+        """ Verify the even firing when the press-release (nornal) sequence
+        is applied.
+
+        """
+        events = self.events
+
         self.button_pressed()
+        self.assertTrue(self.component.down)
         self.button_released()
+        self.assertFalse(self.component.down)
         self.assertEqual(events.count('pressed'), 1)
         self.assertEqual(events.count('released'), 1)
         self.assertNotIn('clicked', events)
