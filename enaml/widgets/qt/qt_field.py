73d0ba46df05b1d3fcf77ba326663b59eb311a52
David Wyde <dwyde@enthought.com>
2011-09-27 15:25:14 -0500

    Enforce maximum length on QtField.

--- enaml/widgets/qt/qt_field.py
+++ enaml/widgets/qt/qt_field.py
@@ -43,6 +43,10 @@ class QtField(QtControl):
             self.update_text()
         parent._modified = False
         self.set_cursor_position(parent.cursor_position)
+        
+        max_length = parent.max_length
+        if max_length:
+            self.set_max_length(max_length)
         self.bind()
 
     def parent_max_length_changed(self, max_length):
