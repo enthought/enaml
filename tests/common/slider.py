245ce444a18f3cef7c2e78867ff5d686bf67687a
itziakos <ioannist@enthought.com>
2011-09-30 18:46:05 +0100

    add event and action testing

--- tests/common/slider.py
+++ tests/common/slider.py
@@ -6,6 +6,35 @@ import abc
 
 from .enaml_test_case import EnamlTestCase
 from enaml.enums import TickPosition, Orientation
+from enaml.util.enum import Enum
+
+class TestEvents(Enum):
+    """ Events required by the testcase. """
+
+    #: The left button is pressed
+    PRESSED = 0x0
+
+    #: The left button is released
+    RELEASED = 0x1
+
+    #: The thumb is moved one step up
+    STEP_UP = 0x2
+
+    #: The thumb is moved one step down
+    STEP_DOWN = 0x3
+
+    #: The thumb is moved one page up
+    PAGE_UP = 0x4
+
+    #: The thumb is moved one page down
+    PAGE_DOWN = 0x5
+
+    #: The thumb is moved to home
+    HOME = 0x6
+
+    #: The thumb is moved to the end
+    END = 0x7
+
 
 class TestSlider(EnamlTestCase):
     """ Logic for testing spin boxes.
@@ -27,6 +56,7 @@ Window:
             Slider slider:
                 tick_interval = 0.1
                 value = 0.5
+                page_step = 2
                 moved >> events.append('moved')
                 pressed >> events.append('pressed')
                 released >> events.append('released')
@@ -57,7 +87,7 @@ Window:
         self.assertEnamlInSync(component, 'tick_position', TickPosition.BOTTOM)
         self.assertEnamlInSync(component, 'orientation', Orientation.HORIZONTAL)
         self.assertEnamlInSync(component, 'single_step', 1)
-        self.assertEnamlInSync(component, 'page_step', 5)
+        self.assertEnamlInSync(component, 'page_step', 2)
 
 
     def test_tracking_attribute(self):
@@ -200,6 +230,110 @@ Window:
         self.component.orientation = Orientation.VERTICAL
         self.assertEnamlInSync(component, 'tick_position', TickPosition.RIGHT)
 
+
+    def testPressingTheThumb(self):
+        """ Test firing events when the thumb is pressed down.
+
+        """
+        component = self.component
+        events = self.events
+
+        self.sent_event(self.widget, TestEvents.PRESSED)
+        self.assertEqual(['pressed'], events)
+
+    def testReleasingTheThumb(self):
+        """ Test firing events when the thumb is released.
+
+        """
+        self.clean_event_queue()
+
+        component = self.component
+        events = self.events
+
+        self.sent_event(self.widget, TestEvents.RELEASED)
+        self.assertEqual([], events)
+
+        self.sent_event(self.widget, TestEvents.PRESSED)
+        self.sent_event(self.widget, TestEvents.RELEASED)
+        self.assertEqual(['pressed', 'released'], events)
+
+    def testMovingTheThumbProgrammaticaly(self):
+        """ Test firing events when the thumb is moved (programmatically).
+
+        """
+        component = self.component
+        events = self.events
+
+        component.value = 0.3
+        self.assertEqual(['moved'], events)
+
+    def testMovingToHome(self):
+        """ Test firing events and value when the thumb is moved to home.
+
+        """
+        component = self.component
+        events = self.events
+
+        self.sent_event(self.widget, TestEvents.HOME)
+        self.assertEnamlInSync(component, 'value', 0.0)
+        self.assertEqual(['moved'], events)
+
+    def testMovingToEnd(self):
+        """ Test firing events and value when the thumb is moved to end.
+
+        """
+        component = self.component
+        events = self.events
+
+        self.sent_event(self.widget, TestEvents.END)
+        self.assertEnamlInSync(component, 'value', 1.0)
+        self.assertEqual(['moved'], events)
+
+    def testMovingDownByOneStep(self):
+        """ Test firing events and value when the thumb is moved by one step down.
+
+        """
+        component = self.component
+        events = self.events
+
+        self.sent_event(self.widget, TestEvents.STEP_DOWN)
+        self.assertEnamlInSync(component, 'value', 0.4)
+        self.assertEqual(['moved'], events)
+
+    def testMovingUpByOneStep(self):
+        """ Test firing events and value when the thumb is moved by one step up.
+
+        """
+        component = self.component
+        events = self.events
+
+        self.sent_event(self.widget, TestEvents.STEP_UP)
+        self.assertEnamlInSync(component, 'value', 0.6)
+        self.assertEqual(['moved'], events)
+
+    def testMovingDownByOnePage(self):
+        """ Test firing events and value when the thumb is moved by one page down.
+
+        """
+        component = self.component
+        events = self.events
+
+        self.sent_event(self.widget, TestEvents.PAGE_DOWN)
+        self.assertEnamlInSync(component, 'value', 0.3)
+        self.assertEqual(['moved'], events)
+
+    def testMovingUpByOnePage(self):
+        """ Test firing events and value when the thumb is moved by one page up.
+
+        """
+        component = self.component
+        events = self.events
+
+        self.sent_event(self.widget, TestEvents.PAGE_UP)
+        self.assertEnamlInSync(component, 'value', 0.7)
+        self.assertEqual(['moved'], events)
+
+
     #--------------------------------------------------------------------------
     # absrtact methods
     #--------------------------------------------------------------------------
@@ -249,6 +383,21 @@ Window:
     @abc.abstractmethod
     def get_tracking(self, widget):
         """ Get the Slider's tracking status.
+
+        """
+        return NotImplemented
+
+    @abc.abstractmethod
+    def sent_event(self, widget, event):
+        """ Sent an event to the Slider programmatically.
+
+        Arguments
+        ---------
+        widget :
+            The widget to sent the event to.
+
+        event :
+            The desired event to be proccessed.
 
         """
         return NotImplemented
