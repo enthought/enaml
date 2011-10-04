153182612ef8358c3a5cc50dde6a5cbdd779068c
itziakos <ioannist@enthought.com>
2011-10-04 14:48:44 +0100

    Implement testcase for the QtDateEdit

---
+++ tests/qt/test_qt_date_edit.py
@@ -0,0 +1,48 @@
+#------------------------------------------------------------------------------
+#  Copyright (c) 2011, Enthought, Inc.
+#  All rights reserved.
+#------------------------------------------------------------------------------
+from .. import date_edit
+from enaml.toolkit import qt_toolkit
+
+from enaml.widgets.qt.qt_date_edit import qdate_to_python
+
+class TestQtDateEdit(date_edit.TestDateEdit):
+    """ QtDateEdit tests. """
+
+    toolkit = qt_toolkit()
+
+    def get_date(self, widget):
+        """  Get the toolkits widget's active date.
+
+        """
+        date = widget.date()
+        return qdate_to_python(date)
+
+    def get_minimum_date(self, widget):
+        """  Get the toolkits widget's maximum date attribute.
+
+        """
+        date = widget.minimumDate()
+        return qdate_to_python(date)
+
+    def get_maximum_date(self, widget):
+        """ Get the toolkits widget's minimum date attribute.
+
+        """
+        date = widget.maximumDate()
+        return qdate_to_python(date)
+
+    def change_date(self, widget, date):
+        """ Simulate a change date action at the toolkit widget.
+
+        """
+        self.widget.setDate(date)
+        self.widget.dateChanged.emit(date)
+
+    def get_date_as_string(self, widget):
+        """  Get the toolkits widget's active date as a string.
+
+        """
+        return self.widget.text()
+
