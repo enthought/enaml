e8338709e5bc3aef6ad54470c068898192ba7a0b
itziakos <ioannist@enthought.com>
2011-10-05 14:37:33 +0100

    Remove trailing spaces

--- tests/qt/test_qt_field.py
+++ tests/qt/test_qt_field.py
@@ -24,36 +24,36 @@ class TestQtField(field.TestField):
 
     def change_text(self, widget, text):
         """ Change text programmatically, rather than "edit" it.
-        
+
         """
         widget.setText(text)
 
     def set_cursor_position(self, widget, index):
         """ Set the cursor at a specific position.
-        
+
         """
         widget.setCursorPosition(index)
 
     def get_cursor_position(self, widget):
         """ Get the cursor position.
-        
+
         """
         return widget.cursorPosition()
 
     def set_selected_text(self, widget, start, stop):
         """ Select text in a field.
-        
+
         """
         widget.setSelection(start, stop - start)
 
     def get_selected_text(self, widget):
         """ Get the currently-selected text from a field.
-        
+
         """
         return widget.selectedText()
 
     def press_return(self, widget):
         """ Simulate a press of the 'Return' key.
-        
+
         """
         widget.returnPressed.emit()
--- tests/wx/test_wx_calendar.py
+++ tests/wx/test_wx_calendar.py
@@ -27,22 +27,22 @@ class TestWxCalendar(calendar.TestCalendar):
 
     def get_maximum_date(self, widget):
         """ Get a calendar's maximum date attribute.
-        
+
         """
         return widget.PyGetUpperDateLimit()
 
     def activate_date(self, widget, date):
         """ Fire an event to indicate that a date was activated.
-        
+
         """
         cal_event = wx.calendar.EVT_CALENDAR
         event = wx.calendar.CalendarEvent(widget, cal_event.typeId)
         event.PySetDate(date)
         widget.GetEventHandler().ProcessEvent(event)
-        
+
     def select_date(self, widget, date):
         """ Fire an event to indicate that a date was selected.
-        
+
         """
         cal_select_event = wx.calendar.EVT_CALENDAR_SEL_CHANGED
         event = wx.calendar.CalendarEvent(widget, cal_select_event.typeId)
