7ffb0b27e63d4c7da6bf5160ba19da097ec153aa
itziakos <ioannist@enthought.com>
2011-10-05 14:43:02 +0100

    Fix enaml Qt based calendar component (all tests pass)

--- enaml/widgets/qt/qt_calendar.py
+++ enaml/widgets/qt/qt_calendar.py
@@ -24,7 +24,7 @@ class QtCalendar(QtControl):
     See Also
     --------
     Calendar
-    
+
     """
     implements(ICalendarImpl)
 
@@ -42,32 +42,25 @@ class QtCalendar(QtControl):
 
         """
         parent = self.parent
-        self.set_date(parent.date)
-        min_date = parent.minimum_date
-        max_date = parent.maximum_date
-        if min_date is not None:
-            self.set_minimum_date(min_date)
-        if max_date is not None:
-            self.set_maximum_date(max_date)
+        self.set_minimum_date(parent.minimum_date)
+        self.set_maximum_date(parent.maximum_date)
+        self.set_and_validate_date()
         self.bind()
 
     def parent_date_changed(self, date):
-        """ The change handler for the 'date' attribute. Not meant for
-        public consumption.
+        """ The change handler for the 'date' attribute.
 
         """
-        self.set_date(date)
+        self.set_and_validate_date()
 
     def parent_minimum_date_changed(self, date):
-        """ The change handler for the 'minimum_date' attribute. Not 
-        meant for public consumption.
+        """ The change handler for the 'minimum_date' attribute.
 
         """
         self.set_minimum_date(date)
 
     def parent_maximum_date_changed(self, date):
-        """ The change handler for the 'maximum_date' attribute. Not
-        meant for public consumption.
+        """ The change handler for the 'maximum_date' attribute.
 
         """
         self.set_maximum_date(date)
@@ -76,55 +69,60 @@ class QtCalendar(QtControl):
     # Implementation
     #---------------------------------------------------------------------------
     def bind(self):
-        """ Binds the event handlers for the calendar widget. Not meant
-        for public consumption.
+        """ Binds the event handlers for the calendar widget.
 
         """
         widget = self.widget
         widget.activated.connect(self.on_date_activated)
         widget.selectionChanged.connect(self.on_date_selected)
-        
+
     def on_date_activated(self):
-        """ The event handler for the calendar's activation event. Not
-        meant for public consumption.
+        """ The event handler for the calendar's activation event.
 
         """
-        qdate = self.widget.selectedDate()
-        py_date = qdate_to_python(qdate)
         parent = self.parent
-        parent.date = py_date
-        parent.activated = py_date
+        widget_date = self.get_date()
+        parent.date = widget_date
+        parent.activated = widget_date
 
     def on_date_selected(self):
-        """ The event handler for the calendar's selection event. Not
-        meant for public consumption.
+        """ The event handler for the calendar's selection event.
 
         """
-        qdate = self.widget.selectedDate()
-        py_date = qdate_to_python(qdate)
-        self.parent.selected = py_date
-        
-    def set_date(self, date):
-        """ Sets the date on the widget with the provided value. Not
-        meant for public consumption.
+        parent = self.parent
+        widget_date = self.get_date()
+        parent.date = widget_date
+        parent.selected = parent.date
 
+    def set_and_validate_date(self):
+        """ Sets and validates the component date on the widget.
+
+        The method sets the date in the toolkit widget and makes sure that
+        if the widget has truncated the enaml component is syncronized
+        without firing trait notification events.
         """
+        parent = self.parent
+        date = parent.date
         self.widget.setSelectedDate(date)
+        validated_widget_date = self.get_date()
+        if validated_widget_date != date:
+            self.parent.trait_setq(date=validated_widget_date)
 
     def set_minimum_date(self, date):
         """ Sets the minimum date on the widget with the provided value.
-        Not meant for public consumption.
 
         """
         self.widget.setMinimumDate(date)
 
     def set_maximum_date(self, date):
         """ Sets the maximum date on the widget with the provided value.
-        Not meant for public consumption.
 
         """
         self.widget.setMaximumDate(date)
 
+    def get_date(self):
+        """ Get the active widget date.
 
-        
-    
+        """
+        qdate = self.widget.selectedDate()
+        return qdate_to_python(qdate)
