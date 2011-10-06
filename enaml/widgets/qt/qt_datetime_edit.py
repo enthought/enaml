a0b3cdfdb7a9a0be8e02d4f80a1eee7044b8cbf7
itziakos <ioannist@enthought.com>
2011-10-06 13:37:36 +0100

    Fix implementation of DateTimeEdit component (all tests pass)

--- enaml/widgets/qt/qt_datetime_edit.py
+++ enaml/widgets/qt/qt_datetime_edit.py
@@ -24,7 +24,7 @@ class QtDateTimeEdit(QtControl):
     See Also
     --------
     DateEdit
-    
+
     """
     implements(IDateTimeEditImpl)
 
@@ -42,75 +42,91 @@ class QtDateTimeEdit(QtControl):
 
         """
         parent = self.parent
-        self.set_datetime(parent.datetime)
-        min_datetime = parent.minimum_datetime
-        max_datetime = parent.maximum_datetime
-        if min_datetime is not None:
-            self.set_minimum_datetime(min_datetime)
-        if max_datetime is not None:
-            self.set_maximum_datetime(max_datetime)
+        self.set_minimum_datetime(parent.minimum_datetime)
+        self.set_maximum_datetime(parent.maximum_datetime)
+        self.set_format(parent.format)
+        self.set_and_validate_date()
         self.connect()
 
     def parent_datetime_changed(self, datetime):
-        """ The change handler for the 'date' attribute. Not meant for
-        public consumption.
+        """ The change handler for the 'date' attribute.
 
         """
-        self.set_datetime(datetime)
+        self.set_and_validate_date()
 
     def parent_minimum_datetime_changed(self, datetime):
-        """ The change handler for the 'minimum_date' attribute. Not 
-        meant for public consumption.
+        """ The change handler for the 'minimum_date' attribute.
 
         """
         self.set_minimum_datetime(datetime)
 
-    def parent_maximum_date_changed(self, datetime):
-        """ The change handler for the 'maximum_date' attribute. Not
-        meant for public consumption.
+    def parent_maximum_datetime_changed(self, datetime):
+        """ The change handler for the 'maximum_date' attribute.
 
         """
         self.set_maximum_datetime(datetime)
+
+    def parent_format_changed(self, datetime_format):
+        """ The change handler for the 'format' attribute.
+
+        """
+        self.set_format(datetime_format)
+
 
     #---------------------------------------------------------------------------
     # Implementation
     #---------------------------------------------------------------------------
     def connect(self):
-        """ Connects the signal handlers for the date edit widget. Not 
+        """ Connects the signal handlers for the date edit widget. Not
         meant for public consumption.
 
         """
         self.widget.dateTimeChanged.connect(self.on_datetime_changed)
-        
+
     def on_datetime_changed(self):
         """ The signal handler for the controls's changed event. Not
         meant for public consumption.
-
         """
         parent = self.parent
-        qdatetime = self.widget.dateTime()
-        py_datetime = qdatetime_to_python(qdatetime)
-        parent.datetime = py_datetime
-        parent.activated = py_datetime
-        
-    def set_datetime(self, datetime):
-        """ Sets the datetime on the widget with the provided value. Not
-        meant for public consumption.
+        new_datetime = self.get_datetime()
+        parent.datetime = new_datetime
+        parent.datetime_changed = new_datetime
 
+    def set_and_validate_date(self):
+        """ Sets and validates the datetime on the widget.
+
+        The method sets the datetime in the toolkit widget and makes sure
+        that if the widget has truncated the value,  the enaml component
+        is syncronized without firing trait notification events.
         """
+        parent = self.parent
+        datetime = parent.datetime
         self.widget.setDateTime(datetime)
+        validated_widget_datetime = self.get_datetime()
+        if validated_widget_datetime != datetime:
+            self.parent.trait_setq(datetime=validated_widget_datetime)
 
     def set_minimum_datetime(self, datetime):
         """ Sets the minimum datetime on the widget with the provided value.
-        Not meant for public consumption.
 
         """
         self.widget.setMinimumDateTime(datetime)
 
     def set_maximum_datetime(self, datetime):
         """ Sets the maximum datetime on the widget with the provided value.
-        Not meant for public consumption.
 
         """
         self.widget.setMaximumDateTime(datetime)
 
+    def set_format(self, datetime_format):
+        """ Sets the display format on the widget with the provided value.
+
+        """
+        self.widget.setDisplayFormat(datetime_format)
+
+    def get_datetime(self):
+        """ Get the active widget date.
+
+        """
+        qdatetime = self.widget.dateTime()
+        return qdatetime_to_python(qdatetime)
