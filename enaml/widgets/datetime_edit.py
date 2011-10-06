87b411e3c25d59457066149ef889b999e258ec2e
itziakos <ioannist@enthought.com>
2011-10-06 13:35:43 +0100

    Fix DateTimeEdit interface
    - fix namespace conflict with the datetime library
    - Correct event name
    - add format change handler (abstract implementation)
    - Set default values for the valid range (based on Qt)

--- enaml/widgets/datetime_edit.py
+++ enaml/widgets/datetime_edit.py
@@ -2,7 +2,7 @@
 #  Copyright (c) 2011, Enthought, Inc.
 #  All rights reserved.
 #------------------------------------------------------------------------------
-from datetime import datetime
+from datetime import datetime as python_datetime
 
 from traits.api import Event, Instance, Str
 
@@ -13,33 +13,42 @@ class IDateTimeEditImpl(IControlImpl):
 
     def parent_datetime_changed(self, datetime):
         raise NotImplementedError
-    
+
     def parent_minimum_datetime_changed(self, datetime):
         raise NotImplementedError
-    
+
     def parent_maximum_datetime_changed(self, datetime):
+        raise NotImplementedError
+
+    def parent_format_changed(self, datetime):
         raise NotImplementedError
 
 
 class DateTimeEdit(Control):
     """ A datetime widget.
 
-    A DatetimeEdit displays a Python datetime.datetime using an 
-    appropriate toolkit specific control.
-    
+    A DatetimeEdit displays a Python datetime.datetime using an
+    appropriate toolkit specific control. The component behaves like the
+    Qt DateTimeEdit widget.
+
     Attributes
     ----------
     datetime : Instance(datetime.datetime)
-        The currently selected datetime.
+        The currently selected datetime. Default value is the current
+        time in the machine.
 
     minimum_datetime : Instance(datetime.datetime)
-        The minimum datetime available in the date edit.
+        The minimum datetime available in the date edit. By default, this
+        property contains a date that refers to September 14, 1752 and a
+        time of 00:00:00 and 0 milliseconds.
 
     maximum_datetime : Instance(datetime.datetime)
-        The maximum datetime available in the date edit.
-    
+        The maximum datetime available in the date edit. By default, this
+        property contains a date that refers to 31 December, 7999 and a
+        time of 23:59:59 and 999 milliseconds.
+
     format : Str
-        A python date format string to format the datetime. If none is 
+        A python date format string to format the datetime. If none is
         supplied (or is invalid) the system locale setting is used.
         This may not be supported by all backends.
 
@@ -47,16 +56,18 @@ class DateTimeEdit(Control):
         Triggered whenever the user clicks or changes the control. The
         event payload will be the datetime on the control.
 
-    """    
-    datetime = Instance(datetime, factory=datetime.now)
+    """
+    datetime = Instance(python_datetime, factory=python_datetime.now)
 
-    minimum_datetime = Instance(datetime)
+    minimum_datetime = Instance(python_datetime,
+                                args=(1752,9,14, 0, 0, 0, 0))
 
-    maximum_datetime = Instance(datetime)
+    maximum_datetime = Instance(python_datetime,
+                                args=(7999, 12, 31, 23, 59, 59, 999000))
 
     format = Str
 
-    activated = Event
+    datetime_changed = Event
 
     #---------------------------------------------------------------------------
     # Overridden parent traits
@@ -64,5 +75,5 @@ class DateTimeEdit(Control):
     toolkit_impl = Instance(IDateTimeEditImpl)
 
 
-DateTimeEdit.protect('activated')
+DateTimeEdit.protect('datetime_changed')
 
