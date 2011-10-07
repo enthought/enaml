3d3b096788d83f9931e159410d5f5941ba35342c
itziakos <ioannist@enthought.com>
2011-10-07 20:09:49 +0100

    Add TestAssistant class for Qt backend

---
+++ tests/widgets/qt/qt_test_assistant.py
@@ -0,0 +1,18 @@
+#------------------------------------------------------------------------------
+#  Copyright (c) 2011, Enthought, Inc.
+#  All rights reserved.
+#------------------------------------------------------------------------------
+from enaml.toolkit import qt_toolkit
+
+class QtTestAssistant(object):
+    """ Assistant class for testing wx based components.
+
+    This class is to be used as a mixing with the base enaml test case
+    class for the components tests of the qt backend. It sets the correct
+    toolkit attribute and (in the future) provide some useful methods for
+    testin Qt based components.
+
+    """
+
+    toolkit = qt_toolkit()
+
