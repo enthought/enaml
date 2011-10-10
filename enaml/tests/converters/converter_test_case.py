1c3cdff0c3acbfef82b611fdb7704fac6b83036c
itziakos <ioannist@enthought.com>
2011-10-10 14:41:08 +0100

    Minor clean up of docstrings

--- enaml/tests/converters/converter_test_case.py
+++ enaml/tests/converters/converter_test_case.py
@@ -3,21 +3,19 @@ import unittest
 
 class ConverterTestCase(unittest.TestCase):
     """ A base class for testing `enaml.converters.Converter` objects.
-    
-    This class simply contains utility methods, and should not be directly
-    instantiated.
-    
+
     """
-    
+
     def assertConverterSymmetric(self, converter, widget_value, external_value):
         """ Check that the 'to_component' and 'from_component' methods of
-        a converter are symmetric; i.e., that they undo each other.
-        
+        a converter are symmetric:
+
+
         """
         external_converted = converter.from_component(widget_value)
         self.assertEqual(external_converted, external_value)
-        
+
         widget_converted = converter.to_component(external_value)
         self.assertEqual(widget_converted, widget_value)
 
-        # Should we check the types?
+        # XXX Should we check the types?
