7b5379f64fb5a83e4e7a646e21f8e3e012f68efd
itziakos <ioannist@enthought.com>
2011-10-24 10:54:13 +0100

    correct the index link to EnamlTestCase

--- enaml/tests/widgets/__init__.py
+++ enaml/tests/widgets/__init__.py
@@ -3,9 +3,10 @@
 The tests.widgets package contains a set of abstract classes that simplify
 the testing of the Enaml object components for the implemented toolkits.
 
-All Enaml testcases are children of the :class:`EnamlTestCase` class.
-The class implements the main functionality to parse and prepare
-the Enaml-based View object for testing without initiating the event Loop.
+All Enaml testcases are children of the
+:class:`~enaml.tests.widgets.enaml_test_case.EnamlTestCase` class. The
+class implements the main functionality to parse and prepare the Enaml
+based View object for testing without initiating the event Loop.
 
 For each Enaml object in the standard set of graphical widgets there exist
 a corresponding abstract test class that tests the specific Enaml object
