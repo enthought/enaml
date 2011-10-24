ef3d009071e4a174fdaabcfbb11912e4ea290a1a
Robert Kern <rkern@enthought.com>
2011-10-24 21:42:12 +0100

    ENH: Add the show_layout tool for displaying Enaml files that do not need to be attached to models. This is useful for experimenting with laying out Components before hooking their behaviors.

---
+++ tools/show_layout.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+""" Simple command-line tool to parse an Enaml file and display a model-less
+version of the argument-less MainWindow component inside of it.
+"""
+
+import enaml
+from enaml.toolkit import default_toolkit
+from enaml.parsing.parser import parse
+from enaml.parsing.enaml_compiler import EnamlCompiler
+
+
+def main():
+    import argparse
+    parser = argparse.ArgumentParser(
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+        description=__doc__,
+    )
+    parser.add_argument('-c', '--component', default='MainWindow',
+        help="The component to view.")
+    parser.add_argument('enaml_file', help='The .enaml file to show.')
+
+    args = parser.parse_args()
+
+    with open(args.enaml_file) as f:
+        enaml_code = f.read()
+    ast = parse(enaml_code)
+    ns = {}
+
+    with enaml.imports():
+        EnamlCompiler.compile(ast, ns)
+    component = ns[args.component]
+    toolkit = default_toolkit()
+    with toolkit:
+        window = component()[0]
+    app = toolkit.create_app()
+    window.show()
+    toolkit.start_app()
+
+if __name__ == '__main__':
+    main()
