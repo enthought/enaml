c270122ee32bb55771a98b0e6edd6787f4534c3d
itziakos <ioannist@enthought.com>
2011-10-27 15:55:44 +0100

    Refactor WXCheckBox to use the new interface

--- enaml/widgets/wx/wx_check_box.py
+++ enaml/widgets/wx/wx_check_box.py
@@ -4,45 +4,34 @@
 #------------------------------------------------------------------------------
 import wx
 
-from traits.api import implements
-
 from .wx_toggle_control import WXToggleControl
+from ..check_box import AbstractTkCheckBox
 
-from ..check_box import ICheckBoxImpl
-
-
-class WXCheckBox(WXToggleControl):
+class WXCheckBox(WXToggleControl, AbstractTkCheckBox):
     """ A wxPython implementation of CheckBox.
 
     A Checkbox provides a toggleable control using a wx.CheckBox.
 
-    See Also
-    --------
-    CheckBox
-
     """
-    implements(ICheckBoxImpl)
 
-    #---------------------------------------------------------------------------
-    # ICheckBoxImpl interface
-    #---------------------------------------------------------------------------
-    def create_widget(self):
+    #--------------------------------------------------------------------------
+    # Setup methods
+    #--------------------------------------------------------------------------
+    def create(self):
         """ Creates and the wx.CheckBox.
 
         """
         self.widget = widget = wx.CheckBox(self.parent_widget())
         widget.SetDoubleBuffered(True)
-        
-    #---------------------------------------------------------------------------
-    # Implementation
-    #---------------------------------------------------------------------------
+
     def bind(self):
         """ Binds the event handlers for the check box. Not meant for
         public consumption.
 
         """
+        super(WXCheckBox, self).bind()
         widget = self.widget
         widget.Bind(wx.EVT_CHECKBOX, self.on_toggled)
         widget.Bind(wx.EVT_LEFT_DOWN, self.on_pressed)
-        widget.Bind(wx.EVT_LEAVE_WINDOW, self.on_leave_window)  
+        widget.Bind(wx.EVT_LEAVE_WINDOW, self.on_released)
 
