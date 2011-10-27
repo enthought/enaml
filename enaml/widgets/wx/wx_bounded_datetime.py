0c75146d63517fbf50d2ef23c596e9e4047e711d
itziakos <ioannist@enthought.com>
2011-10-27 15:20:48 +0100

    Add Bounded_Datetime component to wx

---
+++ enaml/widgets/wx/wx_bounded_datetime.py
@@ -0,0 +1,48 @@
+#------------------------------------------------------------------------------
+#  Copyright (c) 2011, Enthought, Inc.
+#  All rights reserved.
+#------------------------------------------------------------------------------
+from .wx_control import WXControl
+
+from ..bounded_datetime import AbstractTkBoundedDatetime
+
+
+class WXBoundedDatetime(WXControl, AbstractTkBoundedDatetime):
+    """ A base class for use with widgets implementing behavior
+    for subclasses of BoundedDatetime.
+
+    """
+    #--------------------------------------------------------------------------
+    # Setup methods
+    #--------------------------------------------------------------------------
+    def initialize(self):
+        super(QtControl, self).initialize()
+        shell = self.shell_obj
+        self.set_min_datetime(shell.min_datetime)
+        self.set_max_datetime(shell.max_datetime)
+        self.set_datetime(shell.datetime)
+
+    #--------------------------------------------------------------------------
+    # Abstract implementation methods
+    #--------------------------------------------------------------------------
+    def shell_datetime_changed(self, datetime):
+        self.set_datetime(datetime)
+
+    def shell_min_datetime_changed(self, min_datetime):
+        self.set_min_datetime(min_datetime)
+
+    def shell_max_datetime_changed(self, max_datetime):
+        self.set_max_datetime(max_datetime)
+
+    #--------------------------------------------------------------------------
+    # Widget modification methods
+    #--------------------------------------------------------------------------
+    def set_datetime(self, datetime):
+        raise NotImplementedError
+
+    def set_min_datetime(self, min_datetime):
+        raise NotImplementedError
+
+    def set_max_datetime(self, max_datetime):
+        raise NotImplementedError
+
