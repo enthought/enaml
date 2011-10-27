1d7362aa57a4d66ec9ddadf4d64963b37876d947
Corran Webster <cwebster@enthought.com>
2011-10-27 17:25:20 -0500

    Update Field example in docs

--- docs/source/examples/field.py
+++ docs/source/examples/field.py
@@ -2,26 +2,10 @@
 #  Copyright (c) 2011, Enthought, Inc.
 #  All rights reserved.
 #------------------------------------------------------------------------------
-from cStringIO import StringIO
+import enaml
 
-from enaml.factory import EnamlFactory
+with enaml.imports():
+    from field import FieldExample
 
-enml = """
-Window:
-    title = "Field example"
-    Panel:
-        VGroup:
-            Label:
-                text = "This is an integer field"
-            Field mf:
-                to_string = str
-                from_string = lambda val: int(val) if val else 0
-                value = 0
-                style.background_color << "error" if self.error else "nocolor"
-"""
-
-fact = EnamlFactory(StringIO(enml))
-
-view = fact()
-
+view = FieldExample()
 view.show()
