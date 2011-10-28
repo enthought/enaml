fb44f66a834d2bbaaac2e59faeca7ed45cc015e9
itziakos <ioannist@enthought.com>
2011-10-28 17:43:32 +0100

    Refactor WXHtml to the new interface

--- enaml/widgets/wx/wx_html.py
+++ enaml/widgets/wx/wx_html.py
@@ -4,57 +4,43 @@
 #------------------------------------------------------------------------------
 import wx.html
 
-from traits.api import implements
-
 from .wx_control import WXControl
+from ..html import AbstractTkHtml
 
-from ..html import IHtmlImpl
-
-
-class WXHtml(WXControl):
+class WXHtml(WXControl, AbstractTkHtml):
     """ A wxPython implementation of IHtml.
-    
+
     The WXHtml widget renders html source using a wx.html.HtmlWindow.
 
-    See Also
-    --------
-    IHtml
-    
     """
-    implements(IHtmlImpl)
-
-    #---------------------------------------------------------------------------
-    # IHtmlImpl interface
-    #---------------------------------------------------------------------------
-    def create_widget(self):
+    #--------------------------------------------------------------------------
+    # Setup methods
+    #--------------------------------------------------------------------------
+    def create(self):
         """ Creates the underlying html control.
 
         """
-        # XXX GetBestSize on the HtmlWindow returns (0, 0) (wtf?)
+        # XXX GetBestSize on the HtmlWindow returns (0, 0)
         # So for now, we just set the min size to something sensible.
-        self.widget = widget = wx.html.HtmlWindow(self.parent_widget(), 
-                                                  size=(300, 200))
-        widget.SetDoubleBuffered(True)
+        self.widget = wx.html.HtmlWindow(self.parent_widget(), size=(300, 200))
 
-    def initialize_widget(self):
+    def initialize(self):
         """ Initializes the attributes of the control.
 
         """
-        self.set_page_source(self.parent.source)
-    
-    def parent_source_changed(self, source):
-        """ The change handler for the 'source' attribute. Not meant for
-        public consumption.
-
-        """
-        self.set_page_source(source)
+        shell = self.shell_obj
+        self.set_page_source(shell.source)
 
     #---------------------------------------------------------------------------
     # Implementation
     #---------------------------------------------------------------------------
+    def shell_source_changed(self, source):
+        """ The change handler for the 'source' attribute.
+        """
+        self.set_page_source(source)
+
     def set_page_source(self, source):
-        """ Sets the page source for the underlying control. Not meant 
-        for public consumption.
+        """ Sets the page source for the underlying control.
 
         """
         self.widget.SetPage(source)
