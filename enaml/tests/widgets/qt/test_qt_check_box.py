080979babf9d3f0ef8fba50b61588c6daa56c74c
itziakos <ioannist@enthought.com>
2011-11-09 10:56:34 +0000

    DOC: Correct docstring

--- enaml/tests/widgets/qt/test_qt_check_box.py
+++ enaml/tests/widgets/qt/test_qt_check_box.py
@@ -14,9 +14,8 @@ class TestQtCheckBox(QtTestAssistant, check_box.TestCheckBox):
         """
         return widget.text()
 
-    def checked_status(self, widget):
-        """ Returns the text from the tookit widget.
-
+    def get_checked(self, widget):
+        """ Returns the checked status from the tookit widget.
 
         """
         return widget.isChecked()
