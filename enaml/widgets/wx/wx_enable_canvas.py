a690ff73c3e46c126c43d2b82784ae9201c8f90a
Chris Colbert <sccolbert@gmail.com>
2011-11-17 14:19:45 -0500

    update docs

--- enaml/widgets/wx/wx_enable_canvas.py
+++ enaml/widgets/wx/wx_enable_canvas.py
@@ -10,17 +10,29 @@ from ..enable_canvas import AbstractTkEnableCanvas
 
 
 class WXEnableCanvas(WXControl, AbstractTkEnableCanvas):
+    """ A wxPython implementation of EnableCanvas
 
+    """
+    #: The EnableWindow that is created to hold the component
     window = None
 
     #--------------------------------------------------------------------------
     # Setup methods
     #--------------------------------------------------------------------------
     def create(self):
+        """ Creates an EnableWindow instance to hold the component and
+        uses that instance control as the toolkit widget. This assumes
+        that enable picks the appropriate toolkit backend.
+
+        """
         component = self.shell_obj.component
         self.window = EnableWindow(self.parent_widget(), component=component)
         self.widget = self.window.control
 
     def shell_component_changed(self, component):
+        """ The change handler for the 'component' attribute on the 
+        shell object.
+
+        """
         raise NotImplementedError('changing components not yet supported')
 
