2aa838fa97c2a12963efb0a1ef3803db745ab2e2
Chris Colbert <sccolbert@gmail.com>
2011-11-18 11:36:17 -0500

    update docs

--- enaml/widgets/container.py
+++ enaml/widgets/container.py
@@ -2,7 +2,6 @@
 #  Copyright (c) 2011, Enthought, Inc.
 #  All rights reserved.
 #------------------------------------------------------------------------------
-
 from traits.api import List, Instance, Either, Bool, on_trait_change
 
 from .component import Component, AbstractTkComponent
@@ -11,20 +10,45 @@ from .layout.constraints_layout import ConstraintsLayout
 from .layout.layout_manager import AbstractLayoutManager
 
 
+_SIZE_HINT_DEPS = ('children:size_hint_updated, children:hug_width, '
+                   'children:hug_height, children:resist_clip_width, '
+                   'children:resist_clip_height')
+
+
+_CONSTRAINT_DEPS = ('constraints, constraints_items, default_constraints, '
+                    'default_constraints_items')
+
+
 class AbstractTkContainer(AbstractTkComponent):
     """ The abstract toolkit Container interface.
 
     A toolkit container is responsible for handling changes on a shell
-    Container and proxying those changes to and from its internal toolkit
-    widget.
+    Container object and proxying those changes to and from its internal 
+    toolkit widget.
 
     """
+    pass
 
 
 class Container(Component):
-    """ A Component subclass that provides for laying out child Components.
+    """ A Component subclass that provides for laying out child 
+    Components.
 
     """
+    #: An object that manages the layout of this component and its
+    #: direct children. The default is simple constraints based
+    layout = Instance(AbstractLayoutManager)
+
+    #: A list of user-specified linear constraints defined for this 
+    #: container.
+    constraints = List()
+
+    #: Overridden parent class trait
+    abstract_obj = Instance(AbstractTkContainer)
+
+    #: Overridden parent class trait
+    children = List(Either(Instance(Control), Instance('Container')))
+    
     #: A private boolean indicating if the contraints have changed
     #: and need to be updated on the next pass.
     _needs_update_constraints = Bool(True)
@@ -33,20 +57,6 @@ class Container(Component):
     #: its children
     _needs_layout = Bool(True)
 
-    #: An object that manages the layout of this component and its
-    #: direct children. The default is simple constraints based
-    layout = Instance(AbstractLayoutManager)
-
-    #: A list of user-specified linear constraints defined for this container.
-    #constraints = List(Instance(BaseConstraint))
-    constraints = List()
-
-    #: Overridden parent class trait
-    abstract_obj = Instance(AbstractTkContainer)
-
-    #: Overridden parent class trait
-    children = List(Either(Instance(Control), Instance('Container')))
-
     def _layout_default(self):
         """ Default value for the layout manager.
 
@@ -54,15 +64,13 @@ class Container(Component):
         return ConstraintsLayout(self)
 
     def setup(self):
-        """ Run the setup process for the ui tree.
-
-        This is overridden to add the layout set up.
+        """ Run the setup process for the ui tree. This is overridden 
+        from the parent class to add initialize_layout to the setup.
 
         """
         # XXX make layout setup a completely separate pass
         # probably handled by the view object.
         super(Container, self).setup()
-
         self.initialize_layout()
 
     def initialize_layout(self):
@@ -78,16 +86,23 @@ class Container(Component):
         Default behaviour is to put the children into a vertical layout.
         Subclasses of Container which implement container_constraints will
         probably want to override this (possibly to return an empty list).
+
         """
         from .layout.layout_helpers import horizontal, vertical
-        vertical_args = [self.top] + self.children + [self.bottom]
-        return [vertical(*vertical_args)]+[horizontal(self.left, child, self.right)
-            for child in self.children]
+        children = self.children
+        top = self.top
+        bottom = self.bottom
+        left = self.left
+        right = self.right
+        vert = [vertical(*([top] + children + [bottom]))]
+        horiz = [horizontal(left, child, right) for child in children]
+        return vert + horiz
     
     def container_constraints(self):
-        """ A set of constraints that should always be applied to this type of
-        container.  This is intended to be implemented by Container subclasses
-        suchas Form to set up their standard constraints
+        """ A set of constraints that should always be applied to this
+        type of container. This should be implemented by subclasses
+        such as Form to set up their standard constraints.
+
         """
         return []
 
@@ -132,8 +147,7 @@ class Container(Component):
             self.toolkit.invoke_later(self.do_layout)
 
     def set_needs_layout(self, needs=True):
-        """ Indicate that the layout should be refreshed some time
-        later.
+        """ Indicate that the layout should be refreshed some time later.
 
         """
         if self.layout is None:
@@ -143,10 +157,10 @@ class Container(Component):
             old = self._needs_layout
             self._needs_layout = needs
             if not old and needs:
-                # Only invoke the do_layout() once, when _needs_layout changes from
-                # False to True, but not when it was already True. This makes sure
-                # that we only update the layout once even if we set multiple traits
-                # that may request a new layout.
+                # Only invoke the do_layout() once, when _needs_layout 
+                # changes from False to True, but not when it was already 
+                # True. This makes sure that we only update the layout once 
+                # even if we set multiple traits that request a new layout.
                 self.toolkit.invoke_later(self.do_layout)
 
     def do_layout(self):
@@ -157,17 +171,26 @@ class Container(Component):
             self.layout.layout()
         self._needs_layout = False
 
-    @on_trait_change('children:size_hint_updated, children:hug_width, children:hug_height, children:resist_clip_width, children:resist_clip_height')
+    @on_trait_change(_SIZE_HINT_DEPS)
     def handle_size_hint_changed(self, child, name, old, new):
+        """ A change handler for updaing the layout when the size hint
+        of any of the container's children have changed.
+
+        """
         if self.layout is None:
-            # Our layout is managed by an ancestor. Pass up the notification.
+            # Our layout is managed by an ancestor, so pass up 
+            #  the notification.
             self.parent.handle_size_hint_changed(child, name, old, new)
         else:
             self.toolkit.invoke_later(self.layout.update_size_cns, child)
             self.set_needs_layout()
 
-    @on_trait_change('constraints,constraints_items,default_constraints,default_constraints_items')
+    @on_trait_change(_CONSTRAINT_DEPS)
     def handle_constraints_changed(self):
+        """ A handler for updating the layout when the constraints of the
+        container change.
+
+        """
         if self.layout._initialized:
             self.set_needs_update_constraints()
             self.set_needs_layout()
