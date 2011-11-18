24923f38ddc3573edcc3982a6a18c8e224aa78b5
Chris Colbert <sccolbert@gmail.com>
2011-11-18 12:08:33 -0500

    formatting

--- enaml/widgets/wx/wx_form.py
+++ enaml/widgets/wx/wx_form.py
@@ -1,3 +1,7 @@
+#------------------------------------------------------------------------------
+#  Copyright (c) 2011, Enthought, Inc.
+#  All rights reserved.
+#------------------------------------------------------------------------------
 from .wx_container import WXContainer
 
 from ..form import AbstractTkForm
@@ -7,7 +11,6 @@ class WXForm(WXContainer, AbstractTkForm):
     """ A wxPython implementation of Container.
 
     """
-
     # The WXContainer implementation is enough.
     pass
 
