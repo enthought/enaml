8e49b711145b7d1e8121180244c7b2ea7129528e
Chris Colbert <sccolbert@gmail.com>
2011-11-18 12:32:29 -0500

    format. docs. don't update layout during initialization.

--- enaml/widgets/qt/qt_group_box.py
+++ enaml/widgets/qt/qt_group_box.py
@@ -2,7 +2,6 @@
 #  Copyright (c) 2011, Enthought, Inc.
 #  All rights reserved.
 #------------------------------------------------------------------------------
-
 from .qt import QtCore
 from .qt_container import QtContainer
 from .qt_resizing_widgets import QResizingGroupBox
@@ -21,11 +20,9 @@ class QtGroupBox(QtContainer, AbstractTkGroupBox):
     """ A Qt4 implementation of GroupBox.
 
     """
-
     #--------------------------------------------------------------------------
     # Setup methods
     #--------------------------------------------------------------------------
-
     def create(self):
         """ Creates the underlying QGroupBox control.
 
@@ -38,9 +35,9 @@ class QtGroupBox(QtContainer, AbstractTkGroupBox):
         """
         super(QtGroupBox, self).initialize()
         shell = self.shell_obj
-        self.shell_title_changed(shell.title)
-        self.shell_flat_changed(shell.flat)
-        self.shell_title_align_changed(shell.title_align)
+        self._set_title(shell.title)
+        self._set_flat(shell.flat)
+        self._set_title_align(shell.title_align)
 
     #--------------------------------------------------------------------------
     # Implementation
@@ -50,27 +47,57 @@ class QtGroupBox(QtContainer, AbstractTkGroupBox):
         shell object.
 
         """
-        self.widget.setTitle(title)
+        self._set_title(title)
+        # We need to call update constraints since the margins may 
+        # have changed. Using the size_hint_updated event here is
+        # not sufficient.
         self.shell_obj.set_needs_update_constraints()
 
     def shell_flat_changed(self, flat):
-        """ Update the flat flag of the group box with the new value from the
-        shell object.
+        """ Update the flat flag of the group box with the new value from
+        the shell object.
 
         """
-        self.widget.setFlat(flat)
+        self._set_flat(flat)
+        # We need to call update constraints since the margins may 
+        # have changed. Using the size_hint_updated event here is
+        # not sufficient.
         self.shell_obj.set_needs_update_constraints()
 
     def shell_title_align_changed(self, align):
-        """ Update the title alignment to the new value from the shell object.
+        """ Update the title alignment to the new value from the shell 
+        object.
+
+        """
+        self._set_title_align(align)
+
+    def get_contents_margins(self):
+        """ Return the (top, left, right, bottom) margin values for the
+        widget.
+
+        """
+        m = self.widget.contentsMargins()
+        return (m.top(), m.left(), m.right(), m.bottom())
+    
+    #--------------------------------------------------------------------------
+    # Widget Update methods 
+    #--------------------------------------------------------------------------
+    def _set_title(self, title):
+        """ Updates the title of group box.
+
+        """
+        self.widget.setTitle(title)
+    
+    def _set_flat(self, flat):
+        """ Updates the flattened appearance of the group box.
+
+        """
+        self.widget.setFlat(flat)
+    
+    def _set_title_align(self, align):
+        """ Updates the alignment of the title of the group box.
 
         """
         qt_align = QT_ALIGNMENTS[align]
         self.widget.setAlignment(qt_align)
 
-    def get_contents_margins(self):
-        """ Return the (top, left, right, bottom) margin values for the widget.
-
-        """
-        margins = self.widget.contentsMargins()
-        return (margins.top(), margins.left(), margins.right(), margins.bottom())
