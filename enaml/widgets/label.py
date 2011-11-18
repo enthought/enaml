ea9e1c959e708c085321ab8a84ddf4be42f604ba
Chris Colbert <sccolbert@gmail.com>
2011-11-18 16:01:06 -0500

    update docs. unify notification of size hint changed

--- enaml/widgets/label.py
+++ enaml/widgets/label.py
@@ -23,17 +23,18 @@ class Label(Control):
     #: The text in the label.
     text = Str
     
-    #: How strongly a component hugs it's content.
-    #: Labels hug their contents' width weakly.
+    #: How strongly a component hugs it's content. Labels hug their
+    #: contents' width weakly by default.
     hug_width = 'weak'
 
     #: Overridden parent class trait
     abstract_obj = Instance(AbstractTkLabel)
 
     def _text_changed(self):
-        """ Since the text has changed, the label's size hint has probably
-        changed too. The layout system of the parent Container should be
-        informed of this.
+        """ Since the text has changed, the label's size hint has also
+        likely changed. The layout system of the parent Container should 
+        be informed of this.
 
         """
-        self.parent.set_needs_update_constraints(True)
+        self.size_hint_updated = True
+
--- enaml/widgets/wx/wx_label.py
+++ enaml/widgets/wx/wx_label.py
@@ -3,21 +3,21 @@
 #  All rights reserved.
 #------------------------------------------------------------------------------
 import wx
+
 from .wx_control import WXControl
+
 from ..label import AbstractTkLabel
 
 
 class WXLabel(WXControl, AbstractTkLabel):
     """ A wxPython implementation of Label.
 
-    A WXLabel displays static text using a wx.StaticText control.
-
     """
     #--------------------------------------------------------------------------
     # Setup methods
     #--------------------------------------------------------------------------
     def create(self):
-        """ Creates the underlying text control.
+        """ Creates the underlying wx.StaticText control.
 
         """
         self.widget = wx.StaticText(self.parent_widget())
@@ -27,8 +27,7 @@ class WXLabel(WXControl, AbstractTkLabel):
 
         """
         super(WXLabel, self).initialize()
-        shell = self.shell_obj
-        self.set_label(shell.text)
+        self.set_label(self.shell_obj.text)
 
     #--------------------------------------------------------------------------
     # Implementation
@@ -38,7 +37,6 @@ class WXLabel(WXControl, AbstractTkLabel):
 
         """
         self.set_label(text)
-        self.shell_obj.size_hint_updated = True
 
     def set_label(self, label):
         """ Sets the label on the underlying control.
