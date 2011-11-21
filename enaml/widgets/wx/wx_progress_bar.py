221fa9fb7d8215f14f1e19f3381d4ad786c42405
Chris Colbert <sccolbert@gmail.com>
2011-11-21 11:19:49 -0600

    update code style of wx progress bar

--- enaml/widgets/wx/wx_progress_bar.py
+++ enaml/widgets/wx/wx_progress_bar.py
@@ -3,7 +3,9 @@
 #  All rights reserved.
 #------------------------------------------------------------------------------
 import wx
+
 from .wx_control import WXControl
+
 from ..progress_bar import AbstractTkProgressBar
 
 
@@ -23,23 +25,55 @@ class WXProgressBar(WXControl, AbstractTkProgressBar):
     def initialize(self):
         super(WXControl, self).initialize()
         shell = self.shell_obj
-        self.shell_minimum_changed(shell.minimum)
-        self.shell_maximum_changed(shell.maximum)
-        self.shell_value_changed(shell.value)
+        self._set_minimum(shell.minimum)
+        self._set_maximum(shell.maximum)
+        self._set_value(shell.value)
 
     #--------------------------------------------------------------------------
     # Abstract implementation methods
     #--------------------------------------------------------------------------
     def shell_value_changed(self, value):
+        """ The change handler for the 'value' attribute of the shell
+        object.
+
+        """
+        self._set_value(value)
+
+    def shell_minimum_changed(self, minimum):
+        """ The change handler for the 'minimum' attribute of the shell
+        object.
+
+        """
+        self._set_minimum(minimum)
+            
+    def shell_maximum_changed(self, maximum):
+        """ The change handler for the 'maximum' attribute of the shell
+        object
+
+        """
+        self._set_maximum(maximum)
+
+    #--------------------------------------------------------------------------
+    # Widget update  methods
+    #--------------------------------------------------------------------------
+    def _set_value(self, value):
+        """ Sets the value of the progress bar.
+
+        """
         gauge_value = value - self.shell_obj.minimum
         self.widget.SetValue(gauge_value)
 
-    def shell_minimum_changed(self, minimum):
+    def _set_minimum(self, minimum):
+        """ Sets the minimum value of the progress bar.
+
+        """
         gauge_range = self.shell_obj.maximum - minimum
         self.widget.SetRange(gauge_range)
+    
+    def _set_maximum(self, maximum):
+        """ Sets the maximum value of the progress bar.
 
-    def shell_maximum_changed(self, maximum):
+        """
         gauge_range = maximum - self.shell_obj.minimum
         self.widget.SetRange(gauge_range)
-
 
