54af07d6390f38627f963da4d57a1e7cc92f478d
Chris Colbert <sccolbert@gmail.com>
2011-11-23 17:56:32 -0500

    Punt on trying to support dynamic tab positions on wx. Smarter size hint.

--- enaml/widgets/wx/wx_tabbed.py
+++ enaml/widgets/wx/wx_tabbed.py
@@ -4,7 +4,7 @@
 #------------------------------------------------------------------------------
 import wx
 
-from .wx_container import WXContainer
+from .wx_stacked import WXStacked
 
 from ..tabbed import AbstractTkTabbed
 
@@ -18,10 +18,7 @@ _TAB_POSITION_MAP = {
 }
 
 
-# Notice we are not subclassing from WXStacked here because the internals
-# need to be handled drastically differently due to limitations of the
-# wxNotebook.
-class WXTabbed(WXContainer, AbstractTkTabbed):
+class WXTabbed(WXStacked, AbstractTkTabbed):
     """ A wx implementation of the Tabbed container.
 
     """
@@ -33,55 +30,28 @@ class WXTabbed(WXContainer, AbstractTkTabbed):
 
         """
         # Changing the tab position of the notebook dynamically is not
-        # supported by wx. So in order to do this we need to rebuild 
-        # the entire notebook whenever the orientation of the tabs change.
-        # So, we initially create a notebook here just our children will
-        # have a parent to use, but everything is really handled by the 
-        # _rebuild_nb method().
-        self.widget = wx.Notebook(self.parent_widget())
-
+        # supported by wx (the rendering gets all messed up). So, the
+        # tab position must be set at creation time.
+        style = _TAB_POSITION_MAP[self.shell_obj.tab_position]
+        self.widget = wx.Notebook(self.parent_widget(), style=style)
+    
     def bind(self):
         """ Bind to the events emitted by the underlying control.
 
         """
-        # Because of limitations in the wx.Notebook control and how it
-        # handles changing the orientation of the tab bar dynamically,
-        # we just rebuild it as the last part of the layout setup
-        # process.
         super(WXTabbed, self).bind()
-        self._rebuild_nb()
+        self.widget.Bind(wx.EVT_NOTEBOOK_PAGE_CHANGED, self._on_page_changed)
 
     #--------------------------------------------------------------------------
     # Implementation 
     #--------------------------------------------------------------------------
-    def shell_index_changed(self, index):
-        """ The change handler for the 'index' attribute of the shell 
-        object.
-
-        """
-        self._set_index(index)
-        self.shell_obj.size_hint_updated = True
-
-    def shell_children_changed(self, children):
-        """ Update the widget with new children.
-
-        """
-        # It's easiest to just rebuild the notebook
-        self._rebuild_nb()
-
-    def shell_children_items_changed(self, event):
-        """ Update the widget with new children.
-
-        """
-        # It's easiest to just rebuild the notebook
-        self._rebuild_nb()
-
     def shell_tab_position_changed(self, tab_position):
         """ The change handler for the 'tab_position' attribute of the
         shell object.
 
         """
-        self._set_tab_position(tab_position)
+        # Changing tab position dynamically on wx is not supported.
+        pass
 
     def size_hint(self):
         """ Returns a (width, height) tuple of integers which represent
@@ -89,10 +59,45 @@ class WXTabbed(WXContainer, AbstractTkTabbed):
         value is used by the layout manager to determine how much
         space to allocate the widget.
 
-        Override to add the content margins to the size hint.
+        """
+        width_hint, height_hint = super(WXTabbed, self).size_hint()
+
+        # Compute the size of the tab so we can offset the size hint.
+        # On Windows, the return value of the height function is a hard
+        # coded default of 20. This is close for the standard font but
+        # will probably break down with different fonts or icons. I've
+        # found no other way to measure the height of the tab bar.
+        tab_size = wx.RendererNative.Get().GetHeaderButtonHeight(self.widget)
+        tab_length = self._estimate_tab_bar_length()
+
+        # Offset the size hint by the tab bar size
+        if self.shell_obj.tab_position in ('top', 'bottom'):
+            height_hint += tab_size
+            width_hint = max(width_hint, tab_length)
+        else:
+            width_hint += tab_size
+            height_hint = max(height_hint, tab_length)
+
+        return (width_hint, height_hint)
+
+    def _estimate_tab_bar_length(self):
+        """ Estimates the length of the tab bar based on the title text
+        of the tab pages. There doesn't seem to be a better wx api for
+        getting this information. This is a rough estimate only that was
+        empirically determined on Windows 7 using default fonts and does
+        account for things like icons or multiline text.
 
         """
-        return (256, 192)
+        dc = wx.ClientDC(self.widget)
+        f = wx.SystemSettings.GetFont(wx.SYS_DEFAULT_GUI_FONT)
+        dc.SetFont(f)
+        length = 0
+        padding = 10
+        for child in self.shell_obj.children:
+            w, h = dc.GetTextExtent(child.title)
+            w = max(w, 35)
+            length += w + padding
+        return length
 
     #--------------------------------------------------------------------------
     # Event Handlers 
@@ -110,54 +115,21 @@ class WXTabbed(WXContainer, AbstractTkTabbed):
     #--------------------------------------------------------------------------
     # Widget Update Methods 
     #--------------------------------------------------------------------------
-    def _set_tab_position(self, tab_position):
-        """ Sets the position of the tabs on the underlying tab widget.
-
-        """        
-        # The notebook needs to be rebuilt to reliably change the tab
-        # position.
-        self._rebuild_nb()
-
-    def _set_index(self, index):
+    def set_index(self, index):
         """ Sets the current index of the tab widget. This is overridden
         from the parent class.
 
         """
         self.widget.SetSelection(index)
     
-    def _rebuild_nb(self):
-        """ Performs the actual create/initialize/bind of the widget.
+    def update_children(self):
+        """ Populates the notebook with the child notebook pages. This
+        is an overridden parent class method which sets the title of
+        of notebook pages properly.
 
         """
-        # First remove all the old pages from the existing notebook.
-        shell = self.shell_obj
-        old_widget = self.widget
-        while old_widget.GetPageCount():
-            old_widget.RemovePage(0)
+        super(WXTabbed, self).update_children()
+        widget = self.widget
+        for idx, child in enumerate(self.shell_obj.children):
+            widget.SetPageText(idx, child.title)
 
-        # Grab the position that tabs should be in (this is the 
-        # only style flag needed for the notebook) and create the 
-        # new notebook widget.
-        pos = _TAB_POSITION_MAP[shell.tab_position]
-        new_widget = wx.Notebook(self.parent_widget(), style=pos)
-
-        # Reparent all of the child widgets to the new notebook.
-        for child in shell.children:
-            child_widget = child.toolkit_widget
-            child_widget.Reparent(new_widget)
-            new_widget.AddPage(child_widget, child.title)
-
-        # Give the parent classes a chance to initialize and bind 
-        # the new notebook.
-        self.initialize()
-        super(WXTabbed, self).bind()
-
-        # Perform our necessary binding for the notebook changed handler.
-        new_widget.Bind(wx.EVT_NOTEBOOK_PAGE_CHANGED, self._on_page_changed)
-
-        # Finally, destory the old widget and switch to the current page.
-        self.widget = new_widget
-        old_widget.Destroy()
-        self._set_index(shell.index)
-        new_widget.Refresh()
-        shell.set_needs_update_constraints()
