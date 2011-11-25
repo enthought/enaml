71775fe0390a038c0a20758b877e04ea72f8ad80
Robert Kern <rkern@enthought.com>
2011-11-25 14:44:46 +0000

    BUG: Fix the fromlist for the Constructor __import__() statements.

--- enaml/widgets/qt/constructors.py
+++ enaml/widgets/qt/constructors.py
@@ -6,9 +6,8 @@ from ...toolkit import Constructor
 
 
 def importer(module_path, name):
-    fromlist = module_path.split('.')
     def _importer():
-        mod = __import__(module_path, fromlist=fromlist)
+        mod = __import__(module_path, fromlist=[name])
         try:
             res = getattr(mod, name)
         except AttributeError:
