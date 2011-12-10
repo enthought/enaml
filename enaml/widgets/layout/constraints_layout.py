2f60d80d03e4c670e33742cb55a76fb820d2baed
Chris Colbert <sccolbert@gmail.com>
2011-12-09 20:09:23 -0600

    add copyright header

--- enaml/widgets/layout/constraints_layout.py
+++ enaml/widgets/layout/constraints_layout.py
@@ -1,3 +1,7 @@
+#------------------------------------------------------------------------------
+#  Copyright (c) 2011, Enthought, Inc.
+#  All rights reserved.
+#------------------------------------------------------------------------------
 from collections import defaultdict
 import weakref
 
