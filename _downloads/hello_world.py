159c689f26424e3833defdd0300febce365529a4
Tim Diller <timdiller@gmail.com>
2011-12-20 17:34:22 -0600

    progress commit
    includes additions to tutorials and grammar references

---
+++ _downloads/hello_world.py
@@ -0,0 +1,11 @@
+#------------------------------------------------------------------------------
+#  Copyright (c) 2011, Enthought, Inc.
+#  All rights reserved.
+#------------------------------------------------------------------------------
+import enaml
+with enaml.imports():
+    from hello_world import MyMessageToTheWorld
+    
+view = MyMessageToTheWorld("Hello, world!")
+view.show()
+
