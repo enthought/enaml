e79cda0b4dc31c2f76d5b5a4d0402102bc3d79e0
Robert Kern <rkern@enthought.com>
2012-01-11 16:42:47 +0000

    BUG: Remove debugging print.

--- enaml/widgets/code_editor.py
+++ enaml/widgets/code_editor.py
@@ -54,7 +54,6 @@ class CodeEditor(TextEditor):
     def block_indent(self):
         """ Indent the selected lines of code.
         """
-        print 'calling indent'
         self.abstract_obj.block_indent()
     
     def block_unindent(self):
