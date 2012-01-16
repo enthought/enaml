d6a49b60f05b29730e738f4ee35af8bdb318ef81
Chris Colbert <sccolbert@gmail.com>
2012-01-16 03:50:05 -0500

    update QtMainWindow for MainWindow api updates

--- enaml/widgets/qt/qt_main_window.py
+++ enaml/widgets/qt/qt_main_window.py
@@ -2,7 +2,7 @@
 #  Copyright (c) 2011, Enthought, Inc.
 #  All rights reserved.
 #------------------------------------------------------------------------------
-from .qt_resizing_widgets import QResizingMainWindow
+from .qt import QtGui
 from .qt_window import QtWindow
 
 from ..main_window import AbstractTkMainWindow
@@ -19,9 +19,67 @@ class QtMainWindow(QtWindow, AbstractTkMainWindow):
         """ Creates the underlying QMainWindow object.
 
         """
-        self.widget = QResizingMainWindow(parent)
+        self.widget = QtGui.QMainWindow(parent)
 
-    # Note: On Qt a MenuBar that is defined as a child of a MainWindow
-    # will be automatically added as the menubar. There is no need
-    # to explicitly call setMenuBar(...).
+    def initialize(self):
+        """ Initialize the QMainWindow object.
+
+        """
+        super(QtMainWindow, self).initialize()
+        self.update_menu_bar()
+
+    #--------------------------------------------------------------------------
+    # Change Handlers
+    #--------------------------------------------------------------------------
+    def shell_menu_bar_changed(self, menu_bar):
+        """ Update the menu bar of the window with the new value from
+        the shell object.
+
+        """
+        self.update_menu_bar()
+
+    #--------------------------------------------------------------------------
+    # Widget Update Methods
+    #--------------------------------------------------------------------------
+    def update_menu_bar(self):
+        """ Updates the menu bar in the main window with the value
+        from the shell object.
+
+        """
+        menu_bar = self.shell_obj.menu_bar
+        if menu_bar is not None:
+            self.widget.setMenuBar(menu_bar.toolkit_widget)
+        else:
+            self.widget.setMenuBar(None)
+
+    def update_central_widget(self):
+        """ Updates the central widget in the main window with the 
+        value from the shell object.
+
+        """
+        # It's possible for the central widget component to be None.
+        # This must be allowed since the central widget may be generated
+        # by an Include component, in which case it will not exist 
+        # during initialization. However, we must have a central widget
+        # for the MainWindow, and in that case we just fill it with a
+        # dummy widget.
+        central_widget = self.shell_obj.central_widget
+        if central_widget is None:
+            child_widget = QtGui.QWidget()
+        else:
+            child_widget = central_widget.toolkit_widget
+        self.widget.setCentralWidget(child_widget)
+
+    def set_visible(self, visible):
+        """ Overridden from the parent class to raise the window to
+        the front if it should be shown.
+
+        """
+        widget = self.widget
+        if visible:
+            widget.setVisible(True)
+            widget.raise_()
+            widget.activateWindow()
+        else:
+            widget.setVisible(False)
 
