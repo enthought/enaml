dd7e819c21b5cf0b90f2c7e9cf08d438cc114687
Chris Colbert <sccolbert@gmail.com>
2012-01-16 03:54:09 -0500

    update QtContainer for Container api updates.

--- enaml/widgets/qt/qt_container.py
+++ enaml/widgets/qt/qt_container.py
@@ -11,10 +11,6 @@ from ..container import AbstractTkContainer
 class QtContainer(QtLayoutComponent, AbstractTkContainer):
     """ A Qt4 implementation of Container.
 
-    QtContainer is usually to be used as a base class for other container
-    widgets. However, it may also be used directly as an undecorated 
-    container for widgets for layout purposes.
-
     """
     def create(self, parent):
         """ Creates the underlying Qt widget.
@@ -36,34 +32,14 @@ class QtContainer(QtLayoutComponent, AbstractTkContainer):
         has been resized.
 
         """
-        # Notice that we are calling rearrange() here instead of 
-        # rearrange_later() since we want the layout to happen
+        # Notice that we are calling refresh() here instead of 
+        # request_refresh() since we want the refresh to happen
         # immediately. Otherwise the resize layouts will appear 
         # to lag in the ui. This is a safe operation since by the
         # time we get this resize event, the widget has already 
         # changed size. Further, the only geometry that gets set
         # by the layout manager is that of our children. And should
         # it be required to resize this widget from within the layout
-        # call, then the layout manager will do that via invoke_later.
-        self.shell_obj.rearrange()
-
-    def size_hint(self):
-        """ Returns a (width, height) tuple of integers which represent
-        the suggested size of the widget for its current state, ignoring
-        any windowing decorations. This value is used by the layout
-        manager to determine how much space to allocate the widget.
-
-        """
-        size_hint = self.widget.sizeHint()
-        return (size_hint.width(), size_hint.height())
-
-    def _reset_layout_margins(self):
-        """ Reset the layout margins for this widget.
-
-        """
-        super(QtContainer, self)._reset_layout_margins()
-        # Now reset our children's layout margins to account for the fact
-        # that they have recorded our own margins.
-        for child in self.shell_obj.layout_children:
-            child.abstract_obj._reset_layout_margins()
+        # call, then the layout manager will do that asynchronously.
+        self.shell_obj.refresh()
 
