70dcbbc53b7690fbd75fe734af8c33f4dda2e252
Chris Colbert <sccolbert@gmail.com>
2012-01-18 18:52:49 -0600

    Update the QtContainer so that the underlying QWidget pulls the size hint from the layout manager
    This helps unify a lot of code where we were manually computing the size hints
    based on the contents of child widgets since the child was a container. Now, the
    child will just return the right hint by default.

--- enaml/widgets/qt/qt_container.py
+++ enaml/widgets/qt/qt_container.py
@@ -2,10 +2,42 @@
 #  Copyright (c) 2011, Enthought, Inc.
 #  All rights reserved.
 #------------------------------------------------------------------------------
+import weakref
+
+from .qt import QtCore
 from .qt_layout_component import QtLayoutComponent
 from .qt_resizing_widgets import QResizingFrame, QResizingWidget
 
 from ..container import AbstractTkContainer
+
+
+class _QResizingFrame(QResizingFrame):
+    """ A subclass of QResizingFrame which calls back onto the shell
+    Container to get the size hint. If that size hint is invalid, it
+    falls back onto the Qt default.
+
+    """
+    def __init__(self, qt_container, *args, **kwargs):
+        super(QResizingFrame, self).__init__(*args, **kwargs)
+        self.qt_container = weakref.ref(qt_container)
+    
+    def sizeHint(self):
+        """ Computes the size hint from the given Container, falling
+        back on the default size hint computation if the Container 
+        returns one that is invalid.
+
+        """
+        res = None
+        qt_container = self.qt_container()
+        if qt_container is not None:
+            shell = qt_container.shell_obj
+            if shell is not None:
+                sh = shell.size_hint()
+                if sh != (-1, -1):
+                    res = QtCore.QSize(*sh)
+        if res is None:
+            res = super(_QResizingFrame, self).sizeHint()
+        return res
 
 
 class QtContainer(QtLayoutComponent, AbstractTkContainer):
@@ -16,8 +48,8 @@ class QtContainer(QtLayoutComponent, AbstractTkContainer):
         """ Creates the underlying Qt widget.
 
         """
-        self.widget = QResizingFrame(parent)
-    
+        self.widget = _QResizingFrame(self, parent)
+
     def bind(self):
         """ Binds the signal handlers for the widget.
 
