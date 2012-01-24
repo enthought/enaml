98d4359414030d2d5e479b7b8fd7d75f396e0615
Tim Diller <timdiller@gmail.com>
2012-01-24 16:18:58 -0600

    docs copyright year update

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -63,7 +63,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'enaml'
-copyright = u'2011, Enthought, Inc.'
+copyright = u'2012, Enthought, Inc.'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
