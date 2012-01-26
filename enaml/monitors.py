ea55bd62e06e14e26f284da2ff50540f9242fa71
Chris Colbert <sccolbert@gmail.com>
2012-01-25 20:29:28 -0600

    Update Monitors to add trait monitors for attributes and getattr

--- enaml/monitors.py
+++ enaml/monitors.py
@@ -19,8 +19,6 @@ class NotificationEvent(object):
     metadata that the emitter wishes to pass to the receiver.
 
     """
-    __slots__ = ('emitter', 'metadata')
-
     def __init__(self, emitter, **metadata):
         """ Initialize a notifier event.
 
@@ -43,7 +41,7 @@ class WeakSentinel(object):
     notifiers when a reset is requested.
 
     """
-    __slots__ = ('__weakref__')
+    __slots__ = ('__weakref__',)
 
 
 class NotifierProxy(object):
@@ -52,8 +50,6 @@ class NotifierProxy(object):
     callback.
 
     """
-    __slots__ = ('_callback', '_sentinel')
-
     def __init__(self, callback):
         """ Initialize a WeakNotifier.
 
@@ -66,7 +62,7 @@ class NotifierProxy(object):
 
         """
         self._callback = callback
-        self._sentinel = weakref.ref(WeakSentinel())
+        self._sentinel = WeakSentinel()
     
     def reset(self):
         """ Reset this proxy so that any existing notifiers drop their
@@ -75,7 +71,7 @@ class NotifierProxy(object):
         subscriber.
 
         """
-        self._sentinel = weakref.ref(WeakSentinel())
+        self._sentinel = WeakSentinel()
 
     def notify(self, notifier_event):
         """ Notify the external subscriber that an event has occurred.
@@ -90,7 +86,7 @@ class NotifierProxy(object):
         self._callback(notifier_event)
 
     def disconnector(self):
-        """ Returns an internal object which is stored weakly. This 
+        """ Returns an object to be used to disconnect notifiers. This 
         object is destroyed whenever a reset of the proxy is requested. 
         Consumers of this notifier should use a weak reference to this 
         object to connect a callback which will destroy any notifiers 
@@ -213,7 +209,7 @@ class AbstractAttributeMonitor(AbstractMonitor):
         # new bytecode for the expression.
         insertion_code = []
 
-        for idx, (op, op_arg) in code_list:
+        for idx, (op, op_arg) in enumerate(code_list):
             # This bit of code is injected between the object on TOS
             # and its pending attribute access. The TOS obj is duped,
             # the rotated above the binder code. The attr is loaded,
@@ -251,7 +247,7 @@ class AbstractAttributeMonitor(AbstractMonitor):
         """
         raise NotImplementedError
 
-
+    
 #------------------------------------------------------------------------------
 # Abstract Call Monitor
 #------------------------------------------------------------------------------
@@ -297,20 +293,23 @@ class AbstractCallMonitor(AbstractMonitor):
         # A weak closure which is injected into the stack to call the
         # monitor method on function call.
         def code_binder(func_obj, arg_tuple, arg_spec):
-            if callable(func_obj):
-                this = wr_self()
-                if this is not None:
-                    nargs = arg_spec & 0xFF
-                    args = arg_tuple[:nargs]
-                    kwargs = dict(zip(arg_tuple[nargs:], arg_tuple[nargs+1:]))
-                    this.monitor_function(func_obj, args, kwargs)
-            return (func_obj, arg_tuple)
+            this = wr_self()
+            if this is not None:
+                nargs = arg_spec & 0xFF
+                args = arg_tuple[:nargs]
+                kwargs = dict(zip(arg_tuple[nargs:], arg_tuple[nargs+1:]))
+                this.monitor_function(func_obj, args, kwargs)
+            # The UNPACK_SEQUENCE op_codes which will unpack these 
+            # return values will unpack things onto the stack in the
+            # reverse of how they are provided. So, we pre-reverse them
+            # so they come out in the right order.
+            return (tuple(reversed(arg_tuple)), func_obj)
                 
         # The list of code segments that will be inserted into the
         # new bytecode for the expression.
         insertion_code = []
 
-        for idx, (op, op_arg) in code_list:
+        for idx, (op, op_arg) in enumerate(code_list):
             # This bit of code is injected just before a function call
             # is performed. The arguments on the stack are packed into
             # tuple. The code binder is then pushed onto the stack and
@@ -365,34 +364,123 @@ class AbstractCallMonitor(AbstractMonitor):
 
 
 #------------------------------------------------------------------------------
+# Trait Notification Handler
+#------------------------------------------------------------------------------
+class TraitNotificationHandler(object):
+    """ A thin class which makes it easier to manage the lifetime of 
+    a trait notifier.
+
+    """
+    __slots__ = ('notifier', '__weakref__')
+
+    def __init__(self, obj, attr, notifier):
+        """ Initialize a TraitNotificationHandler.
+
+        Parameters
+        ----------
+        obj : HasTraits
+            The HasTraits instance on which we are attaching a listener.
+        
+        attr : string
+            The trait attribute on the object to which we should listen.
+
+        notifier : NotifierProxy
+            The NotifierProxy instance to notify with an event when the
+            underlying trait attribute changes.
+
+        """
+        self.notifier = notifier
+        obj.on_trait_change(self.notify, attr)
+
+    def notify(self, obj, name, old, new):
+        """ The trait change callback which emits an event to the 
+        NotifierProxy when the trait attribute changes.
+
+        """
+        event = NotificationEvent(self, obj=obj, name=name, old=old, new=new)
+        self.notifier.notify(event)
+
+
+#------------------------------------------------------------------------------
+# Handler Mixin
+#------------------------------------------------------------------------------
+class HandlerMixin(object):
+    """ A mixin class desiged to provide common handler functionality
+    for the monitor classes below.
+
+    """
+    __metaclass__ = ABCMeta
+
+    def __init__(self, *args, **kwargs):
+        super(HandlerMixin, self).__init__(*args, **kwargs)
+        # A dictionary which holds the notification handlers. A key
+        # in the dictionary is an (obj_id, attr) tuple and the value
+        # is the notification handler for that pair. The object id 
+        # is used to avoid ref cycles and potential hashing issues.
+        self._handlers = {}
+        
+        # Hookup the weak ref to the disconnector object so that the
+        # handlers are cleaned up with the notifier is reset.
+        self._hookup()
+
+    def _hookup(self):
+        """ Creates a weakref to the current disconnector object. This
+        created ref will call the disconnect method when the underlying
+        object dies.
+
+        """
+        disconnector = self.notifier.disconnector()
+        self._disconnector_ref = weakref.ref(disconnector, self._disconnect)
+
+    def _disconnect(self, ref):
+        """ The disconnect callback which is fired when the sentinel 
+        object is destroyed. This clears the existing handlers and 
+        hooks up a new disconnector ref.
+
+        """
+        self._handlers.clear()
+        self._hookup()
+
+
+#------------------------------------------------------------------------------
+# Trait Handler Mixin
+#------------------------------------------------------------------------------
+class TraitAttributeMixin(HandlerMixin):
+    """ A HandlerMixin subclass which adds the common bind code for the
+    trait monitor classes below.
+
+    """
+    def do_binding(self, obj, attr):
+        """ Hooks up a notifier to the object attribute pair if the 
+        object is a HasTraits instance and the attribute refers to 
+        a valid trait on the object.
+
+        """
+        if isinstance(obj, HasTraits):
+            # Only hook up a notifier if the attribute access refers
+            # to a proper trait. We check for Disallow trait types 
+            # since those can be returned by instances of HasStrictTraits
+            trait = obj.trait(attr)
+            if trait is not None and trait.trait_type is not Disallow:
+                # Don't hook up multiple identifiers to the same object
+                # attribute pair. Use the id of the object to prevent
+                # keeping an accidental strong ref to the object.
+                notifier = self.notifier
+                handlers = self._handlers
+                key = (id(obj), attr)
+                if key not in handlers:
+                    handler = TraitNotificationHandler(obj, attr, notifier)
+                    handlers[key] = handler
+
+
+#------------------------------------------------------------------------------
 # Trait Attribute Monitor
 #------------------------------------------------------------------------------
-class TraitAttributeMonitor(AbstractAttributeMonitor):
+class TraitAttributeMonitor(TraitAttributeMixin, AbstractAttributeMonitor):
     """ An AbstractAttributeMonitor implementation which binds listeners
     to trait attributes on HasTraits classes.
 
     """
-    def __init__(self, *args, **kwargs):
-        super(TraitAttributeMonitor, self).__init__(*args, **kwargs)
-
-        # When passing a bound method to on_trait_change, traits will
-        # ignore the 'target' argument and instead build a weak ref
-        # to the bound method. To avoid that, we create this weak
-        # closure which will call the notifier for us, but it itself 
-        # not a bound method and so can be disconnected with a 'target'.
-        wr_self = weakref.ref(self)
-        def handler(obj, name, old, new):
-            this = wr_self()
-            if this is not None:
-                event = NotificationEvent(
-                    self, obj=obj, name=name, old=old, new=new,
-                )
-                this.notifier.notify(event)
-        
-        # The notification handler that is used for binding any trait
-        # change handlers necessary for the expression.
-        self.notification_handler = handler
-
     def monitor_attribute(self, obj, attr):
         """ Hooks up any necessary trait change notifiers for the given
         object and attribute. 
@@ -407,47 +495,18 @@ class TraitAttributeMonitor(AbstractAttributeMonitor):
             object.
         
         """
-        if isinstance(obj, HasTraits):
-            # Only hook up a notifier if the attribute access refers
-            # to a proper trait. We check for Disallow trait types 
-            # since those can be returned by instances of HasStrictTraits
-            trait = obj.trait(attr)
-            if trait is not None and trait.trait_type is not Disallow:
-                handler = self.notification_handler
-                target = self.notifier.disconnector()
-                obj.on_trait_change(handler, attr, target=target)
+        self.do_binding(obj, attr)
 
 
 #------------------------------------------------------------------------------
 # Trait Getattr Monitor
 #------------------------------------------------------------------------------
-class TraitGetattrMonitor(AbstractCallMonitor):
+class TraitGetattrMonitor(TraitAttributeMixin, AbstractCallMonitor):
     """ An AbstractCallMonitor implementation which binds listeners
     to trait attributes on HasTraits classes which are accessed via
     getattr.
 
     """
-    def __init__(self, *args, **kwargs):
-        super(TraitGetattrMonitor, self).__init__(*args, **kwargs)
-
-        # When passing a bound method to on_trait_change, traits will
-        # ignore the 'target' argument and instead build a weak ref
-        # to the bound method. To avoid that, we create this weak
-        # closure which will call the notifier for us, but it itself 
-        # not a bound method and so can be disconnected with a 'target'.
-        wr_self = weakref.ref(self)
-        def handler(obj, name, old, new):
-            this = wr_self()
-            if this is not None:
-                event = NotificationEvent(
-                    self, obj=obj, name=name, old=old, new=new,
-                )
-                this.notifier.notify(event)
-        
-        # The notification handler that is used for binding any trait
-        # change handlers necessary for the expression.
-        self.notification_handler = handler
-
     def monitor_function(self, func_obj, args, kwargs):
         """ Hooks up any necessary trait change notifiers for the given
         call to getattr
@@ -474,13 +533,5 @@ class TraitGetattrMonitor(AbstractCallMonitor):
         if not isinstance(attr, basestring):
             return
 
-        if isinstance(obj, HasTraits):
-            # Only hook up a notifier if the attribute access refers
-            # to a proper trait. We check for Disallow trait types 
-            # since those can be returned by instances of HasStrictTraits
-            trait = obj.trait(attr)
-            if trait is not None and trait.trait_type is not Disallow:
-                handler = self.notification_handler
-                target = self.notifier.disconnector()
-                obj.on_trait_change(handler, attr, target=target)
+        self.do_binding(obj, attr)
 
