329afd86ffd6218c7a1a73c5ea06b5ee6667ffbf
Chris Colbert <sccolbert@gmail.com>
2012-01-27 15:57:29 -0600

    update the location of the default toolkit factory functions

--- enaml/__init__.py
+++ enaml/__init__.py
@@ -2,16 +2,88 @@
 #  Copyright (c) 2011, Enthought, Inc.
 #  All rights reserved.
 #------------------------------------------------------------------------------
-from .core.import_hooks import imports
+import os
+import sys
 
 
+#------------------------------------------------------------------------------
+# Import Helper
+#------------------------------------------------------------------------------
+def imports():
+    """ Lazily imports and returns an enaml imports context.
+
+    """
+    from .core.import_hooks import imports
+    return imports()
+
+
+#------------------------------------------------------------------------------
+# Toolkit Factory Functions
+#------------------------------------------------------------------------------
+def default_toolkit():
+    """ Creates an returns the default toolkit object based on
+    the user's current ETS_TOOLKIT environment variables.
+
+    """
+    toolkit = os.environ.get('ETS_TOOLKIT', 'qt').lower()
+
+    if toolkit == 'qt' or toolkit == 'qt4':
+        return qt_toolkit()
+
+    if toolkit == 'wx':
+        return wx_toolkit()
+
+    raise ValueError('Invalid Toolkit: %s' % toolkit)
+
+
+def qt_toolkit():
+    """ Creates and return a toolkit object for the Qt backend.
+
+    """
+    from .core.operators import OPERATORS
+    from .core.toolkit import Toolkit
+    from .widgets.constructors import CONSTRUCTORS
+    from .widgets.layout.layout_helpers import LAYOUT_HELPERS
+    from .widgets.qt.constructors import QT_CONSTRUCTORS
+    from .widgets.qt.qt_application import QtApplication
+
+    toolkit = Toolkit(QT_CONSTRUCTORS)
+    toolkit.update(CONSTRUCTORS)
+    toolkit.update(OPERATORS)
+    toolkit.update(LAYOUT_HELPERS)
+    toolkit.app = QtApplication()
+
+    return toolkit
+
+
+def wx_toolkit():
+    """ Creates and return a toolkit object for the Wx backend.
+
+    """
+    from .core.toolkit import Toolkit
+    from .operators import OPERATORS
+    from .widgets.constructors import CONSTRUCTORS
+    from .widgets.layout.layout_helpers import LAYOUT_HELPERS
+    from .widgets.wx.constructors import WX_CONSTRUCTORS
+    from .widgets.wx.wx_application import WXApplication
+
+    toolkit = Toolkit(WX_CONSTRUCTORS)
+    toolkit.update(CONSTRUCTORS)
+    toolkit.update(OPERATORS)
+    toolkit.update(LAYOUT_HELPERS)
+    toolkit.app = WXApplication()
+
+    return toolkit
+
+
+#------------------------------------------------------------------------------
+# Test Helpers
+#------------------------------------------------------------------------------
 def test_collector():
     """ Discover and collect tests for the Enaml Package.
 
         .. note :: addapted from the unittest2
     """
-    import os
-    import sys
     from unittest import TestLoader
 
     # import __main__ triggers code re-execution
