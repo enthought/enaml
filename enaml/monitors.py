83f4a03ac81b16e27f37cb95750fdb77514e251a
Chris Colbert <sccolbert@gmail.com>
2012-01-26 23:55:39 -0600

    update monitors to use the new signaling module to signal changes to the expression.

--- enaml/monitors.py
+++ enaml/monitors.py
@@ -3,97 +3,12 @@
 #  All rights reserved.
 #------------------------------------------------------------------------------
 from abc import ABCMeta, abstractmethod
-import weakref
+from weakref import ref
 
 from traits.api import HasTraits, Disallow
 
 from .parsing import byteplay as bp
-
-
-#------------------------------------------------------------------------------
-# Notification Handling
-#------------------------------------------------------------------------------
-class NotificationEvent(object):
-    """ An event object passed to the callback of a NotifierProxy, it
-    contains information about the emitter of the notification and any
-    metadata that the emitter wishes to pass to the receiver.
-
-    """
-    def __init__(self, emitter, **metadata):
-        """ Initialize a notifier event.
-
-        Parameters
-        ----------
-        emitter : object
-            The object which emitted this event.
-        
-        **metadata
-            Any other information that the emitter wishes to make 
-            available to the receiver.
-
-        """
-        self.emitter = emitter
-        self.metadata = metadata
-
-
-class WeakSentinel(object):
-    """ An object which is simply weak-refable but serves to disconnect
-    notifiers when a reset is requested.
-
-    """
-    __slots__ = ('__weakref__',)
-
-
-class NotifierProxy(object):
-    """ An object which allows a strong callback to be registered weakly
-    with notifiers, without the notifiers requiring knowledge of the
-    callback.
-
-    """
-    def __init__(self, callback):
-        """ Initialize a WeakNotifier.
-
-        Parameters
-        ----------
-        callback : callable
-            A callable which takes a single argument, a NotifierEvent, 
-            and which is called whenever any of its notifiers change.
-            A strong reference is maintained to the callback.
-
-        """
-        self._callback = callback
-        self._sentinel = WeakSentinel()
-    
-    def reset(self):
-        """ Reset this proxy so that any existing notifiers drop their
-        connections and no longer trigger notifications until the next 
-        time they are connected. This will typically be called by the
-        subscriber.
-
-        """
-        self._sentinel = WeakSentinel()
-
-    def notify(self, notifier_event):
-        """ Notify the external subscriber that an event has occurred.
-        This will typically be called by the notifiers.
-
-        Parameters
-        ----------
-        notifier_event : NotificationEvent
-            The NotificationEvent instance to pass to the subscriber.
-
-        """
-        self._callback(notifier_event)
-
-    def disconnector(self):
-        """ Returns an object to be used to disconnect notifiers. This 
-        object is destroyed whenever a reset of the proxy is requested. 
-        Consumers of this notifier should use a weak reference to this 
-        object to connect a callback which will destroy any notifiers 
-        they may have previously created when the disconnector dies.
-
-        """
-        return self._sentinel
+from .signaling import Signal
 
 
 #------------------------------------------------------------------------------
@@ -113,17 +28,8 @@ class AbstractMonitor(object):
     """
     __metaclass__ = ABCMeta
 
-    def __init__(self, notifier):
-        """ Initialize a monitor instance.
-
-        Parameters
-        ----------
-        notifier : NotifierProxy
-            A NotifierProxy object which should be used to connect any 
-            necessary notifiers of the monitor.
-
-        """
-        self.notifier = notifier
+    #: A signal which is emitted when the expression has changed.
+    expression_changed = Signal()
 
     @abstractmethod
     def get_insertion_code(self, code_list):
@@ -151,6 +57,14 @@ class AbstractMonitor(object):
             leave the stack exactly the way it found it. If this is not
             maintained, then random exceptions and/or crashes *will*
             result.
+
+        """
+        raise NotImplementedError
+
+    @abstractmethod
+    def reset(self):
+        """ Unhook any previously connected notifiers. This method is 
+        called by the owner expression when notifiers should be removed.
 
         """
         raise NotImplementedError
@@ -194,14 +108,10 @@ class AbstractAttributeMonitor(AbstractMonitor):
             result.
 
         """
-        # A weakref to this instance which is used by the closure
-        # injected into the bytecode for this monitor.
-        wr_self = weakref.ref(self)
-
         # A weak closure which is injected into the stack to call the
         # monitor method upon attribute access.
-        def code_binder(obj, attr):
-            this = wr_self()
+        def code_binder(obj, attr, selfref=ref(self)):
+            this = selfref()
             if this is not None:
                 this.monitor_attribute(obj, attr)
         
@@ -286,14 +196,10 @@ class AbstractCallMonitor(AbstractMonitor):
             result.
 
         """
-        # A weakref to this instance which is used by the closures
-        # injected into the bytecode for this monitor.
-        wr_self = weakref.ref(self)
-
         # A weak closure which is injected into the stack to call the
         # monitor method on function call.
-        def code_binder(func_obj, arg_tuple, arg_spec):
-            this = wr_self()
+        def code_binder(func_obj, arg_tuple, arg_spec, selfref=ref(self)):
+            this = selfref()
             if this is not None:
                 nargs = arg_spec & 0xFF
                 args = arg_tuple[:nargs]
@@ -371,9 +277,9 @@ class TraitNotificationHandler(object):
     a trait notifier.
 
     """
-    __slots__ = ('notifier', '__weakref__')
+    __slots__ = ('_parent_ref', '__weakref__')
 
-    def __init__(self, obj, attr, notifier):
+    def __init__(self, parent, obj, attr):
         """ Initialize a TraitNotificationHandler.
 
         Parameters
@@ -389,67 +295,42 @@ class TraitNotificationHandler(object):
             underlying trait attribute changes.
 
         """
-        self.notifier = notifier
+        self._parent_ref = ref(parent)
         obj.on_trait_change(self.notify, attr)
 
-    def notify(self, obj, name, old, new):
+    def notify(self):
         """ The trait change callback which emits an event to the 
         NotifierProxy when the trait attribute changes.
 
         """
-        event = NotificationEvent(self, obj=obj, name=name, old=old, new=new)
-        self.notifier.notify(event)
-
-
-#------------------------------------------------------------------------------
-# Handler Mixin
-#------------------------------------------------------------------------------
-class HandlerMixin(object):
-    """ A mixin class desiged to provide common handler functionality
-    for the monitor classes below.
-
-    """
-    __metaclass__ = ABCMeta
-
-    def __init__(self, *args, **kwargs):
-        super(HandlerMixin, self).__init__(*args, **kwargs)
-        # A dictionary which holds the notification handlers. A key
-        # in the dictionary is an (obj_id, attr) tuple and the value
-        # is the notification handler for that pair. The object id 
-        # is used to avoid ref cycles and potential hashing issues.
-        self._handlers = {}
-        
-        # Hookup the weak ref to the disconnector object so that the
-        # handlers are cleaned up with the notifier is reset.
-        self._hookup()
-
-    def _hookup(self):
-        """ Creates a weakref to the current disconnector object. This
-        created ref will call the disconnect method when the underlying
-        object dies.
-
-        """
-        disconnector = self.notifier.disconnector()
-        self._disconnector_ref = weakref.ref(disconnector, self._disconnect)
-
-    def _disconnect(self, ref):
-        """ The disconnect callback which is fired when the sentinel 
-        object is destroyed. This clears the existing handlers and 
-        hooks up a new disconnector ref.
-
-        """
-        self._handlers.clear()
-        self._hookup()
+        parent = self._parent_ref()
+        if parent is not None:
+            parent.expression_changed()
 
 
 #------------------------------------------------------------------------------
 # Trait Handler Mixin
 #------------------------------------------------------------------------------
-class TraitAttributeMixin(HandlerMixin):
-    """ A HandlerMixin subclass which adds the common bind code for the
-    trait monitor classes below.
+class TraitHandlerMixin(object):
+    """ A mixin class which adds the common binding code for the trait
+    monitor classes below.
 
     """
+    def __init__(self, *args, **kwargs):
+        super(TraitHandlerMixin, self).__init__(*args, **kwargs)
+        # A dictionary which holds the notification handlers. A key
+        # in the dictionary is an (obj_id, attr) tuple and the value
+        # is the notification handler for that pair. The object id 
+        # is used to avoid ref cycles and potential hashing issues.
+        self._handlers = {}
+
+    def reset(self):
+        """ Clears the existing handlers which results in the notifiers
+        being destroyed.
+
+        """
+        self._handlers.clear()
+
     def do_binding(self, obj, attr):
         """ Hooks up a notifier to the object attribute pair if the 
         object is a HasTraits instance and the attribute refers to 
@@ -465,18 +346,17 @@ class TraitAttributeMixin(HandlerMixin):
                 # Don't hook up multiple identifiers to the same object
                 # attribute pair. Use the id of the object to prevent
                 # keeping an accidental strong ref to the object.
-                notifier = self.notifier
                 handlers = self._handlers
                 key = (id(obj), attr)
                 if key not in handlers:
-                    handler = TraitNotificationHandler(obj, attr, notifier)
+                    handler = TraitNotificationHandler(self, obj, attr)
                     handlers[key] = handler
 
 
 #------------------------------------------------------------------------------
 # Trait Attribute Monitor
 #------------------------------------------------------------------------------
-class TraitAttributeMonitor(TraitAttributeMixin, AbstractAttributeMonitor):
+class TraitAttributeMonitor(TraitHandlerMixin, AbstractAttributeMonitor):
     """ An AbstractAttributeMonitor implementation which binds listeners
     to trait attributes on HasTraits classes.
 
@@ -501,7 +381,7 @@ class TraitAttributeMonitor(TraitAttributeMixin, AbstractAttributeMonitor):
 #------------------------------------------------------------------------------
 # Trait Getattr Monitor
 #------------------------------------------------------------------------------
-class TraitGetattrMonitor(TraitAttributeMixin, AbstractCallMonitor):
+class TraitGetattrMonitor(TraitHandlerMixin, AbstractCallMonitor):
     """ An AbstractCallMonitor implementation which binds listeners
     to trait attributes on HasTraits classes which are accessed via
     getattr.
