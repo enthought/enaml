b7aca6d1b691aa1d32d45f8d805b8a81b194d1d3
Ilan Schnell <ischnell@enthought.com>
2012-02-09 21:57:34 -0600

    REL: master now 0.1.1

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ except ImportError:
 
 setup(
     name='enaml',
-    version='0.1',
+    version='0.1.1',
     author='Enthought, Inc',
     author_email='info@enthought.com',
     url='https://github.com/enthought/enaml',
