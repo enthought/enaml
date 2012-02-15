3db956f32e8fb3afaa1870b6a48986f586d03008
Deepankar Sharma <deepankarsharma@gmail.com>
2012-02-15 09:27:58 -0600

    Fixed per layer opacity.

--- live_plotting/composite.py
+++ live_plotting/composite.py
@@ -1,13 +1,11 @@
 #!/usr/bin/env python
 import sys
-import math
-from PySide import QtCore, QtGui, QtOpenGL
+from PySide import QtGui, QtOpenGL
 from PySide.QtOpenGL import QGLShader, QGLShaderProgram
 import Image
 import numpy as np
-import time
 try:
-    from OpenGL import GL, GLUT, GLU
+    from OpenGL import GL, GLU
 except ImportError:
     app = QtGui.QApplication(sys.argv)
     QtGui.QMessageBox.critical(None, "OpenGL hellogl",
@@ -33,7 +31,7 @@ uniform float opacity;
 void main(void)
 {
     vec4 color = texture2D(image ,gl_TexCoord[0].st);
-    //color.a *= opacity;
+    color.a *= opacity;
     gl_FragColor = color;
 }
 '''
@@ -52,11 +50,8 @@ class Texture(object):
 class Layer(object):
     def __init__(self, x, y, texture, level=0, texture_format=GL.GL_RGBA, x_offset=0, y_offset=0, opacity=1.0):
         self.x, self.y = x, y
-        t1 = time.time()
         self.id = GL.glGenTextures(1)
         self.upload(texture, level=0, texture_format=GL.GL_RGBA, x_offset=0, y_offset=0)
-        t2 = time.time()
-        print 'texture_upload', t2 - t1
         self.opacity = opacity
 
     def upload(self, texture, level=0, texture_format=GL.GL_RGBA, x_offset=0, y_offset=0):
@@ -72,16 +67,7 @@ class Layer(object):
         GL.glTexSubImage2D(GL.GL_TEXTURE_2D, level, x_offset, y_offset, texture.width, texture.height, texture.format, texture.pixel_type, texture.buffer)
 
     def render(self, width, height, program, uniforms):
-        #program.setUniformValue('uniforms['opacity']', self.opacity)
-        GL.glMatrixMode(GL.GL_PROJECTION)
-        GL.glPushMatrix()
-        GL.glLoadIdentity()
-
-        GLU.gluOrtho2D(0, width, height, 0)
-        GL.glMatrixMode(GL.GL_MODELVIEW)
-        GL.glPushMatrix()
-
-        GL.glLoadIdentity()
+        GL.glUniform1f(uniforms['opacity'], self.opacity)
         GL.glActiveTexture(GL.GL_TEXTURE0)
         GL.glBindTexture(GL.GL_TEXTURE_2D, self.id)
 
@@ -101,17 +87,11 @@ class Layer(object):
 
         GL.glEnd()
 
-        GL.glMatrixMode(GL.GL_MODELVIEW)
-        GL.glPopMatrix()
 
-        GL.glMatrixMode(GL.GL_PROJECTION)
-        GL.glPopMatrix()
-
-
-def make_layer(filename, x, y, in_format=GL.GL_RGBA, out_format=GL.GL_RGBA):
+def make_layer(filename, x, y, in_format=GL.GL_RGBA, out_format=GL.GL_RGBA, opacity=1.0):
     image = np.asarray(Image.open(filename))
     texture = Texture(image.shape[0], image.shape[1], image, format=in_format)
-    layer = Layer(x, y, texture=texture, texture_format=out_format)
+    layer = Layer(x, y, texture=texture, texture_format=out_format, opacity=opacity)
     return layer
 
 
@@ -122,22 +102,20 @@ class GLWidget(QtOpenGL.QGLWidget):
     def initializeGL(self):
         GL.glDisable(GL.GL_DEPTH_TEST)
         GL.glDisable(GL.GL_CULL_FACE)
-        GL.glDisable (GL.GL_STENCIL_TEST)
+        GL.glDisable(GL.GL_STENCIL_TEST)
         GL.glEnable(GL.GL_BLEND)
         GL.glBlendEquation(GL.GL_FUNC_ADD)
         GL.glBlendFunc(GL.GL_SRC_ALPHA, GL.GL_ONE_MINUS_SRC_ALPHA)
         self.initShaders()
         self.layers = []
-        for i in range(30):
-            self.layers.append(make_layer('layer_one.png', 0, 0))
-        self.layers.append(make_layer('layer_two.png', 50, 0))
+        self.layers.append(make_layer('layer_one.png', 0, 0))
+        self.layers.append(make_layer('layer_two.png', 50, 0, opacity=0.5))
         GL.glClearColor(0.0, 0.0, 0.0, 0.0)
         GL.glShadeModel(GL.GL_SMOOTH)
         GL.glClearColor(1, 0.5, 0.5, 1.0)
         self.orientCamera()
 
     def paintGL(self):
-        t1 = time.time()
         GL.glClear(GL.GL_COLOR_BUFFER_BIT | GL.GL_DEPTH_BUFFER_BIT)
         self.program.bind()
         uniforms = {}
@@ -145,25 +123,16 @@ class GLWidget(QtOpenGL.QGLWidget):
         for layer in self.layers:
             layer.render(self.width(), self.height(), self.program, uniforms)
         self.program.release()
-        t2 = time.time()
-        print 'draw_call', t2 - t1
-
-    def paintTeapot(self):
-        self.program.bind()
-        GL.glPushAttrib(GL.GL_POLYGON_BIT)
-        GL.glFrontFace(GL.GL_CW)
-        GL.glColor3f(0.2, 0.2, 0.5)
-        GLUT.glutSolidTeapot(3.0)
-        self.program.release()
-        GL.glPopAttrib()
 
     def orientCamera(self):
-        return
-        GL.glPushAttrib(GL.GL_TRANSFORM_BIT)
         GL.glMatrixMode(GL.GL_PROJECTION)
+        GL.glPushMatrix()
         GL.glLoadIdentity()
+
         GLU.gluOrtho2D(0, self.width(), self.height(), 0)
-        GL.glPopAttrib()
+        GL.glMatrixMode(GL.GL_MODELVIEW)
+        GL.glPushMatrix()
+        GL.glLoadIdentity()
 
     def resizeGL(self, width, height):
         GL.glViewport(0, 0, width, height)
