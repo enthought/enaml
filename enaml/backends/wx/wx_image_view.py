c99b945c007f862731013ca13bdbca591b2df8e5
Chris Colbert <sccolbert@gmail.com>
2012-03-03 17:20:12 -0600

    document and up wx image view

--- enaml/backends/wx/wx_image_view.py
+++ enaml/backends/wx/wx_image_view.py
@@ -9,39 +9,163 @@ from .wx_control import WXControl
 from ...components.image_view import AbstractTkImageView
 
 
-class wxImageWidget(wx.Panel):
+class wxBitmapWidget(wx.Panel):
+    """ A wx.Panel subclass which paints a provided wx.Bitmap. 
 
+    This differs from wx.StaticBitmap in that it provides the option to
+    scale the provided bitmap to the bounds of the widget. If the widget
+    is set to scale its contents, low quality scaling will occur during
+    resize, with a high quality pass performed once resizing as finished.
+
+    """
     def __init__(self, parent):
-        super(wxImageWidget, self).__init__(parent)
+        """ Initialize a wxBitmapWidget.
+
+        Parameters
+        ----------
+        parent : wx.Window
+            The wx.Window object which serves as the widget parent.
+        
+        """
+        super(wxBitmapWidget, self).__init__(parent)
         self._bitmap = None
+        self._timer = None
         self._scaled_contents = False
         self.Bind(wx.EVT_PAINT, self.OnPaint)
 
+    #--------------------------------------------------------------------------
+    # Private API
+    #--------------------------------------------------------------------------
+    def PaintBitmap(self, dc, quality):
+        """ Paints the underlying bitmap into the given dc with the given
+        quality.
+
+        Parameters
+        ----------
+        dc : wx.DC
+            The device context in which to draw the bitmap.
+        
+        quality : wx.IMAGE_QUALITY_NORMAL or wx.IMAGE_QUALITY_HIGH
+            The quality with which to scale the image should scaling
+            be required.
+
+        """
+        bmp = self._bitmap
+        if bmp is not None:
+            if self._scaled_contents:
+                width, height = self.GetSize().asTuple()
+                if width != bmp.GetWidth() or height != bmp.GetHeight():
+                    img = bmp.ConvertToImage()
+                    img.Rescale(width, height, quality)
+                    bmp = wx.BitmapFromImage(img)
+            dc.DrawBitmap(bmp, 0, 0)
+
+    def OnPaint(self, event):
+        """ The paint event handler for the widget.
+
+        """
+        dc = wx.PaintDC(self)
+        self._PaintBitmap(dc, wx.IMAGE_QUALITY_NORMAL)
+
+    def OnResize(self, event):
+        """ The resize event handler for the widget.
+
+        This method is only bound and called when content scaling is
+        enabled. It starts(restarts) a timer to perform a high quality
+        scaled repaint when resizing is finished.
+
+        """
+        if self._scaled_contents:
+            self._timer.Start(60, True)
+
+    def OnRepaintTimer(self, event):
+        """ The repaint timer event handler.
+
+        This method is only bound and called when content scaling is
+        enabled and resizing has completed. It triggers a high quality
+        repaint.
+
+        """
+        dc = wx.ClientDC(self)
+        self._PaintBitmap(dc, wx.IMAGE_QUALITY_HIGH)
+
+    #--------------------------------------------------------------------------
+    # Public API
+    #--------------------------------------------------------------------------
     def GetBitmap(self, bitmap):
+        """ Get the underlying wx.Bitmap used to paint the control.
+
+        Returns
+        -------
+        result : wx.Bitmap or None
+            The bitmap being used to paint the control, or None if
+            no bitmap has been supplied.
+
+        """
         return self._bitmap
 
     def SetBitmap(self, bitmap):
+        """ Set the underlying wx.Bitmap and refresh the widget.
+
+        Parameters
+        ----------
+        bitmap : wx.Bitmap
+            The bitmap to paint on the widget.
+        
+        """
         self._bitmap = bitmap
         self.Refresh()
 
     def GetScaledContents(self):
+        """ Whether or not the bitmap is scaled to fit the bounds.
+
+        Returns
+        -------
+        result : bool
+            Whether or not the bitmap is scaled to fit the bounds of
+            the widget.
+        
+        """
         return self._scaled_contents
     
     def SetScaledContents(self, scaled):
-        self._scaled_contents = scaled
+        """ Set whether or not the bitmap should be scaled to fit the
+        bounds of the widget.
+
+        Parameters
+        ----------
+        scaled : bool
+            Whether or not to scale the bitmap to fit the bounds of the
+            widget.
+        
+        """
+        if scaled:
+            if not self._scaled_contents:
+                self._scaled_contents = True
+                self._timer = wx.Timer(self)
+                self.Bind(wx.EVT_TIMER, self.OnRepaintTimer)
+                self.Bind(wx.EVT_SIZE, self.OnResize)
+        else:
+            if self._scaled_contents:
+                self._scaled_contents = False
+                self._timer = None
+                self.Unbind(wx.EVT_TIMER, handler=self.OnRepaintTimer)
+                self.Unbind(wx.EVT_SIZE, hanlder=self.OnResize)
 
     def GetBestSize(self):
+        """ Overridden method to return the size of the bitmap as the 
+        best size for the widget.
+
+        """
         bmp = self._bitmap
         return wx.Size(bmp.GetWidth(), bmp.GetHeight())
 
     def GetBestSizeTuple(self):
-        return self.GetBestSize().asTuple()
+        """ Overridden method to return the size of the bitmap as the 
+        best size for the widget.
 
-    def OnPaint(self, event):
-        dc = wx.PaintDC(self)
-        bmp = self._bitmap
-        if bmp is not None:
-            dc.DrawBitmap(bmp, 0, 0)
+        """
+        return self.GetBestSize().asTuple()
 
 
 class WXImageView(WXControl, AbstractTkImageView):
@@ -57,10 +181,10 @@ class WXImageView(WXControl, AbstractTkImageView):
     # Setup methods
     #--------------------------------------------------------------------------
     def create(self, parent):
-        """ Creates the underlying wx.StaticBitmap control.
+        """ Creates the underlying wxBitmapWidget control.
 
         """
-        self.widget = wxImageWidget(parent)
+        self.widget = wxBitmapWidget(parent)
 
     def initialize(self):
         """ Initializes the attributes on the underlying control.
@@ -92,11 +216,10 @@ class WXImageView(WXControl, AbstractTkImageView):
     # Widget Update Methods
     #--------------------------------------------------------------------------
     def set_image(self, image):
-        """ Sets the image on the underlying control.
+        """ Sets the image on the underlying wxBitmapWidget.
 
         """
-        wxbitmap = image.as_wxBitmap()
-        self.widget.SetBitmap(wxbitmap)
+        self.widget.SetBitmap(image.as_wxBitmap())
 
         # Emit a size hint updated event if the size hint has actually
         # changed. This is an optimization so that a constraints update
@@ -116,6 +239,7 @@ class WXImageView(WXControl, AbstractTkImageView):
 
         """
         self.widget.SetScaledContents(scale_to_fit)
+        
         # See the comment in set_image(...) about the size hint update
         # notification. The same logic applies here.
         cached = self._cached_size_hint
