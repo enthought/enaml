53849a60839f5c59c1d1fe3a3ccfd39245dac420
Chris Colbert <sccolbert@gmail.com>
2012-04-19 16:37:42 -0500

    Update FileDialog shell component, example, and Qt implementation

--- enaml/backends/qt/qt_file_dialog.py
+++ enaml/backends/qt/qt_file_dialog.py
@@ -2,16 +2,24 @@
 #  Copyright (c) 2012, Enthought, Inc.
 #  All rights reserved.
 #------------------------------------------------------------------------------
-from os.path import split
+import os
 
-from .qt import QtGui, QtCore
+from .qt.QtGui import QFileDialog
 from .qt_dialog import QtDialog
 
-from ...components.file_dialog import FileDialog
+from ...components.file_dialog import AbstractTkFileDialog
+from ...guard import guard
 
 
-class QtFileDialog(QtDialog):
-    """ A Qt implementation of a FileDialog.
+ACCEPT_MODE_MAP = {
+    'open': QFileDialog.AcceptOpen,
+    'save': QFileDialog.AcceptSave,
+}
+
+
+class QtFileDialog(QtDialog, AbstractTkFileDialog):
+    """ A Qt4 implementation of a FileDialog.
+
     """
     #--------------------------------------------------------------------------
     # Setup methods
@@ -20,77 +28,143 @@ class QtFileDialog(QtDialog):
         """ Creates the underlying QFileDialog control.
 
         """
+        self.widget = QFileDialog(parent)
+    
+    def initialize(self):
+        """ Initializes the attributes of the underlying QFileDialog.
+
+        """
+        super(QtFileDialog, self).initialize()
         shell = self.shell_obj
-        
-        # If the caller provided a default path instead of a default directory
-        # and filename, split the path into it directory and filename
-        # components.
-        if len(shell.default_path) != 0 and len(shell.default_directory) == 0 \
-            and len(shell.default_filename) == 0:
-            default_directory, default_filename = split(shell.default_path)
-        else:
-            default_directory = shell.default_directory
-            default_filename = shell.default_filename
+        self.set_mode(shell.mode)
+        self.set_multi_select(shell.multi_select)
+        self.set_directory(shell.directory)
+        self.set_filename(shell.filename)
+        self.set_filters(shell.filters)
+        self.set_selected_filter(shell.selected_filter)
+        self.widget.setViewMode(QFileDialog.Detail)
 
-        # Convert the filter.
-        filters = []
-        for filter_list in shell.wildcard.split('|')[::2]:
-            # Qt uses spaces instead of semicolons for extension separators
-            filter_list = filter_list.replace(';', ' ')
-            filters.append(filter_list)
+    def bind(self):
+        """ Binds the signal handlers for the file dialog.
 
-        # Set the default directory.
-        if not default_directory:
-            default_directory = QtCore.QDir.currentPath()
+        """
+        super(QtFileDialog, self).bind()
+        widget = self.widget
+        widget.filesSelected.connect(self.on_files_selected)
+        widget.filterSelected.connect(self.on_filter_selected)
 
-        widget = QtGui.QFileDialog(parent, shell.title, default_directory)
+    #--------------------------------------------------------------------------
+    # Shell Object Change Handlers
+    #--------------------------------------------------------------------------
+    def shell_mode_changed(self, mode):
+        """ Update the dialog for the given mode of behavior.
 
-        widget.setViewMode(QtGui.QFileDialog.Detail)
-        widget.selectFile(default_filename)
-        widget.setNameFilters(filters)
+        """
+        self.set_mode(mode)
 
-        if shell.wildcard_index < len(filters):
-            widget.selectNameFilter(filters[shell.wildcard_index])
+    def shell_multi_select_changed(self, multi_select):
+        """ Update the dialog for the given multi select behavior.
 
-        if shell.type == 'open':
-            widget.setAcceptMode(QtGui.QFileDialog.AcceptOpen)
-            widget.setFileMode(QtGui.QFileDialog.ExistingFile)
-        elif shell.type == 'open files':
-            widget.setAcceptMode(QtGui.QFileDialog.AcceptOpen)
-            widget.setFileMode(QtGui.QFileDialog.ExistingFiles)
-        else:
-            widget.setAcceptMode(QtGui.QFileDialog.AcceptSave)
-            widget.setFileMode(QtGui.QFileDialog.AnyFile)
+        """
+        self.set_multi_select(multi_select)
 
-        self.widget = widget
+    def shell_directory_changed(self, directory):
+        """ Update the dialog with the new working directory.
 
+        """
+        if not guard.guarded(self, 'setting_directory'):
+            self.set_directory(directory)
+                
+    def shell_filename_changed(self, filename):
+        """ Update the dialog with the new selected filename.
+
+        """
+        if not guard.guarded(self, 'setting_filename'):
+            self.set_filename(filename)
+
+    def shell_filters_changed(self, filters):
+        """ Update the dialog with the new filename filters.
+
+        """
+        # Traits doens't fire off a change event if an Enum updates
+        # because its underlying values have changed. So, we just 
+        # set the selected filter manually here to cover that case.
+        self.set_filters(filters)
+        self.set_selected_filter(self.shell_obj.selected_filter)
+
+    def shell_selected_filter_changed(self, selected_filter):
+        """ Update the dialog with the new selected filter.
+
+        """
+        if not guard.guarded(self, 'setting_selected_filter'):
+            self.set_selected_filter(selected_filter)
 
     #--------------------------------------------------------------------------
     # Event Handlers 
     #--------------------------------------------------------------------------
-    def _on_close(self, qt_result):
-        """ The event handler for the dialog's finished signal.
+    def on_files_selected(self, files):
+        """ The signal handler for the dialog's `filesSelected` signal.
+
+        """
+        shell = self.shell_obj
+        first_file = files[0] if files else u''
+        with guard(self, 'setting_directory'):
+            with guard(self, 'setting_filename'):
+                shell.directory, shell.filename = os.path.split(first_file)
+                shell._paths = files
+                
+    def on_filter_selected(self, qt_filter):
+        """ The signal handler for the dialog's `filterSelected` signal.
+
+        """
+        shell = self.shell_obj
+        with guard(self, 'setting_selected_filter'):
+            shell.selected_filter = qt_filter
+
+    #--------------------------------------------------------------------------
+    # Widget Update Methods
+    #--------------------------------------------------------------------------
+    def set_mode(self, mode):
+        """ Sets the mode behavior of the file dialog.
+
+        """
+        self.widget.setAcceptMode(ACCEPT_MODE_MAP[mode])
+    
+    def set_multi_select(self, multi_select):
+        """ Sets the multi select behavior of the file dialog.
+
         """
         widget = self.widget
-        shell = self.shell_obj
-        
-        # Get the path of the chosen directory.
-        files = widget.selectedFiles()
-
-        if files:
-            shell.path = unicode(files[0])
-            shell.paths = [unicode(file) for file in files]
+        if widget.acceptMode() == QFileDialog.AcceptSave:
+            mode = QFileDialog.AnyFile
         else:
-            shell.path = u''
-            shell.paths = [u'']
+            if multi_select:
+                mode = QFileDialog.ExistingFile
+            else:
+                mode = QFileDialog.ExistingFiles
+        widget.setFileMode(mode)
 
-        # Extract the directory and filename.
-        shell.directory, shell.filename = split(shell.path)
+    def set_directory(self, directory):
+        """ Sets the current directory of the dialog.
 
-        # Get the index of the selected filter.
-        shell.wildcard_index = widget.nameFilters().index(
-            widget.selectedNameFilter())
+        """
+        self.widget.setDirectory(directory)
+    
+    def set_filename(self, filename):
+        """ Sets the selected filename in the dialog.
 
-        super(QtFileDialog, self)._on_close(qt_result)
+        """
+        self.widget.selectFile(filename)
+    
+    def set_filters(self, filters):
+        """ Sets the name filters in the file dialog.
 
+        """
+        self.widget.setNameFilters(filters)
+    
+    def set_selected_filter(self, selected_filter):
+        """ Sets the selected name filter in the file dialog.
+
+        """
+        self.widget.selectNameFilter(selected_filter)
 
