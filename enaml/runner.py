ad91218bb004b7f679ae342b8dc13eaadb9c8712
Ioannis Tziakos <ioannist@enthought.com>
2012-06-15 17:57:50 +0100

    make sure that ets and enaml are going to use the same toolkit back-end especially when ETS_TOOLKIT is not defined or the user want to override the ETS_TOOLKIT.

--- enaml/runner.py
+++ enaml/runner.py
@@ -15,10 +15,47 @@ from enaml.core.parser import parse
 from enaml.core.enaml_compiler import EnamlCompiler
 
 
-toolkits = {
+# Acceptable enaml toolkit options for enaml-run
+ENAML_TOOLKITS = {
     'default': default_toolkit, 'wx': wx_toolkit, 'qt': qt_toolkit,
+    'qt.agg': qt_toolkit}
+
+# Mapping of enaml toolkits to the ETS_TOOLKIT value
+ENAML_TO_ETS = {'default': 'qt4', 'wx': 'wx', 'qt': 'qt4','qt.agg': 'qt4.agg'}
+
+# Resulting ETS_TOOLKIT value when combining the original ETS_TOOLKIT value
+# with the toolkit that enaml is going to use. Enaml alsways wins, but
+# the kiva backend ussualy survives.
+TOOLKIT_COMBINATIONS = {
+    ('wx','default'): 'wx',
+    ('wx','qt'): 'qt4',
+    ('wx','qt.agg'): 'qt4.agg',
+    ('wx','wx'): 'wx',
+    ('qt4','default'): 'qt4',
+    ('qt4','qt'): 'qt4',
+    ('qt4','qt.agg'): 'qt4.agg',
+    ('qt4','wx'): 'wx',
+    ('qt4.agg','default'): 'qt4.agg',
+    ('qt4.agg','qt'): 'qt4.agg',
+    ('qt4.agg','qt.agg'): 'qt4.agg',
+    ('qt4.agg','wx'): 'wx',
 }
 
+def set_common_toolkit(toolkit_option):
+    """ Set the ETS_TOOLKIT variable for the ets ui components.
+
+    ETS gui components default to WX while enaml defaults to WX
+    We set the ETS_TOOLKIT enviroment for the process and it's childern
+    to make sure that ets amd enaml will use the same toolkit at all times.
+
+    """
+    if os.environ.has_key('ETS_TOOLKIT'):
+        ets_toolkit = os.environ.get('ETS_TOOLKIT').lower()
+        # Depending on the toolkit option we might need to override
+        toolkit_name = TOOLKIT_COMBINATIONS[(ets_toolkit,toolkit_option)]
+    else:
+        toolkit_name = ENAML_TO_ETS[toolkit_option]
+    os.environ['ETS_TOOLKIT'] = toolkit_name
 
 def main():
     usage = 'usage: %prog [options] enaml_file [script arguments]'
@@ -29,8 +66,13 @@ def main():
     parser.add_option('-t', '--toolkit', default='default',
                       choices=['default', 'wx', 'qt'],
                       help='The toolkit backend to use')
-    
+
     options, args = parser.parse_args()
+
+
+    # Preapare the toolkit
+    toolkit = ENAML_TOOLKITS[options.toolkit]
+    set_common_toolkit(options.toolkit)
 
     if len(args) == 0:
         print 'No .enaml file specified'
@@ -41,8 +83,8 @@ def main():
 
     with open(enaml_file) as f:
         enaml_code = f.read()
-    
-    # Parse and compile the Enaml source into a code object    
+
+    # Parse and compile the Enaml source into a code object
     ast = parse(enaml_code, filename=enaml_file)
     code = EnamlCompiler.compile(ast, enaml_file)
 
@@ -60,7 +102,7 @@ def main():
     with imports():
         exec code in ns
 
-    with toolkits[options.toolkit]():
+    with toolkit():
         requested = options.component
         if requested in ns:
             component = ns[requested]
