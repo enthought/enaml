fe5ad7418a506784fdcc4b3eab1932927c8be763
Ioannis Tziakos <ioannist@enthought.com>
2012-06-17 17:23:36 +0100

    Update enaml-run code to only for the ETS_TOOLKIT variable when it is not already set.
    - Remove unnecessary TOOLKIT_COMBINATION dictionary.
    - Add a warning when ETS_TOOLKIT and --toolkit option do not match
    - Update documentation

--- enaml/runner.py
+++ enaml/runner.py
@@ -9,6 +9,7 @@ import optparse
 import os
 import sys
 import types
+import warnings
 
 from enaml import imports, default_toolkit, wx_toolkit, qt_toolkit
 from enaml.core.parser import parse
@@ -17,45 +18,45 @@ from enaml.core.enaml_compiler import EnamlCompiler
 
 # Acceptable enaml toolkit options for enaml-run
 ENAML_TOOLKITS = {
-    'default': default_toolkit, 'wx': wx_toolkit, 'qt': qt_toolkit,
-    'qt.agg': qt_toolkit}
+    'default': default_toolkit, 'wx': wx_toolkit, 'qt': qt_toolkit}
 
-# Mapping of enaml toolkits to the ETS_TOOLKIT value
-ENAML_TO_ETS = {'default': 'qt4', 'wx': 'wx', 'qt': 'qt4','qt.agg': 'qt4.agg'}
-
-# Resulting ETS_TOOLKIT value when combining the original ETS_TOOLKIT value
-# with the toolkit that enaml is going to use. Enaml alsways wins, but
-# the kiva backend ussualy survives.
-TOOLKIT_COMBINATIONS = {
-    ('wx','default'): 'wx',
-    ('wx','qt'): 'qt4',
-    ('wx','qt.agg'): 'qt4.agg',
-    ('wx','wx'): 'wx',
-    ('qt4','default'): 'qt4',
-    ('qt4','qt'): 'qt4',
-    ('qt4','qt.agg'): 'qt4.agg',
-    ('qt4','wx'): 'wx',
-    ('qt4.agg','default'): 'qt4.agg',
-    ('qt4.agg','qt'): 'qt4.agg',
-    ('qt4.agg','qt.agg'): 'qt4.agg',
-    ('qt4.agg','wx'): 'wx',
-}
+# Mapping of the --toolkit option to the ETS_TOOLKIT value
+OPTION_TO_ETS = {'default': 'qt4', 'wx': 'wx', 'qt': 'qt4'}
 
 def set_common_toolkit(toolkit_option):
     """ Set the ETS_TOOLKIT variable for the ets ui components.
 
-    ETS gui components default to WX while enaml defaults to WX
-    We set the ETS_TOOLKIT enviroment for the process and it's childern
-    to make sure that ets amd enaml will use the same toolkit at all times.
+    ETS gui components default to WX while enaml defaults to Qt.  We
+    set the ETS_TOOLKIT enviroment for the process and it's childern
+    to make sure that ets amd enaml will use the same toolkit at all
+    times.
+
+    If the ETS_TOOLKIT is already set a warning is raised if there is
+    an incompatibility with the -t option.
+
+    Parameters
+    ----------
+    toolkit_option : str
+        The toolkit option provided to the enaml-run script
+
+    Returns
+    -------
+    enaml_toolkit : str
+       The enaml toolkit object as selected by the user. 
 
     """
+    enaml_toolkit = ENAML_TOOLKITS[toolkit_option]
     if os.environ.has_key('ETS_TOOLKIT'):
-        ets_toolkit = os.environ.get('ETS_TOOLKIT').lower()
-        # Depending on the toolkit option we might need to override
-        toolkit_name = TOOLKIT_COMBINATIONS[(ets_toolkit,toolkit_option)]
+        ets_toolkit = os.environ.get('ETS_TOOLKIT').lower().split('.')[0]
+        if toolkit_option != 'default' and ets_toolkit != OPTION_TO_ETS[toolkit_option]:
+            msg = ('The --toolkit option is different from the ETS_TOOLKIT enviroment'
+                   ' variable which can cause issues if enable components are used.') 
+            warnings.warn(msg)
     else:
         toolkit_name = ENAML_TO_ETS[toolkit_option]
-    os.environ['ETS_TOOLKIT'] = toolkit_name
+        os.environ['ETS_TOOLKIT'] = toolkit_name
+    return enaml_toolkit()
+
 
 def main():
     usage = 'usage: %prog [options] enaml_file [script arguments]'
@@ -71,8 +72,7 @@ def main():
 
 
     # Preapare the toolkit
-    toolkit = ENAML_TOOLKITS[options.toolkit]
-    set_common_toolkit(options.toolkit)
+    toolkit = set_common_toolkit(options.toolkit)
 
     if len(args) == 0:
         print 'No .enaml file specified'
@@ -102,7 +102,7 @@ def main():
     with imports():
         exec code in ns
 
-    with toolkit():
+    with toolkit:
         requested = options.component
         if requested in ns:
             component = ns[requested]
