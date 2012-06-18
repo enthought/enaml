e35d7191a4fbe2c6e5de1d71a142539c325cdf2f
Brett Patterson <bpatterson@enthought.com>
2012-06-18 15:49:18 -0500

    Fixed naming issues and added callback as a property

--- async/async_reply_scratch.py
+++ async/async_reply_scratch.py
@@ -6,15 +6,13 @@ class AsyncCommandReply(object):
     """ A stub out of a possible api for an async reply object.
 
     """
-    pending = True
-    results = None
-    callback = None
-
-    def __init__(self, callback):
+    def __init__(self):
         """ Initialize the reply with a callback
 
         """
-        self._set_callback(callback)
+        self._pending = True
+        self._callback = None
+        self._results = None
         
     def cancel(self):
         """ Cancel the pending task. The behavior of cancelling is 
@@ -23,30 +21,32 @@ class AsyncCommandReply(object):
         Cancelling a task after it is already completed is a no-op.
 
         """
-        self.pending = False
+        self._pending = False
 
     def pending(self):
         """ Returns whether or not the async command is still pending
         execution.
 
         """
-        return self.pending
+        return self._pending
 
     def results(self):
         """ Returns the results of the async command, or raises a 
         ValueError if the results are not yet available.
 
         """
-        if self.pending:
+        if self._pending:
             raise ValueError("Results not yet available.")
         else:
-            return self.results
+            return self._results
 
     def _get_callback(self):
-        return self.callback
+        return self._callback
 
     def _set_callback(self, cb):
-        self.callback = cb
+        self._callback = cb
+
+    callback = property(_get_callback, _set_callback)
 
     # A property which gets/sets a callback to be called when the 
     # results of the command are available
@@ -57,6 +57,7 @@ class AsyncCommandReply(object):
 
         """
         # store the results and call a registered callback
-        self.results = result
-        self.pending = False
+        self._results = result
+        self._pending = False
         self.callback()
+
