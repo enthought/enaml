2b381c55382a4d0f5192fc6a0c82868d06d26fc3
Chris Colbert <sccolbert@gmail.com>
2012-06-20 18:07:28 -0400

    update the messenger widget

--- async/core/messenger_widget.py
+++ async/core/messenger_widget.py
@@ -2,11 +2,13 @@
 #  Copyright (c) 2012, Enthought, Inc.
 #  All rights reserved.
 #------------------------------------------------------------------------------
+from traits.api import Str
+
 from async_messenger import AsyncMessenger
 from base_component import BaseComponent
 
 
-class CommandWidget(AsyncMessenger, BaseComponent):
+class MessengerWidget(AsyncMessenger, BaseComponent):
     """ The base class of all widgets in Enaml.
 
     This extends BaseComponent with the ability to send and receive
@@ -14,5 +16,37 @@ class CommandWidget(AsyncMessenger, BaseComponent):
     class.
 
     """
-    pass
+    # This is a hack so that the message id can be set by the async
+    # messenger class (since we inherit HasStrictTraits)
+    # XXX we need to make this better.
+    _AsyncMessenger__msg_id = Str
+
+    widget_name = Str
+
+    def _widget_name_default(self):
+        return type(self).__name__
+
+    def build_info(self):
+        """ Returns the dictionary of build information for this tree
+        from this point downward.
+
+        """
+        info = {}
+        info['widget'] = self.widget_name
+        info['msg_id'] = self.msg_id
+        info['attrs'] = self.initial_attrs()
+        child_info = []
+        for child in self.children:
+            child_info.append(child.build_info())
+        info['children'] = child_info
+        return info
+
+    def initial_attrs(self):
+        """ Returns a dictionary of attributes to initialize on the
+        client widget.
+
+        XXX - document what types of things to put in this dict.
+
+        """
+        return {}
 
