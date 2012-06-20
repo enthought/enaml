d8214a272ee9a3f36cadb7f539c3f43c3c392215
John Wiggins <jwiggins@enthought.com>
2012-06-20 15:41:26 -0500

    Add the ToggleControl family of components.

---
+++ async/components/radio_button.py
@@ -0,0 +1,24 @@
+#------------------------------------------------------------------------------
+#  Copyright (c) 2011, Enthought, Inc.
+#  All rights reserved.
+#------------------------------------------------------------------------------
+from traits.api import Instance
+
+from .toggle_control import ToggleControl
+
+
+class RadioButton(ToggleControl):
+    """ A radio button widget derived from IToggleElement 
+    
+    Use a radio button to toggle the value of a boolean field. For a 
+    group of radio buttons with the same widget parent, only one radio 
+    button may be selected at a time. This makes groups of radio buttons 
+    useful for selecting amongst a discrete set of values. For multiple 
+    groups of independent radio buttons, place them in their own Container.
+
+    See Also
+    --------
+    ToggleControl
+    
+    """
+
