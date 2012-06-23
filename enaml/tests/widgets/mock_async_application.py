bc9c4b5288e8693549678a22ed691e98942dfb6f
John Wiggins <jwiggins@enthought.com>
2012-06-23 17:25:53 -0500

    Mostly working test harness... except for, you know, the whole asynchronous messaging part. Baby steps.

--- enaml/tests/widgets/mock_async_application.py
+++ enaml/tests/widgets/mock_async_application.py
@@ -9,17 +9,37 @@ from enaml.async.async_application import AbstractBuilder, AsyncApplication, \
 
 from .mock_test_pipe import MockTestPipe
 
+
+def find_widget(root, type_name):
+    """ A simple function that recursively walks a widget tree until it finds
+    a widget of a particular type.
+
+    """
+    if root.widget_type == type_name:
+        return root
+
+    for child in root.children:
+        found = find_widget(child, type_name)
+        if found is not None:
+            return found
+
+    return None
+
+
 class MockWidget(object):
     """ A mock client UI widget
 
     """
-    def __init__(self, parent, send_pipe, recv_pipe):
+    def __init__(self, widget_type, parent, send_pipe, recv_pipe):
+        self.widget_type = widget_type
         self.parent = parent
         self.send_pipe = send_pipe
         self.recv_pipe = recv_pipe
         self.children = []
         self.attributes = {}
         self.recv_pipe.set_callback(self.recv)
+        if parent is not None:
+            parent.add_child(self)
 
     def initialize(self, attributes):
         # Add receive functions for attributes as needed.
@@ -53,6 +73,10 @@ class MockBuilder(AbstractBuilder):
     def __init__(self):
         self._root = None
 
+    @property
+    def root(self):
+        return self._root
+
     def build(self, info):
         info_stack = [(info, None)]
         while info_stack:
@@ -61,7 +85,7 @@ class MockBuilder(AbstractBuilder):
             recv_pipe = info_dct['recv_pipe']
             widget_cls = MockWidget
             # Cross the pipes when hooking the MockWidget up to the server widget
-            widget = widget_cls(parent, recv_pipe, send_pipe)
+            widget = widget_cls(info_dct['widget'], parent, recv_pipe, send_pipe)
             widget.initialize(info_dct['attrs'])
             children = info_dct['children']
             info_stack.extend(izip_longest(children, [], fillvalue=widget))
@@ -76,20 +100,21 @@ class MockApplication(AsyncApplication):
     """ A mock application for testing server widget components.
 
     """
+    def __init__(self):
+        self._builder = None
+
     #--------------------------------------------------------------------------
     # Abstract API implementation
     #--------------------------------------------------------------------------
     def register(self, messenger):
-        return (MockTestPipe(), MockTestPipe())
+        send_pipe = MockTestPipe()
+        recv_pipe = MockTestPipe()
+        send_pipe.run()
+        recv_pipe.run()
+        return (send_pipe, recv_pipe)
 
     def builder(self):
-        return MockBuilder()
-
-    #--------------------------------------------------------------------------
-    # Public API
-    #--------------------------------------------------------------------------
-    def run(self):
-        # XXX How should these get started?
-        self._send_pipe.run()
-        self._recv_pipe.run()
+        if self._builder is None:
+            self._builder = MockBuilder()
+        return self._builder
 
--- enaml/tests/widgets/test_label.py
+++ enaml/tests/widgets/test_label.py
@@ -2,15 +2,16 @@
 #  Copyright (c) 2012, Enthought, Inc.
 #  All rights reserved.
 #------------------------------------------------------------------------------
+from enaml.widgets.label import Label
+
 from .enaml_test_case import EnamlTestCase
-from .mock_async_application import MockApplication
+from .mock_async_application import MockApplication, find_widget
 
 
 class TestLabel(EnamlTestCase):
     def setUp(self):
         enaml_source = """
-from enaml.widgets.label import Label
-from enaml.widgets.window import Window
+from enaml.widgets import Label, Window
 
 enamldef MainView(Window):
     Label:
@@ -18,9 +19,18 @@ enamldef MainView(Window):
 """
         self.app = MockApplication()
         self.view = self.parse_and_create(enaml_source)
-        self.view.show()
-        self.app.run()
+        self.view.prepare()
+
+        for child in self.view.children:
+            if isinstance(child, Label):
+                self.server_label = child
+        self.client_label = find_widget(self.app.builder().root, "Label")
 
     def test_set_text(self):
-        pass
+        """ Test the setting of a Label's text attribute.
+        """
+        set_val = "something else"
+        self.server_label.text = set_val
+        assert self.client_label.text == set_val
+
 
