5cf22f5fa74cbfc233eeacf497f929cf3bcfdd0f
Chris Colbert <sccolbert@gmail.com>
2012-06-25 23:49:32 -0400

    update the FloatSlider

--- enaml/widgets/float_slider.py
+++ enaml/widgets/float_slider.py
@@ -16,32 +16,36 @@ class FloatSlider(Slider):
     #: :attr:`minimum` is higher than :attr:`maximum`. The value is
     #: a positive integer capped by the :attr:`maximum`. If the new
     #: value of :attr:`minimum` make the current position invalid then
-    #: the current position is set to :attr:minimum. Default value is 0.0.
+    #: the current position is set to :attr:minimum. Default is 0.0.
     minimum = Property(Float, depends_on ='_minimum')
 
-    #: The internal minimum storage
+    #: The internal minimum storage.
     _minimum = Float(0)
 
     #: The maximum value for the index. Checks make sure that
     #: :attr:`maximum` cannot be lower than :attr:`minimum`. If the
     #: new value of :attr:`maximum` make the current position invalid
-    #: then the current position is set to :attr:maximum. Default value is 1.0.
+    #: then the current position is set to :attr:maximum. Default is 1.0.
     maximum = Property(Float, depends_on ='_maximum')
 
-    #: The internal maximum storage
+    #: The internal maximum storage.
     _maximum = Float(1.0)
 
-    #: The number of integer steps allowed by the slider. Default value is 100.
+    #: The number of integer steps allowed by the slider. Default is 100.
     precision = Range(value=100, low=1, high=MAX_SLIDER_VALUE)
 
-    #: The interval to put between tick marks in integer units step units.
-    #: Default value is `10`.
-    tick_interval = Range(low='_one', high='precision', value=10, exclude_high=True)
-    _one = Constant(1)
+    #: The interval steps to put between tick marks. Default is 10.
+    tick_interval = Range(
+        low='_one', high='precision', value=10, exclude_high=True,
+    )
 
     #: Defines the number of steps that the slider will move when the
     #: user presses the page_up/page_down keys. Default is 10.
     page_step = Range(low='_one', high='precision', value=10)
+
+    #: An internal attribute to make the traits Range object happy when 
+    #: looking up the bounds of a range.
+    _one = Constant(1)
 
     #--------------------------------------------------------------------------
     # Initialization
@@ -60,22 +64,31 @@ class FloatSlider(Slider):
 
         """
         super_attrs = super(FloatSlider, self).initial_attrs()
-        attrs = {
-            'precision' : self.precision,
-        }
+        attrs = {'precision' : self.precision}
         super_attrs.update(attrs)
         return super_attrs
 
     #--------------------------------------------------------------------------
     # Property methods
     #--------------------------------------------------------------------------
-    def _set_minimum(self, value):
-        """ The property setter for the 'minimum' attribute. This
-        validates that the value is always smaller than :attr:`maximum`.
-        This is an overridden parent class method.
+    def _get_minimum(self):
+        """ The property getter for the slider minimum. 
+
+        Even though this is identical to the parent class method, it is 
+        required since we redefine the property on this class, and 
+        traits doesn't like that too much.
 
         """
-        if (value > self.maximum):
+        return self._minimum
+
+    def _set_minimum(self, value):
+        """ The property setter for the 'minimum' attribute. 
+
+        This validates that the value is always smaller than 
+        :attr:`maximum`. This is an overridden parent class method.
+
+        """
+        if value > self.maximum:
             msg = ('The minimum value of the slider should be a smaller '
                    'than the current maximum ({0}), '
                    'but a value of {1} was given')
@@ -84,13 +97,24 @@ class FloatSlider(Slider):
         else:
             self._minimum = value
 
-    def _set_maximum(self, value):
-        """ The property setter for the 'maximum' attribute. This
-        validates that the value is always larger than :attr:`minimum`.
-        This is an overridden parent class method.
+    def _get_maximum(self):
+        """ The property getter for the slider maximum. 
+
+        Even though this is identical to the parent class method, it is 
+        required since we redefine the property on this class, and 
+        traits doesn't like that too much.
 
         """
-        if (value < self.minimum):
+        return self._maximum
+
+    def _set_maximum(self, value):
+        """ The property setter for the 'maximum' attribute. 
+
+        This validates that the value is always larger than 
+        :attr:`minimum`. This is an overridden parent class method.
+
+        """
+        if value < self.minimum:
             msg = ("The maximum value of the slider should be "
                    "larger than the current minimum ({0}), "
                    "but a value of {1} was given")
@@ -99,19 +123,4 @@ class FloatSlider(Slider):
         else:
             self._maximum = value
 
-    def _get_maximum(self):
-        """ The property getter for the slider maximum. Even though this
-        is identical to the parent class method, it is required since we
-        redefine the property on this class.
-
-        """
-        return self._maximum
-
-    def _get_minimum(self):
-        """ The property getter for the slider minimum. Even though this
-        is identical to the parent class method, it is required since we
-        redefine the property on this class.
-
-        """
-        return self._minimum
-        
+            
