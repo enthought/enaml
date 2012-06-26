b948f6b7571c10b41e8840de51c5717e858ccc77
Brett Patterson <bpatterson@enthought.com>
2012-06-26 16:26:34 -0700

    Added QtSpinBox and finished QtSlider

--- enaml/qt/qt_slider.py
+++ enaml/qt/qt_slider.py
@@ -2,8 +2,26 @@
 #  Copyright (c) 2012, Enthought, Inc.
 #  All rights reserved.
 #------------------------------------------------------------------------------
-from .qt.QtGui import QSlider
+from .qt import QtGui, QtCore
 from .qt_constraints_widget import QtConstraintsWidget
+
+
+#: A map from Enaml constants to QSlider TickPosition values.
+_TICK_POSITION_MAP = {
+    'no_ticks': QtGui.QSlider.NoTicks,
+    'left': QtGui.QSlider.TicksLeft,
+    'right': QtGui.QSlider.TicksRight,
+    'top': QtGui.QSlider.TicksAbove,
+    'bottom': QtGui.QSlider.TicksBelow,
+    'both': QtGui.QSlider.TicksBothSides
+}
+
+#: A map from Enaml enums to Qt constants for horizontal or vertical 
+#: orientation.
+_ORIENTATION_MAP = {
+    'horizontal': QtCore.Qt.Horizontal,
+    'vertical': QtCore.Qt.Vertical
+}
 
 
 class QtSlider(QtConstraintsWidget):
@@ -14,7 +32,7 @@ class QtSlider(QtConstraintsWidget):
         """ Create the underlying QSlider widget.
 
         """
-        self.widget = QSlider(self.parent_widget)
+        self.widget = QtGui.QSlider(self.parent_widget)
 
     def initialize(self, init_attrs):
         """ Initialize the widget's attributes.
@@ -22,12 +40,23 @@ class QtSlider(QtConstraintsWidget):
         """
         super(QtSlider, self).initialize(init_attrs)
         self.set_value(init_attrs.get('value', 0))
+        self.set_maximum(init_attrs.get('maximum', 100))
+        self.set_minimum(init_attrs.get('minimum', 0))
+        self.set_orientation(init_attrs.get('orientation', 'horizontal'))
+        self.set_page_step(init_attrs.get('page_step', 1))
+        self.set_single_step(init_attrs.get('single_step', 1))
+        self.set_tick_interval(init_attrs.get('tick_interval', 1))
+        self.set_tick_position(init_attrs.get('tick_position', 'bottom'))
+        self.set_tracking(init_attrs.get('tracking', True))
 
     def bind(self):
         """ Bind the signal handlers for the widget.
 
         """
         self.widget.valueChanged.connect(self.on_value_changed)
+        self.widget.sliderMoved.connect(self.on_moved)
+        self.widget.sliderPressed.connect(self.on_pressed)
+        self.widget.sliderReleased.connect(self.on_released)
 
     #--------------------------------------------------------------------------
     # Message Handlers
@@ -38,6 +67,54 @@ class QtSlider(QtConstraintsWidget):
         """
         return self.set_value(ctxt['value'])
 
+    def receive_set_maximum(self, ctxt):
+        """ Handle the 'set_maximum' message from the Enaml widget.
+
+        """
+        return self.set_maximum(ctxt['value'])
+
+    def receive_set_minimum(self, ctxt):
+        """ Handle the 'set_minimum' message from the Enaml widget.
+
+        """
+        return self.set_minimum(ctxt['value'])
+
+    def receive_set_orientation(self, ctxt):
+        """ Handle the 'set_orientation' message from the Enaml widget.
+
+        """
+        return self.set_orientation(ctxt['value'])
+
+    def receive_set_page_step(self, ctxt):
+        """ Handle the 'set_page_step' message from the Enaml widget.
+
+        """
+        return self.set_page_step(ctxt['value'])
+
+    def receive_set_single_step(self, ctxt):
+        """ Handle the 'set_single_step' message from the Enaml widget.
+
+        """
+        return self.set_single_step(ctxt['value'])
+
+    def receive_set_tick_interval(self, ctxt):
+        """ Handle the 'set_tick_interval' message from the Enaml widget.
+
+        """
+        return self.set_tick_interval(ctxt['value'])
+
+    def receive_set_tick_position(self, ctxt):
+        """ Handle the 'set_tick_position' message from the Enaml widget.
+
+        """
+        return self.set_tick_position(ctxt['value'])
+
+    def receive_set_tracking(self, ctxt):
+        """ Handle the 'set_tracking' message from the Enaml widget.
+
+        """
+        return self.set_tracking(ctxt['value'])
+    
     #--------------------------------------------------------------------------
     # Signal Handlers
     #--------------------------------------------------------------------------
@@ -49,6 +126,24 @@ class QtSlider(QtConstraintsWidget):
         ctxt = {'value': self.widget.value()}
         self.send('set_value', ctxt)
 
+    def on_moved(self):
+        """ XXX
+
+        """
+        pass
+    
+    def on_pressed(self):
+        """ XXX
+
+        """
+        pass
+
+    def on_released(self):
+        """ XXX
+
+        """
+        pass
+
     #--------------------------------------------------------------------------
     # Widget Update Methods
     #--------------------------------------------------------------------------
@@ -59,3 +154,58 @@ class QtSlider(QtConstraintsWidget):
         self.widget.setValue(value)
         return True
 
+    def set_maximum(self, maximum):
+        """ Set the maximum value of the underlying widget.
+
+        """
+        self.widget.setMaximum(maximum)
+        return True
+
+    def set_minimum(self, minimum):
+        """ Set the minimum value of the underlying widget.
+
+        """
+        self.widget.setMinimum(minimum)
+        return True
+
+    def set_orientation(self, orientation):
+        """ Set the orientation of the underlying widget.
+
+        """
+        self.widget.setOrientation(_ORIENTATION_MAP[orientation])
+        return True
+
+    def set_page_step(self, page_step):
+        """ Set the page step of the underlying widget.
+
+        """
+        self.widget.setPageStep(page_step)
+        return True
+
+    def set_single_step(self, single_step):
+        """ Set the single step of the underlying widget.
+
+        """
+        self.widget.setSingleStep(single_step)
+        return True
+
+    def set_tick_interval(self, tick_interval):
+        """ Set the tick interval of the underlying widget
+
+        """
+        self.widget.setTickInterval(tick_interval)
+        return True
+
+    def set_tick_position(self, tick_position):
+        """ Set the tick position of the underlying widget.
+
+        """
+        self.widget.setTickPosition(_TICK_POSITION_MAP[tick_position])
+        return True
+
+    def set_tracking(self, tracking):
+        """ Set the tracking of the underlying widget.
+
+        """
+        self.widget.setTracking(tracking)
+        return True
---
+++ enaml/qt/qt_spin_box.py
@@ -0,0 +1,189 @@
+#------------------------------------------------------------------------------
+#  Copyright (c) 2012, Enthought, Inc.
+#  All rights reserved.
+#------------------------------------------------------------------------------
+from .qt.QtGui import QSpinBox, QValidator
+from .qt_constraints_widget import QtConstraintsWidget
+from .qt_enaml_validator import QtEnamlValidator
+
+
+class EnamlQSpinBox(QSpinBox):
+    """ A QSpinBox sublcass with hooks for user supplied validation.
+
+    """
+    #: The internal storage for the Enaml validator object. 
+    _validator = None
+
+    def validator(self):
+        """ Returns the Enaml validator assigned to this spin box.
+
+        """
+        return self._validator
+    
+    def setValidator(self, validator):
+        """ Set the validator for this spin box.
+
+        """
+        self._validator = validator
+        self.interpretText()
+
+    def textFromValue(self, value):
+        """ Converts the given integer to a string for display using the 
+        user supplied validator object. 
+
+        If the conversion fails due to the converter raising a ValueError
+        then simple unicode(...) conversion is used.
+
+        """
+        try:
+            text = self._validator.format(value)
+        except ValueError:
+            text = unicode(value)
+        return text
+
+    def valueFromText(self, text):
+        """ Converts the user typed string into an integer for the
+        control using the user supplied validator.
+
+        """
+        return self._validator.convert(text)
+
+    def validate(self, text, pos):
+        """ Validates whether or not the given text can be converted
+        to an integer.
+
+        """
+        v = self._validator
+        rv = v.validate(text)
+        if rv == v.ACCEPTABLE:
+            res = QValidator.Acceptable
+        elif rv == v.INTERMEDIATE:
+            res = QValidator.Intermediate
+        elif rv == v.INVALID:
+            res = QValidator.Invalid
+        else:
+            # This should never happen
+            raise ValueError('Invalid validation result')
+        return (res, text, pos)
+    
+
+class QtSpinBox(QtConstraintsWidget):
+    """ A Qt4 implementation of a SpinBox
+
+    """
+    def create(self):
+        """ Create the underlying widget
+
+        """
+        self.widget = EnamlQSpinBox(self.parent_widget)
+
+    def initialize(self, init_attrs):
+        """ Initialize the widget's attributes
+
+        """
+        self.set_maximum(init_attrs.get('maximum', 100))
+        self.set_minimum(init_attrs.get('minimum', 0))
+        self.set_single_step(init_attrs.get('single_step', 1))
+        self.set_tracking(init_attrs.get('tracking', True))
+        self.set_validator(init_attrs.get('validator',
+                                          QtEnamlValidator(QValidator)))
+        self.set_value(init_attrs.get('value', 0))
+        self.set_wrap(init_attrs.get('wrap', False))
+
+    #--------------------------------------------------------------------------
+    # Message Handlers
+    #--------------------------------------------------------------------------
+    def receive_set_maximum(self, ctxt):
+        """ Message handler for set_maximum
+
+        """
+        return self.set_maximum(ctxt['value'])
+
+    def receive_set_minimum(self, ctxt):
+        """ Message handler for set_minimum
+
+        """
+        return self.set_minimum(ctxt['value'])
+
+    def receive_set_single_step(self, ctxt):
+        """ Message handler for set_single_step
+
+        """
+        return self.set_single_step(ctxt['value'])
+
+    def receive_set_tracking(self, ctxt):
+        """ Message handler for set_tracking
+
+        """
+        self.set_tracking(ctxt['value'])
+
+    def receive_set_validator(self, ctxt):
+        """ Message handler for set_validator
+
+        """
+        return self.set_validator(ctxt['value'])
+
+    def receive_set_value(self, ctxt):
+        """ Message handler for set_value
+
+        """
+        return self.set_value(ctxt['value'])
+
+    def receive_set_wrap(self, ctxt):
+        """ Message handler for set_wrap
+
+        """
+        return self.set_wrap(ctxt['value'])
+
+    #--------------------------------------------------------------------------
+    # Widget Update Methods
+    #--------------------------------------------------------------------------
+    def set_maximum(self, maximum):
+        """ Set the widget's maximum value
+
+        """
+        self.widget.setMaximum(maximum)
+        return True
+
+    def set_minimum(self, minimum):
+        """ Set the widget's minimum value
+
+        """
+        self.widget.setMinimum(minimum)
+        return True
+
+    def set_single_step(self, step):
+        """ Set the widget's single step value
+
+        """
+        self.widget.setSingleStep(step)
+        return True
+
+    def set_tracking(self, tracking):
+        """ Set the keyboard tracking of the widget
+
+        """
+        self.widget.setKeyboardTracking(tracking)
+        return True
+
+    def set_validator(self, validator):
+        """ Set the validator for the spin box
+
+        """
+        self.widget.setValidator(validator)
+        return True
+
+    def set_value(self, value):
+        """ Set the spin box's value
+
+        """
+        self.widget.setValue(value)
+        return True
+
+    def set_wrap(self, wrap):
+        """ Set whether or not to allow the spin box to wrap at its
+        extreme values
+
+        """
+        self.widget.setWrapping(wrap)
+        return True
