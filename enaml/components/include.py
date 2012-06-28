9d74b21a85a9de87cdfacb880463db9806681fc5
Chris Colbert <sccolbert@gmail.com>
2012-06-28 00:39:36 -0400

    Update include to be more performance and more flexible.
    This update adds support for in-place modification of the list
    of components on the Include. The Include now only rebuilds
    what is necessary, rather than rebuilding everything on each
    change.

--- enaml/components/include.py
+++ enaml/components/include.py
@@ -2,7 +2,7 @@
 #  Copyright (c) 2011, Enthought, Inc.
 #  All rights reserved.
 #------------------------------------------------------------------------------
-from traits.api import List, Instance, Either, Bool, Property, cached_property
+from traits.api import CList, Instance, Bool, Property, on_trait_change
 
 from ..core.base_component import BaseComponent
 
@@ -20,18 +20,12 @@ class Include(BaseComponent):
     #: pass down to their dynamic components.
     toolkit_widget = Property
 
-    #: The dynamic components of the Include. This is a cached property
-    #: which will accept a single component, or a list of components as
-    #: input. If the input is a single component, it will be converted
-    #: into a single element list.
-    components = Property(
-        Either(List(Instance(BaseComponent)), Instance(BaseComponent)),
-        depends_on='_components',
-    )
-    
-    #: A private attribute which stores the underlying list of created
-    #: components. This list should not be manipulated by user code.
-    _components = List(Instance(BaseComponent))
+    #: The dynamic components of the Include. This is a component or
+    #: list of components which should be included in the children of 
+    #: the parent of this Include. Changes made to this list in whole 
+    #: or in-place will be reflected as updates in the ui. A single
+    #: component will be converted to a single element list.
+    components = CList(Instance(BaseComponent))
 
     #: A private Boolean flag indicating if the dynamic components of 
     #: this Include have been intialized for the first time. This should 
@@ -40,24 +34,7 @@ class Include(BaseComponent):
 
     #--------------------------------------------------------------------------
     # Property Getters and Setters
-    #--------------------------------------------------------------------------
-    @cached_property
-    def _get_components(self):
-        """ The cached property getter for the 'components' attribute. 
-        It simply returns the private '_components' list.
-
-        """
-        return self._components
-    
-    def _set_components(self, val):
-        """ The property setter for the 'components' attribute. It will
-        convert a single component into a single element list.
-
-        """
-        if isinstance(val, BaseComponent):
-            val = [val]
-        self._components = val
-    
+    #--------------------------------------------------------------------------    
     def _get_toolkit_widget(self):
         """ The property getter for the 'toolkit_widget' attribute.
 
@@ -79,23 +56,34 @@ class Include(BaseComponent):
 
         """
         super(Include, self)._setup_init_layout()
-        self._setup_components()
+        self._setup_components(self.components)
         self.on_trait_change(
-            self._on_components_actual_updated, '_components:_actual_updated',
+            self._on_components_actual_updated, 'components:_actual_updated',
         )
+        self._components_initialized = True
         self._actual_updated()
 
-    def _setup_components(self):
-        """ An internal method used to setup the dynamic child components.
+    def _setup_components(self, components):
+        """ Run the setup process for the provided list of components.
+
+        This is a private internal method which is used to run the 
+        setup process for a list of new components which should be
+        parented by the parent widget of this Include.
+
+        Parameters
+        ----------
+        components : sequence
+            The sequence of BaseComponent instances which should be 
+            setup as children of the parent widget for this Include.
 
         """
-        cmpnts = self.components
-        if not cmpnts:
+        # If we get an empty sequence, don't do any unnecessary work.
+        if not components:
             return
 
         # The dynamic components of an Include are injected into the 
-        # parent's children inline so that they *appear* as if they
-        # are siblings of the Include. To do this, we pass the reference
+        # parent's children so that they *appear* as if they are 
+        # siblings of the Include. To do this, we pass the reference
         # to the parent of the Include and the corresponding toolkit
         # widget where appropriate.
         parent = self.parent
@@ -115,37 +103,35 @@ class Include(BaseComponent):
 
         # Need to explicitly assign the parent to the components 
         # since they were not added via the add_subcomponent method.
-        for child in cmpnts:
+        for child in components:
             child.parent = self
         
-        for child in cmpnts:
+        for child in components:
             child._setup_create_widgets(toolkit_parent)
-            
-        for child in cmpnts:
+        
+        for child in components:
             child._setup_init_widgets()
 
-        for child in cmpnts:
+        for child in components:
             child._setup_eval_expressions()
 
-        for child in cmpnts:
+        for child in components:
             child._setup_bind_widgets()
                 
-        for child in cmpnts:
+        for child in components:
             child._setup_listeners()
 
-        for child in cmpnts:
+        for child in components:
             child._setup_init_visibility()
 
-        for child in cmpnts:
+        for child in components:
             child._setup_init_layout()
 
-        for child in cmpnts:
+        for child in components:
             child._setup_finalize()
 
-        for child in cmpnts:
+        for child in components:
             child._setup_set_initialized()
-
-        self._components_initialized = True
 
     #--------------------------------------------------------------------------
     # Parent Class Overrides 
@@ -155,8 +141,8 @@ class Include(BaseComponent):
         from being declared for an Include instance.
 
         """
-        msg = ("Cannot add subcomponents to an Include. Assign a component "
-               "or a list components to the 'components' attribute instead.")
+        msg = ("Cannot add subcomponents to an Include. Assign a list of "
+               "components to the 'components' attribute instead.")
         raise ValueError(msg)
 
     def get_actual(self):
@@ -182,34 +168,59 @@ class Include(BaseComponent):
     #--------------------------------------------------------------------------
     # Change Handlers 
     #--------------------------------------------------------------------------
-    def _components_changed(self, name, old, new):
-        """ Reacts to changes in the dynamic components and sets up the 
-        new children, making sure the old ones are destroyed and that 
-        the '_actual_updated' event gets fired.
+    @on_trait_change('components')
+    def _handle_components_changed(self, obj, name, old, new):
+        """ A private trait change handler which reacts to changes to 
+        the `components` list as a whole.
 
         """
-        # The first time a cached property is set, the notification
-        # handler will be sent None as the old value even if the 
-        # property getter points to something that has a value. In 
-        # all other cases, it works as expected. We guard against
-        # that condition as well a making sure the object is fully
-        # initialized before destroying any of the old children.
-        if self.initialized and old is not None:
+        self._update_components_diff(set(old), set(new))
+
+    @on_trait_change('components_items')
+    def _handle_components_items_changed(self, obj, name, event):
+        """ A private trait change handler which reacts to changes in
+        the items of the `components` list.
+
+        """
+        self._update_components_diff(set(event.removed), set(event.added))
+
+    def _update_components_diff(self, removed, added):
+        """ Updates the UI components based on an item diff.
+
+        A private method which will update the components by performing
+        diff between the removed and added components. Components no
+        longer in use will be destroyed. New components will be setup.
+
+        Parameters
+        ----------
+        removed : set
+            The set of components being removed. This is allowed to 
+            overlap with added (e.g. a reverse operation).
+
+        added : set
+            The set of components being added. This is allowed to 
+            overlap with added (e.g. a reverse operation).
+
+        """
+        if self.initialized:
+            to_destroy = removed - added
+            to_setup = added - removed
             def closure():
-                for item in old:
+                for item in to_destroy:
                     item.destroy()
-                self._setup_components()
+                self._setup_components(to_setup)
                 self._actual_updated()
             self.request_relayout_task(closure)
-            
-    # This notifier is hooked up in the '_handle_initialized' method 
+
+    # This notifier is hooked up in the '_setup_init_layout' method 
     # due to issues surrounding trait_setq contexts.
     def _on_components_actual_updated(self):
         """ Handles a '_actual_updated' event being fired by one 
-        the dynamic components. The event is proxied up the tree by
-        firing the same event on this instance. This allows a nested
-        Include to update its contents independent of the Include in
-        which it is nested.
+        the dynamic components. 
+
+        The event is proxied up the tree by firing the same event on 
+        this instance. This allows a nested Include to update its 
+        contents independent of the Include in which it is nested.
 
         """        
         self._actual_updated()
