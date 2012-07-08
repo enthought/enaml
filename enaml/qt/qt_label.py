58db31d58c3cf6378b47772154a6c06fb3e13ae6
Chris Colbert <sccolbert@gmail.com>
2012-07-08 17:47:29 -0400

    update the QtLabel widget for the new protocols

--- enaml/qt/qt_label.py
+++ enaml/qt/qt_label.py
@@ -5,55 +5,54 @@
 from .qt.QtGui import QLabel
 from .qt_constraints_widget import QtConstraintsWidget
 
+
 class QtLabel(QtConstraintsWidget):
-    """ An label widget based on a QLabel
+    """ A Qt4 implementation of an Enaml Label.
 
     """
     def create(self):
-        """ Create the underlying widget
+        """ Create the underlying widget.
 
         """
         self.widget = QLabel(self.parent_widget)
 
-    def initialize(self, init_attrs):
-        """ Initialize the widget's attributes
+    def initialize(self, attrs):
+        """ Initialize the widget's attributes.
 
         """
-        super(QtLabel, self).initialize(init_attrs)
-        self.set_text(init_attrs.get('text', ''))
-        self.set_word_wrap(init_attrs.get('word_wrap', False))
+        super(QtLabel, self).initialize(attrs)
+        self.set_text(attrs['text'])
+        self.set_word_wrap(attrs['word_wrap'])
 
     #--------------------------------------------------------------------------
     # Message Handlers
     #--------------------------------------------------------------------------
-    def receive_set_text(self, ctxt):
-        """ Message handler for set_text
+    def on_message_set_text(self, payload):
+        """ Handle the 'set-text' action from the Enaml widget.
 
         """
-        text = ctxt.get('text')
-        if text is not None:
-            self.set_text(text)
+        # XXX trigger a relayout if the size hint has changed.
+        self.set_text(payload['text'])
 
-    def receive_set_word_wrap(self, ctxt):
-        """ Message handler for set_word_wrap
+    def on_message_set_word_wrap(self, payload): 
+        """ Handle the 'set-word_wrap' action from the Enaml widget.
 
         """
-        wrap = ctxt.get('word_wrap')
-        if wrap is not None:
-            self.set_word_wrap(wrap)
+        # XXX trigger a relayout if the size hint has changed.
+        self.set_word_wrap(payload['word_wrap'])
 
     #--------------------------------------------------------------------------
     # Widget Update Methods
     #--------------------------------------------------------------------------
     def set_text(self, text):
-        """ Set the source of the label widget
+        """ Set the text in the underlying widget.
 
         """
         self.widget.setText(text)
 
     def set_word_wrap(self, wrap):
-        """ Set the word wrap of the label widget
+        """ Set the word wrap of the underlying widget.
 
         """
         self.widget.setWordWrap(wrap)
-        
+
