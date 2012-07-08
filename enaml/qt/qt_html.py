d9218e27871dda0914a3e9e789336d6f61ba44a6
Chris Colbert <sccolbert@gmail.com>
2012-07-08 17:38:14 -0400

    update the Qt html component

--- enaml/qt/qt_html.py
+++ enaml/qt/qt_html.py
@@ -5,42 +5,33 @@
 from .qt.QtGui import QTextEdit
 from .qt_constraints_widget import QtConstraintsWidget
 
+
 class QtHtml(QtConstraintsWidget):
-    """ An html display widget based on a QTextEdit
+    """ A Qt4 implementation of the Enaml HTML widget.
 
     """
     def create(self):
-        """ Create the underlying widget
+        """ Create the underlying widget.
 
         """
         self.widget = QTextEdit(self.parent_widget)
+        self.widget.setReadOnly(True)
 
-    def initialize(self, init_attrs):
-        """ Initialize the widget's attributes
+    def initialize(self, attrs):
+        """ Initialize the widget's attributes.
 
         """
-        super(QtHtml, self).initialize(init_attrs)
-        self.set_source(init_attrs.get('source', ''))
-        self.set_read_only(init_attrs.get('read_only', False))
+        super(QtHtml, self).initialize(attrs)
+        self.set_source(attrs['source'])
 
     #--------------------------------------------------------------------------
     # Message Handlers
     #--------------------------------------------------------------------------
-    def receive_set_source(self, ctxt):
-        """ Message handler for set_source
+    def on_message_set_source(self, payload):
+        """ Handle the 'set-source' action from the Enaml widget.
 
         """
-        source = ctxt.get('source')
-        if source is not None:
-            self.set_source(source)
-
-    def receive_set_read_only(self, ctxt):
-        """ Message handler for set_read_only
-
-        """
-        read_only = ctxt.get('read_only')
-        if read_only is not None:
-            self.set_read_only(read_only)
+        self.set_source(payload['source'])
 
     #--------------------------------------------------------------------------
     # Widget Update Methods
@@ -51,8 +42,3 @@ class QtHtml(QtConstraintsWidget):
         """
         self.widget.setHtml(source)
 
-    def set_read_only(self, read_only):
-        """ Set whether or not the widget is editable
-
-        """
-        self.widget.setReadOnly(read_only)
