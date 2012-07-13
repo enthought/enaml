7bf1688466aa839b0f8c6cf66021ccfb6aa28f29
Brett Patterson <bpatterson@enthought.com>
2012-07-13 11:52:33 -0500

    Updated editor to be just a QWebView, not a QMainWindow containing a QWebView. Also added a command document explaining the flow of commands and current list of commands

--- editor/editor_test.py
+++ editor/editor_test.py
@@ -1,34 +1,31 @@
-from PySide.QtGui import QApplication, QMainWindow
+from PySide.QtGui import QApplication
 from PySide.QtWebKit import QWebView, QWebSettings
 from ace_text_editor import AceTextEditor
 
 app = QApplication([])
 
-class AceMainWindow(QMainWindow):
+class AceWidget(QWebView):
     def __init__(self, text="", mode="text", theme="textmate"):
         """ Initialize the editor window
 
         """
-        super(AceMainWindow, self).__init__()
+        super(AceWidget, self).__init__()
         self.ace_editor = AceTextEditor(text=text, mode=mode, theme=theme)
         
-        self.web_view = QWebView()
-        self.web_view.settings().setAttribute(QWebSettings.DeveloperExtrasEnabled, True)
+        self.settings().setAttribute(QWebSettings.DeveloperExtrasEnabled, True)
 
-        self.main_frame = self.web_view.page().mainFrame()
+        self.main_frame = self.page().mainFrame()
         self.main_frame.addToJavaScriptWindowObject('py_ace_editor', self.ace_editor)
             
         self.ace_editor.generate_ace_event('set_text', 'getSession()',
             'ace_editor.getSession().getDocument().getValue()', 'change')
+        
         self.ace_editor.generate_binding('theme_changed', 'ace_editor',
              'setTheme')
         self.ace_editor.generate_binding('mode_changed',
              'ace_editor.getSession()', 'setMode')
         
-        html = self.ace_editor.generate_html()
-        self.web_view.setHtml(html)
-        
-        self.setCentralWidget(self.web_view)
+        self.setHtml(self.ace_editor.generate_html())
             
     def editor(self):
         """ Return the ace editor
@@ -37,15 +34,15 @@ class AceMainWindow(QMainWindow):
         return self.ace_editor
 
 
-window = AceMainWindow(mode="python")
-window.show()
+widget = AceWidget(mode="python")
+widget.show()
 
 def after_load():
-    window.editor().set_theme('twilight')
+    widget.editor().set_theme('twilight')
     
-window.web_view.loadFinished.connect(after_load)
+widget.loadFinished.connect(after_load)
 
 app.exec_()
 
-print window.editor().text()
+print widget.editor().text()
 
