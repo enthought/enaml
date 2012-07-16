22df09eabceb828cb2c07c32e12099d7b12e2112
Chris Colbert <sccolbert@gmail.com>
2012-07-16 15:13:20 -0400

    Update the Qt4 implementations of Notebook and Page
    This updates the logic of who manages what. It makes the code easier
    to follow and maintain.

--- enaml/qt/qt_notebook.py
+++ enaml/qt/qt_notebook.py
@@ -2,6 +2,7 @@
 #  Copyright (c) 2012, Enthought, Inc.
 #  All rights reserved.
 #------------------------------------------------------------------------------
+from .qt.QtCore import Signal
 from .qt.QtGui import QTabWidget
 from .qt_constraints_widget import QtConstraintsWidget
 
@@ -20,6 +21,123 @@ DOCUMENT_MODES = {
 }
 
 
+class QNotebook(QTabWidget):
+    """ A custom QTabWidget which behaves like a notebook holding
+    instances of QPage.
+
+    """
+    #: A signal emitted when the user clicks on the tab close button
+    #: for a QPage which is marked as closable. User code should
+    #: connect to this signal as opposed to the 'tabCloseRequested'
+    #: signal on the parent, since this signal will not be emitted
+    #: for QPage instances which are not closable.
+    pageCloseRequested = Signal(int)
+
+    def __init__(self, *args, **kwargs):
+        """ Initialize a QNotebook.
+
+        Parameters
+        ----------
+        *args, **kwargs
+            The positional and keyword arguments needed to initialize
+            a QTabWidget.
+
+        """
+        super(QNotebook, self).__init__(*args, **kwargs)
+        self.tabCloseRequested.connect(self._onTabCloseRequested)
+
+    #--------------------------------------------------------------------------
+    # Private API
+    #--------------------------------------------------------------------------
+    def _bind(self, page):
+        """ Bind the signal handlers for the given page.
+
+        """
+        page.tabTitleChanged.connect(self._onTabTitleChanged)
+        page.tabToolTipChanged.connect(self._onTabToolTipChanged)
+        page.tabEnabledChanged.connect(self._onTabEnabledChanged)
+
+    def _unbind(self, page):
+        """ Unbind the signal handlers for the given page.
+
+        """
+        page.tabTitleChanged.disconnect(self._onTabTitleChanged)
+        page.tabToolTipChanged.disconnect(self._onTabToolTipChanged)
+        page.tabEnabledChanged.disconnect(self._onTabEnabledChanged)
+
+    def _onTabCloseRequested(self, index):
+        """ The handler for the 'tabCloseRequested' signal.
+
+        """
+        page = self.widget(index)
+        if page.tabClosable():
+            self.pageCloseRequested.emit(index)
+
+    def _onTabTitleChanged(self, page, title):
+        """ The handler for the 'tabTitleChanged' signal on a child
+        QPage.
+
+        """
+        idx = self.indexOf(page)
+        if idx != -1:
+            self.setTabText(idx, title)
+
+    def _onTabToolTipChanged(self, page, tool_tip):
+        """ The handler for the 'tabToolTipChanged' signal on a child
+        QPage.
+
+        """
+        idx = self.indexOf(page)
+        if idx != -1:
+            self.setTabToolTip(idx, tool_tip)
+
+    def _onTabEnabledChanged(self, page, enabled):
+        """ The handler for the 'tabEnabledChanged' signal on a child
+        QPage.
+
+        """
+        idx = self.indexOf(page)
+        if idx != -1:
+            self.setTabEnabled(idx, enabled)
+            page.restoreEnabled()
+
+    #--------------------------------------------------------------------------
+    # Public API
+    #--------------------------------------------------------------------------
+    def addPage(self, page):
+        """ Add a QPage instance to the notebook. This method should
+        be used in favor of the 'addTab' method of the parent class.
+
+        Parameters
+        ----------
+        page : QPage
+            The QPage instance to add to the notebook.
+
+        """
+        idx = self.indexOf(page)
+        if idx == -1:
+            idx = self.addTab(page, page.tabTitle())
+        self.setTabToolTip(idx, page.tabToolTip())
+        self.setTabEnabled(idx, page.tabEnabled())
+        page.restoreEnabled()
+        self._bind(page)
+
+    def removePage(self, index):
+        """ Remove the page at the given index. This method should be
+        used in favor of the 'removeTab' method of the parent class.
+
+        Parameters
+        ----------
+        index : int
+            The index of the page to remove from the notebook.
+
+        """
+        page = self.widget(index)
+        if page is not None:
+            self.removeTab(index)
+            self._unbind(page)
+
+
 class QtNotebook(QtConstraintsWidget):
     """ A Qt4 implementation of an Enaml Notebook.
 
@@ -31,7 +149,7 @@ class QtNotebook(QtConstraintsWidget):
         """ Create the underlying widget.
 
         """
-        self.widget = QTabWidget(self.parent_widget)
+        self.widget = QNotebook(self.parent_widget)
 
     def initialize(self, attrs):
         """ Initialize the widget attributes
@@ -42,7 +160,19 @@ class QtNotebook(QtConstraintsWidget):
         self.set_tab_style(attrs['tab_style'])
         self.set_tabs_closable(attrs['tabs_closable'])
         self.set_tabs_movable(attrs['tabs_movable'])
-        self.widget.tabCloseRequested.connect(self.on_tab_close_requested)
+        self.widget.pageCloseRequested.connect(self.on_page_close_requested)
+
+    def post_initialize(self):
+        """ Handle the post initialization for the notebook.
+
+        This method explicitly adds the child QPage instances to the
+        underlying QNotebook control.
+
+        """
+        super(QtNotebook, self).post_initialize()
+        widget = self.widget
+        for child in self.children:
+            widget.addPage(child.widget)
 
     #--------------------------------------------------------------------------
     # Message Handlers
@@ -78,9 +208,7 @@ class QtNotebook(QtConstraintsWidget):
         target_id = payload['target_id']
         for child in self.children:
             if child.target_id == target_id:
-                idx = self.widget.indexOf(child.widget)
-                if idx == -1:
-                    self.widget.addTab(child.widget, child.get_title())
+                self.widget.addPage(child.widget)
                 return
 
     def on_message_close_tab(self, payload):
@@ -90,22 +218,21 @@ class QtNotebook(QtConstraintsWidget):
         target_id = payload['target_id']
         for child in self.children:
             if child.target_id == target_id:
-                idx = self.widget.indexOf(child.widget)
-                if idx != -1:
-                    self.widget.removeTab(idx)
+                widget = self.widget
+                widget.removePage(widget.indexOf(child.widget))
                 return
 
     #--------------------------------------------------------------------------
     # Signal Handlers
     #--------------------------------------------------------------------------
-    def on_tab_close_requested(self, index):
-        """ The signal handler for the 'tabCloseRequested' signal.
+    def on_page_close_requested(self, index):
+        """ The signal handler for the 'pageCloseRequested' signal.
 
         """
-        widget = self.widget.widget(index)
-        self.widget.removeTab(index)
+        page = self.widget.widget(index)
+        self.widget.removePage(index)
         for child in self.children:
-            if widget == child.widget:
+            if page == child.widget:
                 target_id = child.target_id
                 payload = {'action': 'tab-closed', 'target_id': target_id}
                 self.send_message(payload)
--- enaml/qt/qt_page.py
+++ enaml/qt/qt_page.py
@@ -2,23 +2,172 @@
 #  Copyright (c) 2012, Enthought, Inc.
 #  All rights reserved.
 #------------------------------------------------------------------------------
+from .qt.QtCore import Signal
 from .qt_container import QtContainer, QContainer
+
+
+class QPage(QContainer):
+    """ A QContainer subclass which acts as a page in a QNotebook.
+
+    """
+    #: A signal emitted when the tab title for this page changes.
+    #: The payload is the page itself followed by the new title.
+    tabTitleChanged = Signal(object, unicode)
+
+    #: A signal emitted when the tab tool tip for this page changes.
+    #: The payload is the page itself followed by the new tool tip.
+    tabToolTipChanged = Signal(object, unicode)
+
+    #: A signal emitted when the tab enabled state changes.
+    #: The payload is the page itself followed by the new state.
+    tabEnabledChanged = Signal(object, bool)
+
+    #: A signal emitted when the closable state changes.
+    #: The payload is the page itself followed by the new state.
+    tabClosableChanged = Signal(object, bool)
+
+    def __init__(self, *args, **kwargs):
+        """ Initialize a QPage.
+
+        Parameters
+        ----------
+        *args, **kwargs
+            The position and keyword arguments required to initialize
+            a QContainer.
+
+        """
+        super(QPage, self).__init__(*args, **kwargs)
+        self._tab_title = u''
+        self._tab_tool_tip = u''
+        self._tab_enabled = True
+        self._tab_closable = True
+        self._enabled = self.isEnabled()
+
+    def setEnabled(self, enabled):
+        """ An overridden parent class method. 
+
+        This stores a copy of the enabled flag. The parent QNotebook
+        doesn't keep a separate flag for the tab enabled state (it just
+        uses the flag on the child). So, it must be restored manually 
+        when the tab enabled state is changed.
+
+        """
+        super(QPage, self).setEnabled(enabled)
+        self._enabled = self.isEnabled()
+
+    def restoreEnabled(self):
+        """ A method called by the parent QNotebook.
+
+        This method will restore the enabled state of the QPage to
+        the previously set value. This is used to overcome the issue
+        in QTabWidget, where a separate tab enabled flag is not kept,
+        and enabled flag of a child is clobbered whenever the tab
+        enabled state is changed.
+
+        """
+        self.setEnabled(self._enabled)
+
+    def tabTitle(self):
+        """ Returns the tab title for this page.
+
+        Returns
+        -------
+        result : unicode
+            The title string for the page's tab.
+
+        """
+        return self._tab_title
+
+    def setTabTitle(self, title):
+        """ Set the title for the tab for this page. This will emit the
+        'tabTitleChanged' signal.
+
+        Parameters
+        ----------
+        title : unicode
+            The string to use for this page's tab title.
+
+        """
+        self._tab_title = title
+        self.tabTitleChanged.emit(self, title)
+
+    def tabToolTip(self):
+        """ Returns the tool tip for the tab for this page.
+
+        Returns
+        -------
+        result : unicode
+            The tool tip string for the page's tab.
+
+        """
+        return self._tab_tool_tip
+
+    def setTabToolTip(self, tool_tip):
+        """ Set the tool tip for the tab for this page. This will emit
+        the 'tabToolTipChanged' signal.
+
+        Parameters
+        ----------
+        title : unicode
+            The string to use for this page's tab tool tip.
+
+        """
+        self._tab_tool_tip = tool_tip
+        self.tabToolTipChanged.emit(self, tool_tip)
+
+    def tabEnabled(self):
+        """ Return whether or no the tab for this page is enabled.
+
+        Returns
+        -------
+        result : bool
+            True if the tab for this page is enabled, False otherwise.
+
+        """
+        return self._tab_enabled
+
+    def setTabEnabled(self, enabled):
+        """ Set whether the tab for this page is enabled. This will 
+        emit the 'tabEnabledChanged' signal.
+
+        Parameters
+        ----------
+        enabled : bool
+            True if the tab should be enabled, False otherwise.
+
+        """
+        self._tab_enabled = enabled
+        self.tabEnabledChanged.emit(self, enabled)
+
+    def tabClosable(self):
+        """ Returns whether or not the tab for this page is closable.
+
+        Returns
+        -------
+        result : bool
+            True if this page's tab is closable, False otherwise.
+
+        """
+        return self._tab_closable
+
+    def setTabClosable(self, closable):
+        """ Set whether the tab for this page is closable. This will
+        emit the 'tabClosableChanged' signal.
+
+        Parameters
+        ----------
+        closable : bool
+            True if the tab should be closable, False otherwise.
+
+        """
+        self._tab_closable = closable
+        self.tabClosableChanged.emit(self, closable)
 
 
 class QtPage(QtContainer):
     """ A Qt4 implementation of an Enaml notebook Page.
 
     """
-    #: Internal storage for a copy of the page title. This is needed by
-    #: the parent notebook when re-opening a closed tab.
-    _title = u''
-
-    #: Internal storage for a copy of the enabled flag. This is required
-    #: for maintaining the enabled state of the page versus the enabled
-    #: state of the tab. Qt doesn't keep separate flags for these, so
-    #: we need to do it ourselves.
-    _enabled = True
-
     #--------------------------------------------------------------------------
     # Setup Methods
     #--------------------------------------------------------------------------
@@ -26,10 +175,10 @@ class QtPage(QtContainer):
         """ Create the underlying widget.
 
         """
-        # The widget is properly parented by call addTab on the parent
-        # QTabWidget, as opposed to parenting the widget like normal.
-        self.widget = QContainer()
-        self.parent_widget.addTab(self.widget, self.get_title())
+        # The parent QtNotebook will come along and add the pages to
+        # its notebook in a special way, hence we explicitly do not 
+        # parent the QPage widget at this point.
+        self.widget = QPage()
 
     def initialize(self, attrs):
         """ Initialize the attributes of the underlying control.
@@ -39,6 +188,7 @@ class QtPage(QtContainer):
         self.set_title(attrs['title'])
         self.set_tool_tip(attrs['tool_tip'])
         self.set_tab_enabled(attrs['tab_enabled'])
+        self.set_closable(attrs['closable'])
 
     #--------------------------------------------------------------------------
     # Message Handling
@@ -61,85 +211,46 @@ class QtPage(QtContainer):
         """
         self.set_tab_enabled(payload['tab_enabled'])
 
+    def on_message_set_closable(self, payload):
+        """ Handle the 'set-closable' action from the Enaml widget.
+
+        """
+        self.set_closable(payload['closable'])
+
     #--------------------------------------------------------------------------
     # Widget Update Methods
     #--------------------------------------------------------------------------
-    def get_title(self):
-        """ Return the title in use for this page.
-
-        """
-        return self._title
-
     def set_title(self, title):
         """ Set the title of the tab for this page.
 
         """
-        op = lambda tabw, idx: tabw.setTabText(idx, title)
-        self._tab_index_op(op)
-        self._title = title
+        self.widget.setTabTitle(title)
+
+    def set_closable(self, closable):
+        """ Set whether or not this page is closable.
+
+        """
+        self.widget.setTabClosable(closable)
 
     def set_tool_tip(self, tool_tip):
         """ Set the tooltip of the tab for this page.
 
         """
-        op = lambda tabw, idx: tabw.setTabToolTip(idx, tool_tip)
-        self._tab_index_op(op)
+        self.widget.setTabToolTip(tool_tip)
 
     def set_tab_enabled(self, enabled):
         """ Set the enabled state for the tab for this page.
 
         """
-        op = lambda tabw, idx: tabw.setTabEnabled(idx, enabled)
-        self._tab_index_op(op)
-        # Restore the current flag for the child, since the tab widget
-        # doesn't store its own flag and overrides that of the page.
-        self.set_enabled(self._enabled)
-
-    def set_enabled(self, enabled):
-        """ Overridden method which stores the enabled flag.
-
-        The QTabWidget parent does not keep its own enabled flag for 
-        each tab. It reuses that from the child widgets. This means
-        enabling a disabled tab will re-enable the child page even 
-        if it was previously disabled. Storing this flag allows us
-        to restore the enabled state of the page, after changing the
-        enabled state of the tab.
-
-        """
-        super(QtPage, self).set_enabled(enabled)
-        self._enabled = enabled
+        self.widget.setTabEnabled(enabled)
 
     def set_visible(self, visible):
         """ Overriden method which disables the setting of visibility
         by the user code.
 
         The visibility of a page is controlled entirely by the parent
-        Notebook.
+        QtNotebook.
 
         """
         pass
-
-    #--------------------------------------------------------------------------
-    # Private Api
-    #--------------------------------------------------------------------------
-    def _tab_index_op(self, op):
-        """ Perform an operation on the parent notebook widget which
-        requires the tab index.
-
-        The callable will only be called the index of this page can
-        be successfully determined.
-
-        Parameters
-        ----------
-        op : callable
-            A callable which accepts two arguments, the QTabWidget
-            control, and the integer index of the tab representing
-            this page.
-
-        """
-        widget = self.widget
-        tab_widget = self.parent_widget
-        idx = tab_widget.indexOf(widget)
-        if idx != -1:
-            op(tab_widget, idx)
 
