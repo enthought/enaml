28f9a03a297b6f3c8919c6617ec8937c5360a6cc
Corran Webster <cwebster@enthought.com>
2012-07-20 10:03:24 -0500

    Get hello world working.

--- examples/hello_world/hello_world.py
+++ examples/hello_world/hello_world.py
@@ -3,10 +3,19 @@
 #  All rights reserved.
 #------------------------------------------------------------------------------
 import enaml
+from enaml.qt.qt_local_application import QtLocalApplication
 
-with enaml.imports():
-    from hello_world_view import Main
 
-view = Main(message="Hello, world, from Python!")
-view.show()
+if __name__ == '__main__':
+
+    with enaml.imports():
+        from hello_world_view import Main
+
+
+    view = Main(message="Hello, world, from Python!")
+    
+    app = QtLocalApplication()
+    app.serve('main', view)
+
+    app.mainloop()
 
