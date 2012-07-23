cd49f27c446bf7c47fa95e59894517bea6aadda7
Chris Colbert <sccolbert@gmail.com>
2012-07-22 23:41:15 -0400

    update the message format to be an array at the top level.
    This saves bytes on the wire, and is optimized for multipart messaging

--- message_format.js
+++ message_format.js
@@ -10,77 +10,72 @@
 // Message Format
 //-----------------------------------------------------------------------------
 // This describes the message object that is used to communicate between
-// objects in an Enaml application. This message format is the same as
-// that used by the most recent version of IPython.
+// objects in an Enaml application. This message format is very similar
+// to that used by the most recent version of IPython. This was done on
+// purpose so that it will not be difficult to have the two communicate
+// with each other.
 //
 // The basic requirement of message is that be serializable to JSON. 
 // For applications which operate locally, they may choose to omit
 // the serialization step, since it would be unnecessary.
 //
-// Once messages are created and posted to the dispatcher, they should
-// be treated as read-only. Thus, it is safe for applications to read
-// messages from mutiple threads without locking.
-{
-  // The header of the message. The header contains information about
-  // the message sender, and the type of the message. The header is
-  // mainly useful for application level objects.
-  "header":
-    {
-      // A unique identifier for the session which created the message.
-      "session": "<string>",
+// A message is an array with four elements. The details of each element
+// are described in detail below:
+[
+  // The first element in the message is the message header. The header
+  // contains information necessary for routing the message to the 
+  // appropriate session object. The header of the message can never 
+  // be null, and all keys are required. However, certain keys may be
+  // null depending on the 'msg_type'.
+  {
+    // A unique identifier for the session which created the message.
+    // This will be null when the client is establishing a session.
+    "session": "<string>",
 
-      // The username for the originator of the message.
-      "username": "<string>",
-      
-      // A unique identifier for this message.
-      "msg_id": "<string>",
-      
-      // The type of this message. Supported message types are 
-      // enumerated below.
-      "msg_type": "<string>",
+    // The username for the originator of the message. This should
+    // never be null.
+    "username": "<string>",
+    
+    // A unique identifier for this message. This should never be null.
+    "msg_id": "<string>",
+    
+    // The type of this message. The supported message types are 
+    // described individually below.
+    "msg_type": "<string>",
 
-      // The version of the message structure in use.
-      "version": "<string>",
-    }
+    // The version of the message protocol being used. (still in flux)
+    "version": "<string>",
+  },
 
-  // The parent header for this message. This may be null for an 
-  // original message. For a response message, this will be the
-  // header of the original message.
-  "parent_header": null,
+  // The second element in a message is the parent header. This is the 
+  // header of the message which originated this particular message. 
+  // This allows clients to track the progress of a particular request. 
+  // This field may be an empty mapping for an original message.
+  {},
 
-  // The metadata supplied by the implementations. There is formally
-  // no requirement for what must be placed in this object. It is 
-  // entirely implementation defined and may be null.
-  "metadata": null,
+  // The third element in a message is metadata. The metadata of a 
+  // message is entirely implementation defined, but must be an object.
+  {},
 
-  // The content of the message. What exists in the content will be
-  // dependent upon the "msg_type" and "metadata". The supported message
-  // types are listed below and describe what will be included in their
-  // metadata.
-  "content": {},
-}
+  // The fourth element in a message is the content. What exists in the
+  // content will be dependent on, the "msg_type" and "metadata". The 
+  // supported message types are listed below and describe what will be 
+  // included in their metadata.
+  {},
+]
 
 
 // For every request made by a client to the server. The server will
 // respond with a standard reply message where the "msg_type" is the
 // same as the original message and the content is as follows:
-{
-  // The status of the request, either "ok" or "error"
-  "status": "<ok | error",
 
-  // The error message if the status is "error". If the status is
-  // "ok", the message will be an empty string.
-  "message": "<string>",
-}
 
 
 //-----------------------------------------------------------------------------
 // Supported Message Types
 //-----------------------------------------------------------------------------
-//
-// These are the message types currently supported by an Enaml application
-//
-//
+
+
 // "enaml_message"
 // ---------------
 // This is the most common message type passed around in Enaml applications.
@@ -97,6 +92,7 @@
   // The types of actions supported are defined by a given receiver.
   "action": "<string>",
 }
+
 
 // "enaml_request"
 // ---------------
@@ -135,6 +131,39 @@
 }
 
 
+// "enaml_discover"
+// ----------------
+// This type of message is used to discover what views are available
+// on the server. The session field of the header for this type of
+// message may be null.
+//
+// The metadata and content of this message should be null.
+//
+// The content of the reponse to this message is as follows:
+{
+  // The status of the request, either "ok" or "error"
+  "status": "<ok | error",
+
+  // The error message if the status is "error". If the status is
+  // "ok", the message will be an empty string.
+  "message": "<string>",
+
+  // The available sessions that the client may run. This will only
+  // be provided if the status is 'ok'.
+  "sessions": [
+    {
+      // The name of the session. This will be unique amongst all 
+      // available sessions.
+      "name": "<string>",
+
+      // The description of the session. This is a brief description
+      // of what the session will peform when launched.
+      "description": "<string>",
+    }
+  ]
+}
+
+
 // "enaml_begin_session"
 // ---------------------
 // This message is used by a client to start an Enaml session. This is
@@ -146,8 +175,24 @@
 //
 // The content for this message type is as follows:
 {
-  // The version of the Enaml protocol to be used by the session.
-  "version": "<string>",
+  // The name of the session to start for this client. This must be
+  // equal to one of the session names provided by 'enaml_discover'.
+  "name": "<string>"
+}
+//
+// The content of the response to this message is as follows
+{
+  // The status of the request, either "ok" or "error"
+  "status": "<ok | error",
+
+  // The error message if the status is "error". If the status is
+  // "ok", the message will be an empty string.
+  "message": "<string>",
+
+  // The session identifier to use for communicating with the 
+  // session object on the server. This will only be provided if
+  // the status is 'ok'.
+  "session": "<string>",
 }
 
 
@@ -156,4 +201,14 @@
 // This message is used by a client to end its session.
 // 
 // Both the "metadata" and the "content" are null.
+//
+// The content of the response to this message is as follows;
+{
+  // The status of the request, either "ok" or "error"
+  "status": "<ok | error",
+
+  // The error message if the status is "error". If the status is
+  // "ok", the message will be an empty string.
+  "message": "<string>",
+}
 
