47a4ed14578f001557080e9931d4537f58ef9da8
Chris Colbert <sccolbert@gmail.com>
2012-08-01 14:32:49 -0400

    Add the wx ComboBox and ConstraintsWidget

---
+++ enaml/wx/wx_combo_box.py
@@ -0,0 +1,70 @@
+#------------------------------------------------------------------------------
+#  Copyright (c) 2012, Enthought, Inc.
+#  All rights reserved.
+#------------------------------------------------------------------------------
+import wx
+
+from .wx_constraints_widget import WxConstraintsWidget
+
+
+class WxComboBox(WxConstraintsWidget):
+    """ A Wx implementation of an Enaml ComboBox.
+    
+    """
+    def create(self):
+        """ Create the underlying widget
+
+        """
+        self.widget = wx.ComboBox(self.parent_widget, style=wx.CB_READONLY)
+
+    def initialize(self, attrs):
+        """ Initialize the widget's attributes
+
+        """
+        super(WxComboBox, self).initialize(attrs)
+        self.set_items(attrs['items'])
+        self.set_index(attrs['index'])
+        self.widget.Bind(wx.EVT_COMBOBOX, self.on_index_changed)
+
+    #--------------------------------------------------------------------------
+    # Message Handlers
+    #--------------------------------------------------------------------------
+    def on_action_set_index(self, content):
+        """ Handle the 'set_index' action from the Enaml widget.
+
+        """
+        self.set_index(content['index'])
+
+    def on_action_set_items(self, content):
+        """ Handle the 'set_items' action from the Enaml widget.
+
+        """
+        self.set_items(content['items'])
+
+    #--------------------------------------------------------------------------
+    # Signal Handlers
+    #--------------------------------------------------------------------------
+    def on_index_changed(self):
+        """ The signal handler for the index changed signal.
+
+        """
+        content = {'index': self.widget.GetCurrentSelection()}
+        self.send_action('index_changed', content)
+
+    #--------------------------------------------------------------------------
+    # Widget Update Methods
+    #--------------------------------------------------------------------------
+    def set_items(self, items):
+        """ Set the items of the ComboBox.
+
+        """
+        sel = self.widget.GetCurrentSelection()
+        self.widget.SetItems(items)
+        self.widget.SetSelection(sel)
+
+    def set_index(self, index):
+        """ Set the current index of the ComboBox
+
+        """
+        self.widget.SetSelection(index)
+
---
+++ enaml/wx/wx_constraints_widget.py
@@ -0,0 +1,212 @@
+#------------------------------------------------------------------------------
+#  Copyright (c) 2012, Enthought, Inc.
+#  All rights reserved.
+#------------------------------------------------------------------------------
+from casuarius import ConstraintVariable 
+
+from .wx_widget_component import WxWidgetComponent
+
+
+class LayoutBox(object):
+    """ A class which encapsulates a layout box using casuarius 
+    constraint variables.
+
+    The constraint variables are created on an as-needed basis, this
+    allows Enaml widgets to define new constraints and build layouts
+    with them, without having to specifically update this client 
+    code.
+
+    """
+    def __init__(self, name, owner):
+        """ Initialize a LayoutBox.
+
+        Parameters
+        ----------
+        name : str
+            A name to use in the label for the constraint variables in
+            this layout box.
+
+        owner : str
+            The owner id to use in the label for the constraint variables
+            in this layout box.
+
+        """
+        self._name = name
+        self._owner = owner
+        self._primitives = {}
+
+    def primitive(self, name, force_create=True):
+        """ Returns a primitive casuarius constraint variable for the
+        given name.
+
+        Parameters
+        ----------
+        name : str
+            The name of the constraint variable to return.
+
+        force_create : bool, optional
+            If the constraint variable does not yet exist and this 
+            parameter is True, then the constraint variable will be
+            created on-the-fly. If the parameter is False, and the
+            variable does not exist, a ValueError will be raised.
+
+        """
+        primitives = self._primitives
+        try:
+            res = primitives[name]
+        except KeyError:
+            if force_create:
+                label = '{0}_{1}'.format(self._name, self._owner)
+                res = primitives[name] = ConstraintVariable(label)
+            else:
+                msg = 'Constraint variable `{0}` does not exist'
+                raise ValueError(msg.format(name))
+        return res
+
+
+class WxConstraintsWidget(WxWidgetComponent):
+    """ A Wx implementation of an Enaml ConstraintsWidget.
+
+    """
+    #--------------------------------------------------------------------------
+    # Initialization
+    #--------------------------------------------------------------------------
+    def initialize(self, attrs):
+        """ Initialize the attributes of the widget.
+
+        """
+        super(WxConstraintsWidget, self).initialize(attrs)
+        layout = attrs['layout']
+        self.hug = layout['hug']
+        self.resist_clip = layout['resist_clip']
+        self.constraints = layout['constraints']
+        self.layout_box = LayoutBox(type(self).__name__, self.widget_id)
+        
+    #--------------------------------------------------------------------------
+    # Message Handlers
+    #--------------------------------------------------------------------------
+    def on_action_relayout(self, content):
+        """ Handle the 'relayout' action from the Enaml widget.
+
+        """
+        print 'relayout!'
+
+    #--------------------------------------------------------------------------
+    # Layout Handling
+    #--------------------------------------------------------------------------
+    def size_hint_constraints(self):
+        """ Creates the list of size hint constraints for this widget.
+
+        This method using the provided size hint of the widget and the
+        policies for 'hug' and 'resist_clip' to generate casuarius 
+        LinearConstraint objects which respect the size hinting of the
+        widget.
+
+        If the size hint of the underlying widget is not valid, then
+        no constraints will be generated.
+
+        Returns
+        -------
+        result : list
+            A list of casuarius LinearConstraint instances.
+
+        """
+        cns = []
+        push = cns.append
+        hint = self.layout_size_hint()
+        if hint.IsFullySpecified():
+            width_hint = hint.width
+            height_hint = hint.height
+            primitive = self.layout_box.primitive
+            width = primitive('width')
+            height = primitive('height')
+            hug_width, hug_height = self.hug
+            resist_width, resist_height = self.resist_clip
+            if width_hint >= 0:
+                if hug_width != 'ignore':
+                    cn = (width == width_hint) | hug_width
+                    push(cn)
+                if resist_width != 'ignore':
+                    cn = (width >= width_hint) | resist_width
+                    push(cn)
+            if height_hint >= 0:
+                if hug_height != 'ignore':
+                    cn = (height == height_hint) | hug_height
+                    push(cn)
+                if resist_height != 'ignore':
+                    cn = (height >= height_hint) | resist_height
+                    push(cn)
+        return cns
+
+    def layout_size_hint(self):
+        """ Returns the size hint to use in layout computation.
+
+        The default implementation returns the result of calling the
+        widget.GetBestSize() method. If a subclass requires more 
+        control, it should override this method.
+
+        Returns
+        -------
+        result : wxSize
+            The size hint to use in layout computations for the widget.
+
+        """
+        return self.widget.GetBestSize()
+
+    def update_layout_geometry(self, dx, dy):
+        """ A method which can be called during a layout pass to compute
+        the new layout geometry rect and update the underlying widget.
+
+        Parameters
+        ----------
+        dx : int
+            The offset of the parent widget from the computed origin
+            of the layout. This amount should be subtracted from the 
+            computed layout 'x' amount.
+
+        dy : int
+            The offset of the parent widget from the computed origin
+            of the layout. This amount should be subtracted from the
+            computed layout 'y' amount.
+
+        Returns
+        -------
+        result : (x, y)
+            The computed layout 'x' and 'y' amount, unadjusted with
+            the given dx and dy.
+
+        """
+        primitive = self.layout_box.primitive
+        x = int(round(primitive('left', False).value))
+        y = int(round(primitive('top', False).value))
+        width = int(round(primitive('width', False).value))
+        height = int(round(primitive('height', False).value))
+        self.set_layout_geometry(x - dx, y - dy, width, height)
+        return (x, y)
+    
+    def set_layout_geometry(self, x, y, width, height):
+        """ Updates the layout geometry for the widget.
+
+        The default implementation sets the geometry by calling the
+        widget.SetDimensions(...) method. If a subclass requires more
+        control, it should override this method.
+
+        Parameters
+        ----------
+        x : int
+            The x position of the widget, relative to the origin of
+            its parent.
+
+        y : int
+            The y position of the widget, relative to the origin of
+            its parent.
+
+        width : int
+            The width of the widget.
+
+        height : int
+            The height of the widget.
+
+        """
+        self.widget.SetDimensions(x, y, width, height)
+
