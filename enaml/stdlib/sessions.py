6ac564a50768844d79812a9fda861c3823c2b9ec
Corran Webster <cwebster@enthought.com>
2012-08-03 10:48:15 -0500

    Add simple single session app, add wx support to enaml-run.

--- enaml/stdlib/sessions.py
+++ enaml/stdlib/sessions.py
@@ -65,7 +65,15 @@ def ComponentSession(component, name=None, description=None):
 
 def _view_handler(name, description):
     """ Utility decorator that pre-fills session name and description
+
+    Parameters
+    ----------
+    name : string
+        An optional unique, human-friendly name.
     
+    description : string
+        An optional brief description of the session.
+   
     """
     def decorator(component):
         Session = ComponentSession(component, name, description)
@@ -75,7 +83,25 @@ def _view_handler(name, description):
 
 def view_handler(*args, **kwargs):
     """ Decorator that creates a handler factory for a view factory.
-     
+    
+    This can be used either as::
+        
+        @view_handler
+        def view(...):
+            ...
+            return [View1(...), View2(...)]
+    
+    or for a component::
+    
+        handler = view_handler(Main)
+    
+    or::
+    
+        @view_handler('my-view', 'This is my view')
+        def view(...):
+            ...
+            return [View1(...), View2(...)]
+    
     """
     if len(args) == 1 and callable(args[0]):
         component = args[0]
@@ -84,4 +110,38 @@ def view_handler(*args, **kwargs):
         return handler_factory
     else:
         return _view_handler(*args, **kwargs)
+
+
+def single_view_app(component, session_name, session_description, *args,
+        **kwargs):
+    """ Utility function which creates an application from a component
+    
+    This is suitable for use in simple applications, particularly "traditional"
+    GUI applications running a single main view in a single process.
+    
+    Parameters
+    ----------
+    component : Enaml component
+        The component to wrap into a session
+
+    name : string
+        An optional unique, human-friendly name.
+    
+    description : string
+        An optional brief description of the session.
+    
+    args : tuple
+        Optional positional arguments to be passed to the component by the
+        Session when opened.
         
+    kwargs : dict
+        Optional keyword arguments to be passed to the component by the
+        Session when opened.
+    
+    """
+    from ..application import Application
+    handler = view_handler(component)
+    app = Application([handler(session_name, session_description, *args,
+        **kwargs)])
+    return app
+    
