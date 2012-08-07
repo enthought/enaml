d623792b6549d6c15402d9ae07ec16bbda4969e2
Chris Colbert <sccolbert@gmail.com>
2012-08-07 10:25:16 -0400

    update the Field for new validator

--- enaml/widgets/field.py
+++ enaml/widgets/field.py
@@ -2,55 +2,33 @@
 #  Copyright (c) 2012, Enthought, Inc.
 #  All rights reserved.
 #------------------------------------------------------------------------------
-from traits.api import (
-    HasTraits, Bool, Int, Unicode, Enum, List, Dict, Instance, ReadOnly
-)
+from traits.api import Bool, Int, Unicode, Enum, List, Instance
 
-from enaml.core.trait_types import EnamlEvent
 from enaml.validation import Validator
 
 from .constraints_widget import ConstraintsWidget
 
 
-class TextEditEvent(HasTraits):
-    """ An event object that is emitted when the client edits text.
-
-    """
-    #: The original unicode text in the field pre-edit.
-    orig_text = ReadOnly
-
-    #: The unicode text that was edited in the client control. This
-    #: text can be modified to change what is applied to the field.
-    edit_text = Unicode
-
-    #: Whether or not the edit text is considered valid. This can
-    #: be modified to change whether or not the edit text is valid.
-    edit_valid = Bool
-
-
 class Field(ConstraintsWidget):
-    """ A single-line editable text widget.
+    """ A single line editable text widget.
 
     """
     #: The unicode text to display in the field.
     text = Unicode
 
-    #: An event fired when text has been edited by the client. The 
-    #: payload will be an instance of TextEditEvent and can be used
-    #: to control the subsequent update to the 'text' attribute.
-    text_edited = EnamlEvent
-
     #: The mask to use for text input. 
     #: TODO - describe and implement this mask
     mask = Unicode
 
-    #: The validator to use with the field. Note that the validator will
-    #: executed client-side.
+    #: The validator to use for this field. If the validator provides
+    #: a client side validator, then text will only be submitted if it
+    #: passes that validator.
     validator = Instance(Validator)
 
-    #: The actions which should cause the client to send a text edited
-    #: event back to the server if the text in the control has changed.
-    edit_triggers = List(
+    #: The list of actions which should cause the client to submit its
+    #: text to the server for validation and update. The currently 
+    #: supported values are 'lost_focus' and 'return_pressed'.
+    submit_triggers = List(
         Enum('lost_focus', 'return_pressed'), ['lost_focus', 'return_pressed']
     )
 
@@ -79,13 +57,13 @@ class Field(ConstraintsWidget):
     # Initialization
     #--------------------------------------------------------------------------
     def snapshot(self):
-        """ Returns the dict of creation attributes for the control.
+        """ Returns the snapshot dict for the field.
 
         """
         snap = super(Field, self).snapshot()
         snap['text'] = self.text
-        snap['validator'] = self._snap_validator()
-        snap['edit_triggers'] = self.edit_triggers
+        snap['validator'] = self._client_validator()
+        snap['submit_triggers'] = self.submit_triggers
         snap['placeholder'] = self.placeholder
         snap['echo_mode'] = self.echo_mode
         snap['max_length'] = self.max_length
@@ -103,60 +81,53 @@ class Field(ConstraintsWidget):
         )
         self.publish_attributes(*attrs)
         self.on_trait_change(self._send_validator, 'validator')
-        self.on_trait_change(self._send_edit_triggers, 'edit_triggers[]')
+        self.on_trait_change(self._send_submit_triggers, 'submit_triggers[]')
 
     #--------------------------------------------------------------------------
     # Private API
     #--------------------------------------------------------------------------
-    def _snap_validator(self):
-        """ A private method which returns the serializable form of the
-        current validator.
+    def _client_validator(self):
+        """ A private method which returns the current client validator.
 
         """
-        validator = self.validator
-        return validator.as_dict() if validator is not None else None
-
-    #--------------------------------------------------------------------------
-    # Message Handling
-    #--------------------------------------------------------------------------
-    def on_action_text_edited(self, content):
-        """ Handle the 'text_edited' action from the client widget.
-
-        """
-        orig_text = self.text
-        edit_text = content['text']
-        edit_valid = content['valid']
-        
-        validator = self.validator
-        if validator is not None:
-            edit_text, edit_valid = validator.validate(
-                orig_text, edit_text, edit_valid
-            )
-        
-        event = TextEditEvent(
-            orig_text=orig_text, edit_text=edit_text, edit_valid=edit_valid,
-        )
-        self.text_edited(event)
-        
-        if event.edit_valid:
-            # If the edit is valid and text differs from the original
-            # edit text, we push an update to the client.
-            if edit_text != event.edit_text:
-                content = {'text': event.edit_text}
-                self.send_action('set_text', content)
-            self.set_guarded(text=event.edit_text)
+        v = self.validator
+        return v.client_validator() if v is not None else None
 
     def _send_validator(self):
         """ Send the new validator to the client widget.
 
         """
-        content = {'validator': self._snap_validator()}
+        content = {'validator': self._client_validator()}
         self.send_action('set_validator', content)
 
-    def _send_edit_triggers(self):
-        """ Send the new edit triggers to the client widget.
+    def _send_submit_triggers(self):
+        """ Send the new submit triggers to the client widget.
 
         """
-        content = {'edit_triggers': self.edit_triggers}
-        self.send_action('set_edit_triggers', content)
+        content = {'submit_triggers': self.submit_triggers}
+        self.send_action('set_submit_triggers', content)
+
+    #--------------------------------------------------------------------------
+    # Message Handling
+    #--------------------------------------------------------------------------
+    def on_action_submit_text(self, content):
+        """ Handle the 'submit_text' action from the client widget.
+
+        """
+        edit_text = content['text']
+        validator = self.validator
+        if validator is not None:
+            text, valid = validator.validate(edit_text, self)
+        else:
+            text, valid = edit_text, True
+        if valid:
+            # If the new text differs from the original edit text,
+            # we push an update to the client.
+            if text != edit_text:
+                content = {'text': text}
+                self.send_action('set_text', content)
+            self.set_guarded(text=text)
+        else:
+            # XXX notify the client that server validation failed.
+            pass
 
