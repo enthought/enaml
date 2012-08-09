5b8217305366c2e5e554c3c8cbca07163de7e47d
Chris Colbert <sccolbert@gmail.com>
2012-08-08 20:21:50 -0400

    update DockPane and MainWindow

--- enaml/widgets/dock_pane.py
+++ enaml/widgets/dock_pane.py
@@ -2,9 +2,7 @@
 # Copyright (c) 2012, Enthought, Inc.
 # All rights reserved.
 #------------------------------------------------------------------------------
-from traits.api import (
-    List, Enum, Unicode, Bool, Instance, Property, cached_property,
-)
+from traits.api import List, Enum, Unicode, Bool,  Property, cached_property
 
 from enaml.core.trait_types import EnamlEvent
 
@@ -13,39 +11,75 @@ from .widget_component import WidgetComponent
 
 
 class DockPane(WidgetComponent):
-    """ A DockPane is a widget which holds a single child Container
-    and can be docked in arbitrary locations in a MainWindow.
+    """ A widget which can be docked in a MainWindow.
+
+    A DockPane is a widget which can be docked in designated dock areas
+    in a MainWindow. I can have at most a single child widget which is 
+    an instance of Container.
 
     """
     #: The title of the dock pane.
     title = Unicode
 
-    #: Whether or not the dock area is closable via a close button.
+    #: The orientation of the title bar.
+    title_bar_orientation = Enum('horizontal', 'vertical')
+
+    #: Whether or not the dock pane is closable via a close button.
     closable = Bool(True)
 
-    #: Whether or not the dock area is movable from its current position.
+    #: Whether or not the dock pane is movable by the user.
     movable = Bool(True)
 
-    #: Whether or not the dock are is floatable as a separate window.
+    #: Whether or not the dock can be floated as a separate window.
     floatable = Bool(True)
     
     #: A boolean indicating whether or not the dock pane is floating.
     floating = Bool(False)
 
-    #: The orientation of the title bar.
-    title_bar_orientation = Enum('horizontal', 'vertical')
-
-    #: The dock area in which the pane currently resides.
+    #: The dock area in the MainWindow where the pane is docked.
     dock_area = Enum('left', 'right', 'top', 'bottom')
 
-    #: The areas in the main window in which this dock area is allowed
-    #: to be docked via user interaction.
+    #: The dock areas in the MainWindow where the pane can be docked 
+    #: by the user. Note that this does not preclude the pane from 
+    #: docked manually via the 'dock_area' attribute.
     allowed_dock_areas = List(
         Enum('left', 'right', 'top', 'bottom', 'all'), value=['all'],
     )
 
     #: A read only property which returns the dock widget for the pane.
     dock_widget = Property(depends_on='children[]')
+
+    #: An event fired when the user closes the page by clicking on 
+    #: the tab's close button. This event is fired by the parent 
+    #: MainWindow when the tab is closed. This event has no payload.
+    closed = EnamlEvent
+
+    #--------------------------------------------------------------------------
+    # Initialization
+    #--------------------------------------------------------------------------
+    def snapshot(self):
+        """ Returns the snapshot dict for the DockPane.
+
+        """
+        snap = super(DockPane, self).snapshot()
+        snap['dock_widget_id'] = self._snap_dock_widget_id()
+        snap['title'] = self.title
+        snap['title_bar_orientation'] = self.title_bar_orientation
+        snap['closable'] = self.closable
+        snap['movable'] = self.movable
+        snap['floatable'] = self.floatable
+        snap['floating'] = self.floating
+        snap['dock_area'] = self.dock_area
+        snap['allowed_dock_areas'] = self.allowed_dock_areas
+        return snap
+
+    def bind(self):
+        super(DockPane, self).bind()
+        attrs = (
+            'title', 'title_bar_orientation', 'closable', 'movable',
+            'floatable', 'floating', 'dock_area', 'allowed_dock_areas'
+        )
+        self.publish_attributes(*attrs)
 
     #--------------------------------------------------------------------------
     # Private API
@@ -57,16 +91,15 @@ class DockPane(WidgetComponent):
         Returns
         -------
         result : Container or None
-            The dock widget for the Pane, or None if no dock widget
-            is provided.
+            The dock widget for the Pane, or None if not provided.
 
         """
         for child in self.children:
             if isinstance(child, Container):
                 return child
 
-    def _snap_dock_widget(self):
-        """ Returns the serializable target ids for the dock widget.
+    def _snap_dock_widget_id(self):
+        """ Returns the widget id for the dock widget or None.
 
         """
         dock_widget = self.dock_widget
@@ -74,13 +107,40 @@ class DockPane(WidgetComponent):
             return dock_widget.widget_id
 
     #--------------------------------------------------------------------------
-    # Initialization
+    # Message Handling
     #--------------------------------------------------------------------------
-    def snapshot(self):
-        """ Returns the snapshot dict for the DockPane.
+    def on_action_floating_changed(self, content):
+        """ Handle the 'floating_changed' action from the client 
+        widget.
 
         """
-        snap = super(DockPane, self).snapshot()
-        snap['dock_widget'] = self._snap_dock_widget()
-        return snap
+        self.set_guarded(floating=content['floating'])
+
+    def on_action_dock_area_changed(self, content):
+        """ Handle the 'dock_area_changed' action from the client 
+        widget.
+
+        """
+        self.set_guarded(dock_area=content['dock_area'])
+    
+    #--------------------------------------------------------------------------
+    # Public API
+    #--------------------------------------------------------------------------
+    def open(self):
+        """ A convenience method for calling 'open_dock_pane' on the
+        parent MainWindow, passing this pane as an argument.
+
+        """
+        parent = self.parent
+        if parent is not None:
+            parent.open_dock_pane(self)
+
+    def close(self):
+        """ A convenience method for calling 'close_dock_pane' on the
+        parent MainWindow, passing this page as an argument.
+
+        """ 
+        parent = self.parent
+        if parent is not None:
+            parent.close_dock_pane(self)
 
--- enaml/widgets/main_window.py
+++ enaml/widgets/main_window.py
@@ -4,33 +4,49 @@
 #------------------------------------------------------------------------------
 from traits.api import Property, cached_property
 
+from enaml.core.trait_types import EnamlEvent
+
 from .dock_pane import DockPane
 from .window import Window
 
 
 class MainWindow(Window):
-    """ A top-level main window widget.
+    """ A top level main window widget.
 
-    MainWindow widgets are top-level widgets which provide various frame 
-    decorations and other window related functionality. A window may 
-    optionally contain a menubar, any number of toolbars, a status bar,
-    and dock panes. A window can have at most one central widget child
-    which will be expanded to fit the available space of the window.
-    
-    Sizing information relates to the size of the central widget rather
-    than the overall size of the window. That is, specifying a minimum
-    size for a MainWindow is akin to specifying a minimum size for the
-    central widget. The space consumed by dock panes and menus is in
-    addition to this space.
+    MainWindow widgets are top level widgets which provide additional
+    functionality beyond frame decoration. A MainWindow may optionally
+    contain a MenuBar, any number of ToolBars, a StatusBar, and any
+    number of DockPanes. Like Window, a MainWindow can have at most one
+    central Container widget, which will be expanded to fit int the 
+    available space.
 
     """
-    #: A read only property which returns the MenuBar in use for this
-    #: MainWindow.
+    #: A read only property which returns the window's MenuBar.
     # menu_bar = Property(depends_on='children[]')
 
-    #: A read only property which returns the list of DockPanes in use
-    #: for this MainWindow.
+    #: A read only property which returns the window's StatusBar.
+    # status_bar = Property(depends_on='children[]')
+
+    #: A read only property which returns the window's ToolBars.
+    # tool_bars = Property(depends_on='children[]')
+
+    #: A read only property which returns the window's DockPanes.
     dock_panes = Property(depends_on='children[]')
+
+    #: An event fired when the user closes a dock pane by clicking on 
+    #: its close button. The content will be the dock pane object.
+    dock_pane_closed = EnamlEvent
+
+    #--------------------------------------------------------------------------
+    # Initialization 
+    #--------------------------------------------------------------------------
+    def snapshot(self):
+        """ Returns the snapshot for the MainWindow.
+
+        """
+        snap = super(MainWindow, self).snapshot()
+        snap['dock_pane_ids'] = self._snap_dock_pane_ids()
+        return snap
 
     #--------------------------------------------------------------------------
     # Private API
@@ -42,26 +58,66 @@ class MainWindow(Window):
         Returns
         -------
         result : list
-            The list of dock panes in use on this MainWindow.
+            The list of DockPane instances defined as children of this
+            MainWindow.
 
         """
         isinst = isinstance
-        return [child for child in self.children if isinst(child, DockPane)]
+        panes = (child for child in self.children if isinst(child, DockPane))
+        return tuple(panes)
 
-    def _snap_dock_panes(self):
-        """ Returns the serializable target ids for the dock panes.
+    def _snap_dock_pane_ids(self):
+        """ Returns the widget ids of window's dock panes.
 
         """
         return [pane.widget_id for pane in self.dock_panes]
-
+    
     #--------------------------------------------------------------------------
-    # Initialization 
+    # Message Handling
     #--------------------------------------------------------------------------
-    def snapshot(self):
-        """ Return the snapshot for a MainWindow.
+    def on_action_dock_pane_closed(self, content):
+        """ Handle the 'dock_pane_closed' action from the client widget.
 
         """
-        snap = super(MainWindow, self).snapshot()
-        snap['dock_panes'] = self._snap_dock_panes()
-        return snap
-    
+        widget_id = content['widget_id']
+        for dock_pane in self.dock_panes:
+            if dock_pane.widget_id == widget_id:
+                self.dock_pane_closed(dock_pane)
+                dock_pane.closed()
+                return
+
+    #--------------------------------------------------------------------------
+    # Public API
+    #--------------------------------------------------------------------------
+    def open_dock_pane(self, dock_pane):
+        """ Open the given dock pane if it isn't already open.
+
+        Parameters
+        ----------
+        dock_pane : DockPane
+            The DockPane instance to open. It must be a child of this 
+            MainWindow.
+
+        """
+        if dock_pane not in self.dock_panes:
+            msg = 'DockPane is not a child of the MainWindow'
+            raise ValueError(msg)
+        content = {'widget_id': dock_pane.widget_id}
+        self.send_action('open_dock_pane', content)
+
+    def close_dock_pane(self, dock_pane):
+        """ Close the given dock pane if it isn't already open.
+
+        Parameters
+        ----------
+        dock_pane : DockPane
+            The DockPane instance to close. It must be a child of this 
+            MainWindow.
+
+        """
+        if dock_pane not in self.dock_panes:
+            msg = 'DockPane is not a child of the MainWindow'
+            raise ValueError(msg)
+        content = {'widget_id': dock_pane.widget_id}
+        self.send_action('close_dock_pane', content)
+
