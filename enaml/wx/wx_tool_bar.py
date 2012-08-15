d24c80bde0b6963cc3e58bf59fea1e1a6c981b78
Chris Colbert <sccolbert@gmail.com>
2012-08-15 17:17:53 -0400

    Start updating the toolbar for the non-aui version. Not yet finished.

--- enaml/wx/wx_tool_bar.py
+++ enaml/wx/wx_tool_bar.py
@@ -8,33 +8,16 @@ from .wx_action import wxAction, EVT_ACTION_CHANGED
 from .wx_action_group import wxActionGroup
 from .wx_constraints_widget import WxConstraintsWidget
 from .wx_main_window import wxMainWindow
-from .wx_upstream import aui
 
 
-#: A mapping from Enaml dock areas to wx aui dock area enums
+#: A mapping from Enaml dock orientation to wx dock position
 _DOCK_AREA_MAP = {
-    'top': aui.AUI_DOCK_TOP,
-    'right': aui.AUI_DOCK_RIGHT,
-    'bottom': aui.AUI_DOCK_BOTTOM,
-    'left': aui.AUI_DOCK_LEFT,
+    'left': wx.TB_LEFT,
+    'right': wx.TB_RIGHT,
+    'top': wx.TB_TOP,
+    'bottom': wx.TB_BOTTOM,
 }
 
-#: A mapping from wx aui dock area enums to Enaml dock areas.
-_DOCK_AREA_INV_MAP = {
-    aui.AUI_DOCK_TOP: 'top',
-    aui.AUI_DOCK_RIGHT: 'right',
-    aui.AUI_DOCK_BOTTOM: 'bottom',
-    aui.AUI_DOCK_LEFT: 'left',
-}
-
-#: A mapping from Enaml allowed dock areas to wx direction enums.
-_ALLOWED_AREAS_MAP = {
-    'top': wx.TOP,
-    'right': wx.RIGHT,
-    'bottom': wx.BOTTOM,
-    'left': wx.LEFT,
-    'all': wx.ALL,
-}
 
 #: A mapping from Enaml orientation to wx Orientation
 _ORIENTATION_MAP = {
@@ -43,8 +26,8 @@ _ORIENTATION_MAP = {
 }
 
 
-class wxToolBar(aui.AuiToolBar):
-    """ An AuiToolBar subclass which handles wxAction instances.
+class wxToolBar(wx.ToolBar):
+    """ A wx.ToolBar subclass which handles wxAction instances.
 
     """
     def __init__(self, *args, **kwargs):
@@ -60,48 +43,11 @@ class wxToolBar(aui.AuiToolBar):
         super(wxToolBar, self).__init__(*args, **kwargs)
         self._all_items = []
         self._actions_map = {}
-        self._movable = True
-        self._floatable = True
-        self._floating = False
-        self._dock_area = aui.AUI_DOCK_LEFT
-        self._allowed_dock_areas = wx.ALL
         self.Bind(wx.EVT_MENU, self.OnMenu)
 
     #--------------------------------------------------------------------------
     # Private API
     #--------------------------------------------------------------------------
-    def _FindPaneManager(self):
-        """ Find the pane manager for this tool bar.
-
-        Returns
-        -------
-        result : AuiManager or None
-            The AuiManager for this tool bar, or None if not found.
-            If the tool bar is not a child of a wxMainWindow, then
-            there will be no pane manager.
-
-        """
-        event = aui.AuiManagerEvent(aui.wxEVT_AUI_FIND_MANAGER)
-        self.ProcessEvent(event)
-        return event.GetManager()
-
-    def _PaneInfoOperation(self, closure):
-        """ A private method which will run the given closure if there 
-        is a valid pane info object for this tool bar.
-
-        """
-        manager = self._FindPaneManager()
-        if manager is not None:
-            pane = manager.GetPane(self)
-            if pane.IsOk():
-                closure(pane)
-                # Do some shenannigans to make sure the orientation and
-                # and size of the toolbar is consistent. There shouldn't 
-                # be a need to do this; this aui code is really bad...
-                manager.SwitchToolBarOrientation(pane)
-                pane.BestSize(self.GetClientSize())
-                manager.Update()
-
     def _CreationActionItem(self, action):
         """ Create an AuiToolBarItem for the given action.
 
@@ -116,34 +62,7 @@ class wxToolBar(aui.AuiToolBar):
             The tool bar item instance to add to the toolbar.
 
         """
-        item = aui.AuiToolBarItem()
-        item.SetLabel(action.GetText())
-        item.SetShortHelp(action.GetToolTip())
-        item.SetLongHelp(action.GetStatusTip())
-        item.SetId(action.GetId())
-
-        state = item.GetState()
-
-        if action.IsSeparator():
-            item.SetKind(wx.ITEM_SEPARATOR)
-        else:
-            if action.IsCheckable():
-                item.SetKind(wx.ITEM_CHECK)
-                if action.IsChecked():
-                    state |= aui.AUI_BUTTON_STATE_CHECKED
-            else:
-                item.SetKind(wx.ITEM_NORMAL)
-
-        if not action.IsEnabled():
-            state |= aui.AUI_BUTTON_STATE_DISABLED
         
-        item.SetState(state)
-
-        # Extra initing not dependent on the action.
-        item.SetHasDropDown(False)
-        item.SetSticky(False)
-        item.SetOrientation(self._tool_orientation)
-        return item
 
     def OnMenu(self, event):
         """ The event handler for the EVT_MENU event.
@@ -151,11 +70,12 @@ class wxToolBar(aui.AuiToolBar):
         This handler maps the event to the appropriate wxAction.
 
         """
-        action = wxAction.FindById(event.GetId())
-        if action is not None:
-            if action.IsCheckable():
-                action.SetChecked(event.Checked())
-            action.Trigger()
+        print 'tb menu event'
+        # action = wxAction.FindById(event.GetId())
+        # if action is not None:
+        #     if action.IsCheckable():
+        #         action.SetChecked(event.Checked())
+        #     action.Trigger()
 
     def OnActionChanged(self, event):
         """ The event handler for the EVT_ACTION_CHANGED event.
@@ -165,93 +85,62 @@ class wxToolBar(aui.AuiToolBar):
         the associated tool bar item.
 
         """
-        event.Skip()
-        action = event.GetEventObject()
-        item = self._actions_map.get(action)
+        # event.Skip()
+        # action = event.GetEventObject()
+        # item = self._actions_map.get(action)
 
-        # Fist, check for a visibility change. This requires adding or 
-        # removing the item from the tool bar and the actions map.
-        visible = action.IsVisible()
-        if visible != bool(item):
-            if visible:
-                new_item = self._CreationActionItem(action)
-                index = self._all_items.index(action)
-                index = min(index, len(self._actions_map))
-                self.InsertItem(index, item)
-                self._actions_map[action] = new_item
-                self.Realize()
-            else:
-                self.DeleteTool(item.GetId())
-                self.Realize()
-                del self._actions_map[action]
-            self.Refresh()
-            return
+        # # Fist, check for a visibility change. This requires adding or 
+        # # removing the item from the tool bar and the actions map.
+        # visible = action.IsVisible()
+        # if visible != bool(item):
+        #     if visible:
+        #         new_item = self._CreationActionItem(action)
+        #         index = self._all_items.index(action)
+        #         index = min(index, len(self._actions_map))
+        #         self.InsertItem(index, item)
+        #         self._actions_map[action] = new_item
+        #         self.Realize()
+        #     else:
+        #         self.DeleteTool(item.GetId())
+        #         self.Realize()
+        #         del self._actions_map[action]
+        #     self.Refresh()
+        #     return
 
-        # If the item is invisible, there is nothing to update.
-        if not item:
-            return
+        # # If the item is invisible, there is nothing to update.
+        # if not item:
+        #     return
 
-        # Handle a separator action.
-        if action.IsSeparator():
-            item.SetKind(wx.ITEM_SEPARATOR)
-            self.Refresh()
-            return
+        # # Handle a separator action.
+        # if action.IsSeparator():
+        #     item.SetKind(wx.ITEM_SEPARATOR)
+        #     self.Refresh()
+        #     return
 
-        # All other state is updated in-place
-        item.SetLabel(action.GetText())
-        item.SetShortHelp(action.GetToolTip())
-        item.SetLongHelp(action.GetStatusTip())
-        state = item.GetState()
-        if action.IsCheckable():
-            item.SetKind(wx.ITEM_CHECK)
-            if action.IsChecked():
-                state |= aui.AUI_BUTTON_STATE_CHECKED
-            else:
-                state &= ~aui.AUI_BUTTON_STATE_CHECKED
-        else:
-            state &= ~aui.AUI_BUTTON_STATE_CHECKED
-            item.SetKind(wx.ITEM_NORMAL)
-        if not action.IsEnabled():
-            state |= aui.AUI_BUTTON_STATE_DISABLED
-        else:
-            state &= ~aui.AUI_BUTTON_STATE_DISABLED
-        item.SetState(state) 
-        self.Refresh()
+        # # All other state is updated in-place
+        # item.SetLabel(action.GetText())
+        # item.SetShortHelp(action.GetToolTip())
+        # item.SetLongHelp(action.GetStatusTip())
+        # state = item.GetState()
+        # if action.IsCheckable():
+        #     item.SetKind(wx.ITEM_CHECK)
+        #     if action.IsChecked():
+        #         state |= aui.AUI_BUTTON_STATE_CHECKED
+        #     else:
+        #         state &= ~aui.AUI_BUTTON_STATE_CHECKED
+        # else:
+        #     state &= ~aui.AUI_BUTTON_STATE_CHECKED
+        #     item.SetKind(wx.ITEM_NORMAL)
+        # if not action.IsEnabled():
+        #     state |= aui.AUI_BUTTON_STATE_DISABLED
+        # else:
+        #     state &= ~aui.AUI_BUTTON_STATE_DISABLED
+        # item.SetState(state) 
+        # self.Refresh()
 
     #--------------------------------------------------------------------------
     # Public API
     #--------------------------------------------------------------------------
-    def MakePaneInfo(self):
-        """ Create a new AuiPaneInfo object for this tool bar.
-
-        This is called by the wxMainWindow when it adds this tool bar
-        to its internal layout for the first time.
-        
-        Returns
-        -------
-        result : AuiPaneInfo
-            An initialized AuiPaneInfo object for this tool bar.
-
-        """
-        info = aui.AuiPaneInfo().ToolbarPane()
-
-        info.Movable(self.GetMovable())
-        info.Floatable(self.GetFloatable())
-        info.Direction(self.GetDockArea())
-
-        areas = self.GetAllowedDockAreas()
-        info.TopDockable(bool(areas & wx.TOP))
-        info.RightDockable(bool(areas & wx.RIGHT))
-        info.LeftDockable(bool(areas & wx.LEFT))
-        info.BottomDockable(bool(areas & wx.BOTTOM))
-
-        if self.GetFloating():
-            info.Float()
-        else:
-            info.Dock()
-
-        return info
-
     def AddAction(self, action):
         """ Add an action to the tool bar.
 
@@ -266,184 +155,19 @@ class wxToolBar(aui.AuiToolBar):
         all_items = self._all_items
         if action not in all_items:
             all_items.append(action)
-            item = self._CreationActionItem(action)
-            self.AddItem(item)
-            self._actions_map[action] = item
-            action.Bind(EVT_ACTION_CHANGED, self.OnActionChanged)
-
-    def SetToolBarOrientation(self, orientation):
-        """ Set the toolbar orientation.
-
-        Set the orientation of the toolbar. This will take effect if the
-        parent of the toolbar is not a wxMainWindow.
-
-        """
-        # This method is not named SetOrientation, because that exists 
-        # on the superclass with a null implementation, yet that method
-        # is still called by various methods on the parent class. So,
-        # we just use a unique name to eliminate the confusion.
-        if not isinstance(self.GetParent(), wxMainWindow):
-            flags = self.GetAGWWindowStyleFlag()
-            if orientation == wx.HORIZONTAL:
-                flags &= ~aui.AUI_TB_VERTICAL
+            bmp = wx.EmptyBitmap(0, 0)
+            #bmp = wx.BitmapFromImage(wx.EmptyImage(1, 1))
+            action_id = action.GetId()
+            text = action.GetText()
+            text = text or 'action_%d' % action_id
+            if action.IsSeparator():
+                self.AddSeparator()
             else:
-                flags |= aui.AUI_TB_VERTICAL
-            self.SetAGWWindowStyleFlag(flags)
-
-    def GetMovable(self):
-        """ Get the movable state of the tool bar.
-
-        Returns
-        -------
-        result : bool
-            Whether or not the tool bar is movable.
-
-        """
-        return self._movable
-
-    def SetMovable(self, movable):
-        """ Set the movable state of the tool bar.
-
-        This will only have an effect if this tool bar is a child of a
-        wxMainWindow.
-
-        Parameters
-        ----------
-        movable : bool
-            Whether or not the tool bar is movable.
-
-        """
-        if self._movable != movable:
-            self._movable = movable
-            def closure(pane):
-                pane.Movable(movable)
-            self._PaneInfoOperation(closure)
-
-    def GetFloatable(self):
-        """ Get the floatable state of the tool bar.
-
-        Returns
-        -------
-        result : bool
-            Whether or not the tool bar is floatable.
-
-        """
-        return self._floatable
-
-    def SetFloatable(self, floatable):
-        """ Set the floatable state of the tool bar.
-
-        This will only have an effect if this tool bar is a child of a
-        wxMainWindow.
-
-        Parameters
-        ----------
-        floatable : bool
-            Whether or not the tool bar is floatable.
-
-        """
-        if self._floatable != floatable:
-            self._floatable = floatable
-            def closure(pane):
-                pane.Floatable(floatable)
-            self._PaneInfoOperation(closure)
-
-    def GetFloating(self):
-        """ Get the floating state of the tool bar.
-
-        Returns
-        -------
-        result : bool
-            Whether or not the tool bar is floating.
-
-        """
-        return self._floating
-
-    def SetFloating(self, floating):
-        """ Set the floating state of the tool bar.
-
-        This will only have an effect if this tool bar is a child of a
-        wxMainWindow.
-
-        Parameters
-        ----------
-        floating : bool
-            Whether or not the tool bar should be floating.
-
-        """
-        if self._floating != floating:
-            self._floating = floating
-            def closure(pane):
-                if floating:
-                    pane.Float()
-                else:
-                    pane.Dock()
-            self._PaneInfoOperation(closure)
-
-    def GetDockArea(self):
-        """ Get the current dock area of the tool bar.
-
-        Returns
-        -------
-        result : int
-            The current dock area of the tool bar. One of the wx enums 
-            LEFT, RIGHT, TOP, or BOTTOM.
-
-        """
-        return self._dock_area
-
-    def SetDockArea(self, dock_area):
-        """ Set the dock area for the tool bar.
-
-        This will only have an effect if this tool bar is a child of a
-        wxMainWindow.
-
-        Parameters
-        ----------
-        dock_area : int
-            The dock area for the tool bar. One of the wx enums LEFT, 
-            RIGHT, TOP, or BOTTOM.
-
-        """
-        if self._dock_area != dock_area:
-            self._dock_area = dock_area
-            def closure(pane):
-                pane.Direction(dock_area)
-            self._PaneInfoOperation(closure)
-
-    def GetAllowedDockAreas(self):
-        """ Get the allowed dock areas for the tool bar.
-
-        Returns
-        -------
-        result : int
-            The allowed dock areas for the tool bar. One of the wx enums
-            LEFT, RIGHT, TOP, BOTTOM, or ALL.
-
-        """
-        return self._allowed_dock_areas
-
-    def SetAllowedDockAreas(self, dock_areas):
-        """ Set the allowed dock areas for the tool bar.
-
-        This will only have an effect if this tool bar is a child of a
-        wxMainWindow.
-
-        Parameters
-        ----------
-        dock_areas : int
-            The allowed dock areas for the tool bar. One of the wx enums
-            LEFT, RIGHT, TOP, BOTTOM, or ALL.
-
-        """
-        if self._allowed_dock_areas != dock_areas:
-            self._allowed_dock_areas = dock_areas
-            def closure(pane):
-                pane.TopDockable(bool(dock_areas & wx.TOP))
-                pane.RightDockable(bool(dock_areas & wx.RIGHT))
-                pane.LeftDockable(bool(dock_areas & wx.LEFT))
-                pane.BottomDockable(bool(dock_areas & wx.BOTTOM))
-            self._PaneInfoOperation(closure)
+                self.AddCheckLabelTool(action_id, text, bmp)
+            #item = self._CreationActionItem(action)
+            #self.AddItem(item)
+            #self._actions_map[action] = item
+            #action.Bind(EVT_ACTION_CHANGED, self.OnActionChanged)
 
 
 class WxToolBar(WxConstraintsWidget):
@@ -460,11 +184,26 @@ class WxToolBar(WxConstraintsWidget):
         """ Create the underlying tool bar widget.
 
         """
-        # Overflow is broken: checking an item on the overflow menu
-        # doesn't emit any events, so keep it off.
-        flags = aui.AUI_TB_TEXT #| aui.AUI_TB_OVERFLOW
-        return wxToolBar(parent, agwStyle=flags)
+        # The orientation or dock position of a tool bar can only be set
+        # at creation time. Wx does not support changing it dynamically.
+        style = wx.TB_TEXT
+        if isinstance(parent, wxMainWindow):
+            style |= _DOCK_AREA_MAP[tree['dock_area']]
+        else:
+            style |= _ORIENTATION_MAP[tree['orientation']]
     
+        tbar = wxToolBar(parent, style=style)
+
+        # Setting the tool bar to double buffered avoids a ton of
+        # flickering on Windows during resize events.
+        tbar.SetDoubleBuffered(True) 
+
+        # For now, we set the bitmap size to 0 since we don't yet
+        # support icons or images.
+        tbar.SetToolBitmapSize(wx.Size(0, 0))
+
+        return tbar
+
     def create(self, tree):
         """ Create and initialize the underlying tool bar control.
 
@@ -555,39 +294,46 @@ class WxToolBar(WxConstraintsWidget):
         """ Set the orientation of the underlying widget.
 
         """
-        wx_orientation = _ORIENTATION_MAP[orientation]
-        self.widget().SetToolBarOrientation(wx_orientation)
+        # Wx does not support dynamically changing the orientation.
+        pass
 
     def set_movable(self, movable):
         """ Set the movable state on the underlying widget.
 
         """
-        self.widget().SetMovable(movable)
+        # The standard wx toolbar doesn't support docking. The Aui
+        # toolbar sucks, don't use it.
+        pass
 
     def set_floatable(self, floatable):
         """ Set the floatable state on the underlying widget.
 
         """
-        self.widget().SetFloatable(floatable)
+        # The standard wx toolbar doesn't support docking. The Aui
+        # toolbar sucks, don't use it.
+        pass
 
     def set_floating(self, floating):
         """ Set the floating staet on the underlying widget.
 
         """
-        self.widget().SetFloating(floating)
+        # The standard wx toolbar doesn't support docking. The Aui
+        # toolbar sucks, don't use it.
+        pass
 
     def set_dock_area(self, dock_area):
         """ Set the dock area on the underyling widget.
 
         """
-        self.widget().SetDockArea(_DOCK_AREA_MAP[dock_area])
+        # The standard wx toolbar doesn't support docking. The Aui
+        # toolbar sucks, don't use it.
+        pass
 
     def set_allowed_dock_areas(self, dock_areas):
         """ Set the allowed dock areas on the underlying widget.
 
         """
-        wx_areas = 0
-        for area in dock_areas:
-            wx_areas |= _ALLOWED_AREAS_MAP[area]
-        self.widget().SetAllowedDockAreas(wx_areas)
+        # The standard wx toolbar doesn't support docking. The Aui
+        # toolbar sucks, don't use it.
+        pass
 
