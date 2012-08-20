d7c586604dbf64f83a656ecf620f7a89fac4e298
Chris Colbert <sccolbert@gmail.com>
2012-08-20 03:11:16 -0400

    Update example

--- python_api.py
+++ python_api.py
@@ -6,37 +6,51 @@ from enaml.stdlib.sessions import view_factory
 
 @view_factory('main')
 def create_view():
-   main_window = MainWindow()
+    main_window = MainWindow()
 
-   # We can provide parents as we go, an the parent-child relationship
-   # is fully established automatically.
-   dock_pane = DockPane(main_window)
-   dock_container = Container(dock_pane)
-   button1 = PushButton(dock_container, text='Button1')
-   button2 = PushButton(dock_container, text='Button2')
-   field1 = Field(dock_container, placeholder='Data...')
+    # We can provide parents as we go, an the parent-child relationship
+    # is fully established automatically.
+    dock_pane = DockPane(main_window)
+    dock_container = Container(dock_pane)
+    button1 = PushButton(dock_container, text='Button1')
+    button2 = PushButton(dock_container, text='Button2')
+    field1 = Field(dock_container, placeholder='Data...')
 
-   # But, parents don't *need* to be provided immediately.
-   main_container = Container()
-   html = Html(source='<h1><center>Hello World</center></h1>')
+    # But, parents don't *need* to be provided immediately.
+    main_container = Container()
+    html = Html(source='<h1><center>Hello World</center></h1>')
 
-   # Things can be (re)parented at any time, and the parent-child 
-   # relationships will be updated appropriately. Things just need 
-   # to be parented "at some point", or we'll end up with multiple
-   # top-level widgets.
-   main_container.parent = main_window
-   html.parent = main_container
+    # Things can be (re)parented at any time, and the parent-child 
+    # relationships will be updated appropriately. Things just need 
+    # to be parented "at some point", or we'll end up with multiple
+    # top-level widgets.
+    main_container.parent = main_window
+    html.parent = main_container
 
-   menu_bar = MenuBar(main_window)
-   file_menu = Menu(menu_bar, title='File')
-   action1 = Action(file_menu, text='New')
-   action2 = Action(file_menu, text='Open')
-   edit_menu = Menu(menu_bar, title='Edit')
-   action3 = Action(edit_menu, text='Cut')
-   action4 = Action(edit_menu, text='Copy')
-   action5 = Action(edit_menu, text='Past', enabled=False)
+    # Parent-child relationships can also be established the other
+    # way around, by specifying children instead of parents.
+    main_container.children.extend([
+        PushButton(text='Just'),
+        PushButton(text='Some'),
+        Container(
+            hug_height='strong',
+            children=[
+                Field(text='Extra'),
+                Field(text='Children')
+            ]
+        ),
+    ])
 
-   return main_window
+    menu_bar = MenuBar(main_window)
+    file_menu = Menu(menu_bar, title='File')
+    action1 = Action(file_menu, text='New')
+    action2 = Action(file_menu, text='Open')
+    edit_menu = Menu(menu_bar, title='Edit')
+    action3 = Action(edit_menu, text='Cut')
+    action4 = Action(edit_menu, text='Copy')
+    action5 = Action(edit_menu, text='Past', enabled=False)
+
+    return main_window
 
 
 if __name__ == '__main__':
