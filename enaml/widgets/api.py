f931f046d5ecd76a4983fd400ee62e5ea9aa502f
Chris Colbert <sccolbert@gmail.com>
2012-08-20 11:53:01 -0400

    Move widget imports into an api file.
    Add initial declaration of a DockArea (not sold on the name)

---
+++ enaml/widgets/api.py
@@ -0,0 +1,38 @@
+#------------------------------------------------------------------------------
+#  Copyright (c) 2012, Enthought, Inc.
+#  All rights reserved.
+#------------------------------------------------------------------------------
+from .action import Action
+from .action_group import ActionGroup
+from .bounded_date import BoundedDate
+from .bounded_datetime import BoundedDatetime
+from .calendar import Calendar
+from .check_box import CheckBox
+from .combo_box import ComboBox
+from .container import Container
+from .date_selector import DateSelector
+from .datetime_selector import DatetimeSelector
+from .dialog import Dialog
+from .dock_pane import DockPane
+from .field import Field
+from .form import Form
+from .group_box import GroupBox
+from .html import Html
+from .image_view import ImageView
+from .label import Label
+from .main_window import MainWindow
+from .menu import Menu
+from .menu_bar import MenuBar
+from .notebook import Notebook
+from .page import Page
+from .progress_bar import ProgressBar
+from .push_button import PushButton
+from .radio_button import RadioButton
+from .scroll_area import ScrollArea
+from .slider import Slider
+from .spin_box import SpinBox
+from .splitter import Splitter
+from .text_editor import TextEditor
+from .tool_bar import ToolBar
+from .window import Window
+
