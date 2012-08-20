fbcbdb9b36b15d9d75426cad228a6191081cc951
Chris Colbert <sccolbert@gmail.com>
2012-08-20 03:03:28 -0400

    update comments

--- python_api.py
+++ python_api.py
@@ -8,21 +8,22 @@ from enaml.stdlib.sessions import view_factory
 def create_view():
    main_window = MainWindow()
 
-   # We can provided parents as we go, the parent-child relationship
-   # is established automatically.
+   # We can provide parents as we go, an the parent-child relationship
+   # is fully established automatically.
    dock_pane = DockPane(main_window)
    dock_container = Container(dock_pane)
    button1 = PushButton(dock_container, text='Button1')
    button2 = PushButton(dock_container, text='Button2')
    field1 = Field(dock_container, placeholder='Data...')
 
-   # But, parents don't need to be provided immediately
+   # But, parents don't *need* to be provided immediately.
    main_container = Container()
    html = Html(source='<h1><center>Hello World</center></h1>')
 
-   # Things can be parented at any time, and the parent-child 
-   # relationship will be made properly. Things just need to 
-   # be parents "at some point"
+   # Things can be (re)parented at any time, and the parent-child 
+   # relationships will be updated appropriately. Things just need 
+   # to be parented "at some point", or we'll end up with multiple
+   # top-level widgets.
    main_container.parent = main_window
    html.parent = main_container
 
@@ -34,7 +35,7 @@ def create_view():
    action3 = Action(edit_menu, text='Cut')
    action4 = Action(edit_menu, text='Copy')
    action5 = Action(edit_menu, text='Past', enabled=False)
-   
+
    return main_window
 
 
