0d1b068eb74bb0a9c7e41e09c327a7b7219ed32b
Chris Colbert <sccolbert@gmail.com>
2012-09-24 18:35:27 -0400

    Use Declarative as base class in place of old MessengerWidget

--- enaml/widgets/action_group.py
+++ enaml/widgets/action_group.py
@@ -4,11 +4,12 @@
 #------------------------------------------------------------------------------
 from traits.api import Bool, Property, cached_property
 
+from enaml.core.declarative import Declarative
+
 from .action import Action
-from .messenger_widget import MessengerWidget
 
 
-class ActionGroup(MessengerWidget):
+class ActionGroup(Declarative):
     """ A non visible widget used to group actions.
 
     An action group can be used in a MenuBar or a ToolBar to group a 
