989a0fda6d330c3e53cd7a33133796b1a98a95c4
Naveen Michaud-Agrawal <naveenm@enthought.com>
2012-10-11 12:04:09 -0400

    Undefined variable left over from older implementation - removing

--- enaml/backends/qt/qt_label.py
+++ enaml/backends/qt/qt_label.py
@@ -27,7 +27,7 @@ class QtLabel(QtControl, AbstractTkLabel):
         self.widget = QtGui.QLabel(parent)
 
         # If we have HTML markup, open links externally
-        self.widget.setOpenExternalLinks(allow_markup)
+        self.widget.setOpenExternalLinks(True)
 
     def initialize(self):
         """ Initializes the attributes on the underlying control.
