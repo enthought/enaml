90be45719eca6cfc6c0429e6fbd9900f61e0001f
Chris Colbert <sccolbert@gmail.com>
2012-10-14 21:18:02 -0400

    Update the C++ implementation of WeakMethod

--- enaml/extensions/weakmethod.cpp
+++ enaml/extensions/weakmethod.cpp
@@ -28,45 +28,19 @@ typedef struct {
 static PyObject* weak_methods;
 
 
-// A python callable which wraps the weak_methods_dead_ref C function.
-// This is initialized in the module init function. This is not declared 
-// as a PyObjectPtr since a destructor being run on dlclose() has the 
-// potential to call the tp_dealloc slot after interpreter shut down.
-static PyObject* dead_ref;
-
-
-// A callback fired by the object underlyling a weakref used as a key 
-// in the weak_methods dict is collected. This removes the entry from 
-// the dict and allows the WeakMethod instances to be collected.
-static PyObject*
-weak_methods_dead_ref( PyObject* self, PyObject* wr_item )
-{   
-    PyDictPtr wmethods_ptr( weak_methods, true );
-    PyObjectPtr wrptr( wr_item, true );
-    if( wmethods_ptr.del_item( wrptr ) )
-        Py_RETURN_NONE;
-    return 0;
-}
-
-
-// A method structure used for initializing the PyCFunction object which
-// wraps the weak_methods_dead_ref callback.
-static PyMethodDef
-dead_ref_method = {
-    "weak_methods_dead_ref", ( PyCFunction )weak_methods_dead_ref, 
-    METH_O, "Release internal strong references to WeakMethod instances"
-};
+// A preallocated string object to lookup the `_remove` staticmethod.
+static PyObject* remove_str;
 
 
 static PyObject*
 WeakMethod_new( PyTypeObject* type, PyObject* args, PyObject* kwargs )
-{   
+{
     PyDictPtr kwargsptr( kwargs, true );
     if( ( kwargsptr ) && ( kwargsptr.size() > 0 ) )
     {
         std::ostringstream ostr;
         ostr << "WeakMethod() takes no keyword arguments (";
-        ostr << static_cast<int>( kwargsptr.size() ) << " given)";
+        ostr << kwargsptr.size() << " given)";
         return py_type_fail( ostr.str().c_str() );
     }
 
@@ -75,7 +49,7 @@ WeakMethod_new( PyTypeObject* type, PyObject* args, PyObject* kwargs )
     {
         std::ostringstream ostr;
         ostr << "WeakMethod() takes 1 argument (";
-        ostr << static_cast<int>( argsptr.size() ) << " given)";
+        ostr << argsptr.size() << " given)";
         return py_type_fail( ostr.str().c_str() );
     }
 
@@ -89,9 +63,9 @@ WeakMethod_new( PyTypeObject* type, PyObject* args, PyObject* kwargs )
     if( !self )
         return py_type_fail( "Expected a bound method. Got unbound method instead." );
 
-    /* The logic to setup the weakref is as follows: 
-       
-    The keys of the weak_methods dict should be weakrefs for a given 
+    /* The logic to setup the weakref is as follows:
+
+    The keys of the weak_methods dict should be weakrefs for a given
     object and have a callback that will pop the item from the dict
     when the underlying object is destroyed. When using weakrefs as
     keys in a dictionary, two weakrefs will hash to the same value
@@ -106,7 +80,7 @@ WeakMethod_new( PyTypeObject* type, PyObject* args, PyObject* kwargs )
         >>> r is weakref.ref(f)
         True
 
-    However, Python will create a new weakref instance for each 
+    However, Python will create a new weakref instance for each
     weakref with a callback, even if the callback is the same:
 
         >>> def bar(): pass
@@ -119,15 +93,15 @@ WeakMethod_new( PyTypeObject* type, PyObject* args, PyObject* kwargs )
     good amount of space can be saved if all `WeakMethod` instances for
     a given object share the same no-callback weakref for that object,
     and the only weakref with callback kept around is the one used as
-    the key in the dict. 
+    the key in the dict.
 
     The logic below first creates a no-callback weakref, which is always
-    necessary and will only be created by Python once and then reused. 
+    necessary and will only be created by Python once and then reused.
     That weakref is used to lookup the item in the dict. If that lookup
     succeeds, the weakref with callback already exists and no more work
-    is required. Otherwise, the weakref with callback is created and 
+    is required. Otherwise, the weakref with callback is created and
     used as the key in the dict.
-    
+
     */
 
     PyWeakrefPtr selfref( PyWeakref_NewRef( self.get(), 0 ) );
@@ -141,7 +115,12 @@ WeakMethod_new( PyTypeObject* type, PyObject* args, PyObject* kwargs )
         items = PyList_New( 0 );
         if( !items )
             return 0;
-        PyWeakrefPtr selfrefcb( PyWeakref_NewRef( self.get(), dead_ref ) );
+        PyObjectPtr wm_type( reinterpret_cast<PyObject*>( type ), true );
+        PyObjectPtr _remove_str( remove_str, true );
+        PyObjectPtr _remove( wm_type.get_attr( _remove_str ) );
+        if( !_remove )
+            return 0;
+        PyWeakrefPtr selfrefcb( PyWeakref_NewRef( self.get(), _remove.get() ) );
         if( !selfrefcb )
             return 0;
         if( !wmethods_ptr.set_item( selfrefcb, items ) )
@@ -164,7 +143,7 @@ WeakMethod_new( PyTypeObject* type, PyObject* args, PyObject* kwargs )
 
     pywm = reinterpret_cast<WeakMethod*>( wm.get() );
     pywm->func = func.release();
-    pywm->selfref = selfref.release(); 
+    pywm->selfref = selfref.release();
     pywm->cls = cls.release();
 
     if( !items.append( wm ) )
@@ -195,12 +174,12 @@ WeakMethod_traverse( WeakMethod* self, visitproc visit, void* arg )
 
 static void
 WeakMethod_dealloc( WeakMethod* self )
-{   
+{
     PyObject_GC_UnTrack( self );
     if( self->weakreflist )
-        PyObject_ClearWeakRefs( ( PyObject* )self );
+        PyObject_ClearWeakRefs( reinterpret_cast<PyObject*>( self ) );
     WeakMethod_clear( self );
-    self->ob_type->tp_free( ( PyObject* )self );
+    self->ob_type->tp_free( reinterpret_cast<PyObject*>( self ) );
 }
 
 
@@ -208,7 +187,7 @@ static PyObject*
 WeakMethod_call( WeakMethod* self, PyObject* args, PyObject* kwargs )
 {
     PyWeakrefPtr selfref( self->selfref, true );
-    PyObjectPtr mself( selfref() );
+    PyObjectPtr mself( selfref.get_object() );
     if( mself.is_None() )
         Py_RETURN_NONE;
     PyMethodPtr method( PyMethod_New( self->func, mself.get(), self->cls ) );
@@ -220,15 +199,38 @@ WeakMethod_call( WeakMethod* self, PyObject* args, PyObject* kwargs )
 }
 
 
+// A staticmethod fired by the object underlyling a weakref used as the
+// key in the weak_methods dict when it is garbage collected. This will
+// remove the entry from the dict and allows the WeakMethod instances
+// to be garbage collected.
+static PyObject*
+WeakMethod__remove( PyObject* ignored, PyObject* wr_item )
+{
+    PyDictPtr wmethods_ptr( weak_methods, true );
+    PyObjectPtr wrptr( wr_item, true );
+    if( !wmethods_ptr.del_item( wrptr ) )
+        return 0;
+    Py_RETURN_NONE;
+}
+
+
+static PyMethodDef
+WeakMethod_methods[] = {
+    { "_remove", ( PyCFunction )WeakMethod__remove, METH_O | METH_STATIC,
+      "Release internal strong references to WeakMethod instances" },
+    { 0 } // sentinel
+};
+
+
 PyDoc_STRVAR(WeakMethod__doc__,
 "WeakMethod(method)\n\n"
-"A `WeakMethod` is an object which weakly wraps a method with a\n"
-"lifetime bound to the lifetime of the underlying object.\n\n"
-"Instances of `WeakMethod` are also weakref-able with a lifetime\n"
-"which is also bound to lifetime of the method owner.\n\n"
-"If multiple `WeakMethod` instances are requested for the same\n"
-"equivalent method object, the same `WeakMethod` instance will be\n"
-"returned. This behavior follows normal weakref semantics.\n\n"
+"An object which weakly binds a method with a lifetime bound\n"
+"to the lifetime of the underlying object.\n\n"
+"Instances of WeakMethod are also weakref-able with a lifetime which\n"
+"is also bound to lifetime of the method owner.\n\n"
+"If multiple WeakMethods are requested for the same equivalent method\n"
+"object, the same WeakMethod will be returned. This behavior is the\n"
+"same as the standard weakref semantics.\n\n"
 "Parameters\n"
 "----------\n"
 "method : A bound method object\n"
@@ -264,7 +266,7 @@ PyTypeObject WeakMethod_Type = {
     offsetof( WeakMethod, weakreflist ),    /* tp_weaklistoffset */
     (getiterfunc)0,                         /* tp_iter */
     (iternextfunc)0,                        /* tp_iternext */
-    (struct PyMethodDef*)0,                 /* tp_methods */
+    (struct PyMethodDef*)WeakMethod_methods, /* tp_methods */
     (struct PyMemberDef*)0,                 /* tp_members */
     0,                                      /* tp_getset */
     0,                                      /* tp_base */
@@ -286,7 +288,7 @@ PyTypeObject WeakMethod_Type = {
 };
 
 
-static PyMethodDef 
+static PyMethodDef
 weakmethod_methods[] = {
     { 0 } // Sentinel
 };
@@ -303,13 +305,14 @@ initweakmethod( void )
     if( !weak_methods )
         return;
 
-    dead_ref = PyCFunction_New( &dead_ref_method, mod );
-    if( !dead_ref )
+    remove_str = PyString_FromString( "_remove" );
+    if( !remove_str )
         return;
 
     if( PyType_Ready( &WeakMethod_Type ) )
         return;
 
-    PyModule_AddObject( mod, "WeakMethod", ( PyObject* )&WeakMethod_Type );
+    PyObjectPtr wm_type( reinterpret_cast<PyObject*>( &WeakMethod_Type ), true );
+    PyModule_AddObject( mod, "WeakMethod", wm_type.release() );
 }
 
