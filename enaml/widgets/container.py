759918dbb4f93508780bd92ba56c24897022a22f
Chris Colbert <sccolbert@gmail.com>
2012-10-16 18:20:31 -0400

    Add a child validator to Container

--- enaml/widgets/container.py
+++ enaml/widgets/container.py
@@ -10,6 +10,7 @@ from enaml.layout.geometry import Box
 from enaml.layout.layout_helpers import vbox
 
 from .constraints_widget import ConstraintsWidget, BoxModel, get_from_box_model
+from .include import Include
 
 
 class ContainerBoxModel(BoxModel):
@@ -17,10 +18,10 @@ class ContainerBoxModel(BoxModel):
 
     Primitive Variables:
         contents_[left|right|top|bottom]
-    
+
     Derived Variables:
         contents_[width|height|v_center|h_center]
-    
+
     """
     def __init__(self, owner):
         """ Initialize a ContainerBoxModel.
@@ -28,7 +29,7 @@ class ContainerBoxModel(BoxModel):
         Parameters
         ----------
         owner : string
-            A string which uniquely identifies the owner of this box 
+            A string which uniquely identifies the owner of this box
             model.
 
         """
@@ -44,18 +45,18 @@ class ContainerBoxModel(BoxModel):
 
 
 class Container(ConstraintsWidget):
-    """ A ConstraintsWidget subclass that provides functionality for 
-    laying out constrainable children according to their system of 
+    """ A ConstraintsWidget subclass that provides functionality for
+    laying out constrainable children according to their system of
     constraints.
 
     The Container is the canonical component used to arrange child
     widgets using constraints-based layout. Given a heierarchy of
     components, the top-most Container will be charged with the actual
-    layout of the decendents. This allows constraints to cross the 
+    layout of the decendents. This allows constraints to cross the
     boundaries of Containers, enabling powerful and flexible layouts.
 
     There are widgets whose boundaries constraints may not cross. Some
-    examples of these would be a ScrollArea or a TabGroup. See the 
+    examples of these would be a ScrollArea or a TabGroup. See the
     documentation of a given container component as to whether or not
     constraints may cross its boundaries.
 
@@ -65,24 +66,24 @@ class Container(ConstraintsWidget):
     #: This is False by default, which means that every container
     #: get its own layout solver. This improves speed and reduces
     #: memory use (by keeping a solver's internal tableaux small)
-    #: but at the cost of not being able to share constraints 
-    #: across Container boundaries. This flag must be explicitly 
+    #: but at the cost of not being able to share constraints
+    #: across Container boundaries. This flag must be explicitly
     #: marked as True to enable sharing.
     share_layout = Bool(False)
 
-    #: A read-only symbolic object that represents the internal left 
+    #: A read-only symbolic object that represents the internal left
     #: boundary of the content area of the container.
     contents_left = Property(fget=get_from_box_model)
 
-    #: A read-only symbolic object that represents the internal right 
+    #: A read-only symbolic object that represents the internal right
     #: boundary of the content area of the container.
     contents_right = Property(fget=get_from_box_model)
 
-    #: A read-only symbolic object that represents the internal top 
+    #: A read-only symbolic object that represents the internal top
     #: boundary of the content area of the container.
     contents_top = Property(fget=get_from_box_model)
 
-    #: A read-only symbolic object that represents the internal bottom 
+    #: A read-only symbolic object that represents the internal bottom
     #: boundary of the content area of the container.
     contents_bottom = Property(fget=get_from_box_model)
 
@@ -94,32 +95,32 @@ class Container(ConstraintsWidget):
     #: the content area of the container.
     contents_height = Property(fget=get_from_box_model)
 
-    #: A read-only symbolic object that represents the internal center 
+    #: A read-only symbolic object that represents the internal center
     #: along the vertical direction the content area of the container.
     contents_v_center = Property(fget=get_from_box_model)
 
-    #: A read-only symbolic object that represents the internal center 
+    #: A read-only symbolic object that represents the internal center
     #: along the horizontal direction of the content area of the container.
     contents_h_center = Property(fget=get_from_box_model)
 
     #: A box object which holds the padding for this component. The
     #: padding is the amount of space between the outer boundary box
     #: and the content box. The default padding is (10, 10, 10, 10).
-    #: Certain subclasses, such as GroupBox, may provide additional 
+    #: Certain subclasses, such as GroupBox, may provide additional
     #: margin than what is specified by the padding.
     padding = CoercingInstance(Box, (10, 10, 10, 10))
 
     #: A read only property which returns this container's widgets.
     widgets = Property(depends_on='children')
 
-    #: Containers freely exapnd in width and height. The size hint 
+    #: Containers freely exapnd in width and height. The size hint
     #: constraints for a Container are used when the container is
-    #: not sharing its layout. In these cases, expansion of the 
+    #: not sharing its layout. In these cases, expansion of the
     #: container is typically desired.
     hug_width = 'ignore'
     hug_height = 'ignore'
 
-    #: The private storage the box model instance for this component. 
+    #: The private storage the box model instance for this component.
     _box_model = Instance(ContainerBoxModel)
     def __box_model_default(self):
         return ContainerBoxModel(self.widget_id)
@@ -135,6 +136,26 @@ class Container(ConstraintsWidget):
         self.on_trait_change(self._send_relayout, 'share_layout, padding')
 
     #--------------------------------------------------------------------------
+    # Overrides
+    #--------------------------------------------------------------------------
+    def validate_children(self, children):
+        """ A child validator for a Container.
+
+        The allowable child types are `ConstraintsWidget` and `Include`.
+
+        """
+        types = (ConstraintsWidget, Include)
+        isinst = isinstance
+        for child in children:
+            if not isinst(child, types):
+                name = type(self).__name__
+                msg = ('The children of a component of type `%s` must be '
+                       'instances of `ConstraintsWidget` or `Include`. Got '
+                       'object of type `%s` instead.')
+                raise ValueError(msg % (name, type(child).__name__))
+        return children
+
+    #--------------------------------------------------------------------------
     # Constraints Generation
     #--------------------------------------------------------------------------
     def _layout_info(self):
@@ -148,7 +169,7 @@ class Container(ConstraintsWidget):
         return layout
 
     def _default_constraints(self):
-        """ Supplies a default vbox constraint to the constraints 
+        """ Supplies a default vbox constraint to the constraints
         children of the container if other constraints are not given.
 
         """
