c13274b27745868b10b5f13a5a16fe635f23387e
Chris Colbert <sccolbert@gmail.com>
2012-10-16 17:19:38 -0400

    Only allow Action and Include as children of an ActionGroup

--- enaml/widgets/action_group.py
+++ enaml/widgets/action_group.py
@@ -7,12 +7,13 @@ from traits.api import Bool, Property, cached_property
 from enaml.core.declarative import Declarative
 
 from .action import Action
+from .include import Include
 
 
 class ActionGroup(Declarative):
     """ A non visible widget used to group actions.
 
-    An action group can be used in a MenuBar or a ToolBar to group a 
+    An action group can be used in a MenuBar or a ToolBar to group a
     related set of Actions and apply common operations to the set. The
     primary use of an action group is to make any checkable actions in
     the group mutually exclusive.
@@ -49,6 +50,25 @@ class ActionGroup(Declarative):
         """
         super(ActionGroup, self).bind()
         self.publish_attributes('exclusive', 'enabled', 'visible')
+
+    #--------------------------------------------------------------------------
+    # Overrides
+    #--------------------------------------------------------------------------
+    def validate_children(self, children):
+        """ A child validator for an Action Group.
+
+        The allowable child types are `Action` and `Include`.
+
+        """
+        types = (Action, Include)
+        isinst = isinstance
+        for child in children:
+            if not isinst(child, types):
+                msg = ('The children of an `ActionGroup` must be instances '
+                       'of `Action` or `Include`. Got object of type `%s` '
+                       'instead.')
+                raise ValueError(msg % type(child).__name__)
+        return children
 
     #--------------------------------------------------------------------------
     # Private API
