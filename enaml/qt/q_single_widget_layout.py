bf1e62ca858f63795c68b7bb3d3fa0ac9161a1a9
Chris Colbert <sccolbert@gmail.com>
2012-10-17 13:24:52 -0400

    Make the QSingleWidgetLayout None-safe and hide the old widget when removing.

--- enaml/qt/q_single_widget_layout.py
+++ enaml/qt/q_single_widget_layout.py
@@ -7,9 +7,9 @@ from .qt.QtGui import QLayout
 
 
 class QSingleWidgetLayout(QLayout):
-    """ A QLayout subclass which can have at most one layout item. 
+    """ A QLayout subclass which can have at most one layout item.
 
-    The layout item is expanded to fit the allowable space. This is 
+    The layout item is expanded to fit the allowable space. This is
     similar to how central widgets behave in a QMainWindow.
 
     """
@@ -27,6 +27,18 @@ class QSingleWidgetLayout(QLayout):
         self._layout_item = item
         self.update()
 
+    def addWidget(self, widget):
+        """ An overridden method which removes the old layout widget
+        before adding the new one.
+
+        This method can be safetly called with None as the widget
+        argument.
+
+        """
+        self.takeAt(0)
+        if widget is not None:
+            super(QSingleWidgetLayout, self).addWidget(widget)
+
     def count(self):
         """ A virtual method implementation which returns 0 if no layout
         item is supplied, or 1 if there is a current layout item.
@@ -35,7 +47,7 @@ class QSingleWidgetLayout(QLayout):
         return 0 if self._layout_item is None else 1
 
     def itemAt(self, idx):
-        """ A virtual method implementation which returns the layout item 
+        """ A virtual method implementation which returns the layout item
         for the given index or None if one does not exist.
 
         """
@@ -50,8 +62,12 @@ class QSingleWidgetLayout(QLayout):
         if idx == 0:
             res = self._layout_item
             self._layout_item = None
+            if res is not None:
+                old = res.widget()
+                if old is not None:
+                    old.hide()
             return res
-    
+
     def sizeHint(self):
         """ A virtual method implementation which returns an invalid
         size hint for the top-level Window.
@@ -81,7 +97,7 @@ class QSingleWidgetLayout(QLayout):
         if item is not None:
             return item.widget().minimumSizeHint()
         return super(QSingleWidgetLayout, self).minimumSize()
-            
+
     def maximumSize(self):
         """ A reimplemented method which returns the minimum size hint
         of the layout item widget as the minimum size of the window.
