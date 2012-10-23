3012866e26e9fdc0946beb48e1ea40bd7cbfad90
Didrik Pinte <dpinte@enthought.com>
2012-10-23 22:41:49 +0200

    updating enaml_test_case to use the mock_server

--- enaml/tests/widgets/enaml_test_case.py
+++ enaml/tests/widgets/enaml_test_case.py
@@ -3,14 +3,17 @@
 #  All rights reserved.
 #------------------------------------------------------------------------------
 import types
+import unittest
 
 from enaml.core.parser import parse
 from enaml.core.enaml_compiler import EnamlCompiler
+from enaml.application import Application
+from enaml.stdlib.sessions import simple_session
 
-from .mock_application import MockApplication
+from .mock_request import MockLocalServer
 
 
-class EnamlTestCase(object):
+class EnamlTestCase(unittest.TestCase):
     """ Base class for testing Enaml object widgets.
 
     This class provide utility methods functions to help the testing of
@@ -23,10 +26,13 @@ class EnamlTestCase(object):
         finds a widget of a particular type.
 
         """
-        if root.widget_type == type_name:
+
+        print root.widget()
+        #if widget is None, it means nobody has called create on them
+        if type_name == root.widget()['class']:
             return root
 
-        for child in root.children:
+        for child in root.children():
             found = self.find_client_widget(child, type_name)
             if found is not None:
                 return found
@@ -50,7 +56,7 @@ class EnamlTestCase(object):
 
     def parse_and_create(self, source, **kwargs):
         """ Parses and compiles the source. The source should have a
-        component defined with the name 'MainView'. 
+        component defined with the name 'MainView'.
 
         Arguments
         ---------
@@ -65,21 +71,35 @@ class EnamlTestCase(object):
             The component tree for the 'MainView' component.
 
         """
-        # Start the app instance first.
-        app = MockApplication.instance()
-        if app is None:
-            app = MockApplication()
-        self.app = app
-
         enaml_ast = parse(source)
         enaml_module = types.ModuleType('__tests__')
         ns = enaml_module.__dict__
         code = EnamlCompiler.compile(enaml_ast, '__enaml_tests__')
 
         exec code in ns
-        view = ns['MainView']
-        self.view = view(**kwargs)
-        self.view.prepare()
+        View = ns['MainView']
 
-        self.client_view = app.builder().root
+        # Start the app instance first.
+        v = simple_session('main', 'test', View)
+
+        self.app = Application([v])
+
+        # uses QT for now
+        server = MockLocalServer(self.app)
+        client = server.local_client()
+
+        client.start_session('main')
+
+        self.server = server
+
+        server.start()
+
+        session_id = client._client_sessions.keys()[0]
+
+        server_view_id = self.app._sessions[session_id]._widgets.keys()[0]
+        self.view = self.app._sessions[session_id]._widgets[server_view_id]
+
+        session = client._client_sessions[session_id]
+
+        self.client_view = session._widgets[session._widgets.keys()[0]]
 
