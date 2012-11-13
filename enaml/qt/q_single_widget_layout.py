24f207fabdfc663c96a673dc1f59b6e464d15e8c
Chris Colbert <sccolbert@gmail.com>
2012-11-12 22:47:47 -0500

    Update QSingleWidgetLayout to be more idiomatic Qt.
    The primary change is the use of a QWidgetItem subclass which
    reimplements `setGeometry` according to the semantics needed
    for the layout.

--- enaml/qt/q_single_widget_layout.py
+++ enaml/qt/q_single_widget_layout.py
@@ -3,49 +3,116 @@
 #  All rights reserved.
 #------------------------------------------------------------------------------
 from .qt.QtCore import QSize
-from .qt.QtGui import QLayout
+from .qt.QtGui import QLayout, QWidgetItem
+
+
+class QSingleWidgetItem(QWidgetItem):
+    """ A QWidgetItem subclass for use with the QSingleWidgetLayout.
+
+    The semantics of this widget item are slightly different from that
+    of the standard QWidgetItem; it always aligns its widget with the
+    top left of the layout rect. This class is expressly meant for use
+    by the QSingleWidgetLayout.
+
+    """
+    def setGeometry(self, rect):
+        """ Set the rectangle covered by this layout item.
+
+        Parameters
+        ----------
+        rect : QRect
+            The rectangle that this layout item should cover.
+
+        """
+        if self.isEmpty():
+            return
+        s = rect.size().boundedTo(self.maximumSize())
+        self.widget().setGeometry(rect.x(), rect.y(), s.width(), s.height())
 
 
 class QSingleWidgetLayout(QLayout):
     """ A QLayout subclass which can have at most one layout item.
 
-    The layout item is expanded to fit the allowable space. This is
-    similar to how central widgets behave in a QMainWindow.
+    The layout item is expanded to fit the allowable space; similar to
+    how a central widget behaves in a QMainWindow. Unlike QMainWindow,
+    this layout respects the maximum size of the widget. The default
+    contents margins of this layout is 0px in all directions.
 
     """
-    #: An invalid size to use if there is no layout item.
-    _invalid_size = QSize()
+    def __init__(self, *args, **kwargs):
+        """ Initialize a QSingleWidgetLayout.
 
-    #: Storage for the generated layout item.
-    _layout_item = None
-
-    def addItem(self, item):
-        """ A virtual method implementation which sets the layout item
-        in the layout. Any old item will be overridden.
+        Parameters
+        ----------
+        *args, **kwargs
+            The positional and keyword arguments needed to initialize
+            a QLayout.
 
         """
-        self._layout_item = item
-        self.update()
+        super(QSingleWidgetLayout, self).__init__(*args, **kwargs)
+        self.setContentsMargins(0, 0, 0, 0)
+        self._item = None
 
-    def addWidget(self, widget):
-        """ An overridden method which removes the old layout widget
-        before adding the new one.
+    #--------------------------------------------------------------------------
+    # Public API
+    #--------------------------------------------------------------------------
+    def widget(self):
+        """ Get the widget being managed by this layout.
 
-        This method can be safetly called with None as the widget
-        argument.
+        Returns
+        -------
+        result : QWidget or None
+            The widget being managed by this layout or None if it does
+            not exist.
+
+        """
+        item = self._item
+        if item is not None:
+            return item.widget()
+
+    def setWidget(self, widget):
+        """ Set the widget for this layout.
+
+        Parameters
+        ----------
+        widget : QWidget
+            The widget to manage with this layout.
 
         """
         self.takeAt(0)
         if widget is not None:
-            widget.show()
-            super(QSingleWidgetLayout, self).addWidget(widget)
+            self.addChildWidget(widget)
+            self.addItem(QSingleWidgetItem(widget))
+            widget.setVisible(True)
+            self.update()
+
+    #--------------------------------------------------------------------------
+    # Private API
+    #--------------------------------------------------------------------------
+    def addWidget(self, widget):
+        """ Overridden parent class method. This method redirects to the
+        `setWidget` method. User code should call `setWidget` instead.
+
+        """
+        import warnings
+        msg = ('`QSingleWidgetLayout.addWidget`: use the '
+               '`QSingleWidgetLayout.setWidget` method instead.')
+        warnings.warn(msg)
+        self.setWidget(widget)
+
+    def addItem(self, item):
+        """ A virtual method implementation which sets the layout item
+        in the layout. The old item will be overridden.
+
+        """
+        self._item = item
 
     def count(self):
         """ A virtual method implementation which returns 0 if no layout
         item is supplied, or 1 if there is a current layout item.
 
         """
-        return 0 if self._layout_item is None else 1
+        return 0 if self._item is None else 1
 
     def itemAt(self, idx):
         """ A virtual method implementation which returns the layout item
@@ -53,7 +120,7 @@ class QSingleWidgetLayout(QLayout):
 
         """
         if idx == 0:
-            return self._layout_item
+            return self._item
 
     def takeAt(self, idx):
         """ A virtual method implementation which removes and returns the
@@ -61,8 +128,8 @@ class QSingleWidgetLayout(QLayout):
 
         """
         if idx == 0:
-            res = self._layout_item
-            self._layout_item = None
+            res = self._item
+            self._item = None
             if res is not None:
                 old = res.widget()
                 if old is not None:
@@ -70,14 +137,18 @@ class QSingleWidgetLayout(QLayout):
             return res
 
     def sizeHint(self):
-        """ A virtual method implementation which returns an invalid
-        size hint for the top-level Window.
+        """ A virtual method implementation which returns the size hint
+        for the layout.
 
         """
-        item = self._layout_item
+        item = self._item
         if item is not None:
-            return item.widget().sizeHint()
-        return self._invalid_size
+            hint = item.sizeHint()
+            left, top, right, bottom = self.getContentsMargins()
+            hint.setHeight(hint.height() + top + bottom)
+            hint.setWidth(hint.width() + left + right)
+            return hint
+        return QSize()
 
     def setGeometry(self, rect):
         """ A reimplemented method which sets the geometry of the managed
@@ -85,18 +156,22 @@ class QSingleWidgetLayout(QLayout):
 
         """
         super(QSingleWidgetLayout, self).setGeometry(rect)
-        item = self._layout_item
+        item = self._item
         if item is not None:
-            item.widget().setGeometry(rect)
+            item.setGeometry(self.contentsRect())
 
     def minimumSize(self):
         """ A reimplemented method which returns the minimum size hint
         of the layout item widget as the minimum size of the window.
 
         """
-        item = self._layout_item
+        item = self._item
         if item is not None:
-            return item.widget().minimumSizeHint()
+            s = item.minimumSize()
+            left, top, right, bottom = self.getContentsMargins()
+            s.setHeight(s.height() + top + bottom)
+            s.setWidth(s.width() + left + right)
+            return s
         return super(QSingleWidgetLayout, self).minimumSize()
 
     def maximumSize(self):
@@ -104,8 +179,12 @@ class QSingleWidgetLayout(QLayout):
         of the layout item widget as the maximum size of the window.
 
         """
-        item = self._layout_item
+        item = self._item
         if item is not None:
-            return item.widget().maximumSize()
+            s = item.maximumSize()
+            left, top, right, bottom = self.getContentsMargins()
+            s.setHeight(s.height() + top + bottom)
+            s.setWidth(s.width() + left + right)
+            return s
         return super(QSingleWidgetLayout, self).maximumSize()
 
