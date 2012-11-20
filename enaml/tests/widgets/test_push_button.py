123725128ae40c03c229a6d15f1627c30f9d70a1
Didrik Pinte <dpinte@enthought.com>
2012-11-20 13:49:54 +0100

    moved the test_toggle_control tests to test_push_button

--- enaml/tests/widgets/test_push_button.py
+++ enaml/tests/widgets/test_push_button.py
@@ -12,19 +12,106 @@ class TestPushButton(EnamlTestCase):
 
     def setUp(self):
         enaml_source = """
-from enaml.widgets import PushButton, Window
+from enaml.widgets.api import PushButton, Window
+
+enamldef Button(PushButton):
+    attr click_count = 0
+
+
+enamldef MainView(Window):
+    Button:
+        id: pb1
+        clicked::
+            pb1.click_count += 1
+"""
+        self.parse_and_create(enaml_source)
+        self.server_widget = self.find_server_widget(self.view, "Button")
+        self.client_widget = self.find_client_widget(self.client_view, "QtPushButton")
+
+    def test_set_text(self):
+        """ Test the setting of a PushButton's text attribute
+        """
+
+        with self.app.process_events():
+            self.server_widget.text = "Push Me!"
+
+        self.assertEquals(self.client_widget.text(), self.server_widget.text)
+
+    def test_client_clicked(self):
+        """ Test a click on the client side that must increase the click count on the
+        server side.
+        """
+
+        initial_click_count = self.server_widget.click_count
+
+        with self.app.process_events():
+            self.client_widget.click()
+
+        self.assertEquals(1 + initial_click_count, self.server_widget.click_count)
+
+    def test_server_clicked(self):
+        """ Test a click on the client side that must increase the click count on the
+        server side.
+        """
+
+        initial_click_count = self.server_widget.click_count
+
+        with self.app.process_events():
+            self.server_widget.clicked()
+
+        self.assertEquals(1 + initial_click_count, self.server_widget.click_count)
+
+
+
+class TestTogglePushButtonControl(EnamlTestCase):
+    """ Unit tests for the ToggleControl widget.
+
+    """
+
+    def setUp(self):
+        enaml_source = """
+from enaml.widgets.api import Window, PushButton
 
 enamldef MainView(Window):
     PushButton:
-        pass
+        checkable = True
+        checked = False
 """
         self.parse_and_create(enaml_source)
         self.server_widget = self.find_server_widget(self.view, "PushButton")
-        self.client_widget = self.find_client_widget(self.client_view, "PushButton")
+        self.client_widget = self.find_client_widget(self.client_view, "QtPushButton")
 
-    def test_(self):
-        """ Test the setting of a PushButton's text attribute
+    def test_set_checked(self):
+        """ Test the setting of a ToggleControl's checked attribute
         """
-        self.server_widget.text = "Push Me!"
-        assert self.client_widget.text == self.server_widget.text
+        with self.app.process_events():
+            self.server_widget.checked = not self.server_widget.checked
 
+        self.assertEquals(self.client_widget.isChecked(), self.server_widget.checked)
+
+    def test_client_toggle(self):
+        """ Test toggling the state on the client side .
+        """
+
+        initial_state = self.server_widget.checked
+
+        with self.app.process_events():
+            self.client_widget.toggle()
+
+        self.assertEquals(initial_state, not self.server_widget.checked)
+
+    def test_server_clicked(self):
+        """ Test toggling the state on the server side .
+        """
+
+        initial_state = self.client_widget.isChecked()
+
+        with self.app.process_events():
+            self.server_widget.checked = not initial_state
+
+        self.assertEquals(initial_state, not self.client_widget.isChecked())
+
+
+if __name__ == '__main__':
+    import unittest
+    unittest.main()
