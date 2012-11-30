8f88933404e6e4573f75bd1f09305060f920c562
Chris Colbert <sccolbert@gmail.com>
2012-11-30 18:07:40 -0500

    Initial update for Include for the new stuff.

--- enaml/widgets/include.py
+++ enaml/widgets/include.py
@@ -5,7 +5,42 @@
 from traits.api import List, Instance, Bool
 
 from enaml.core.declarative import Declarative
-from enaml.core.object import Object
+from enaml.core.object import Object, ChildrenEventContext
+
+
+def _permutation(target, objects, obj_set, current):
+    """ Compute the permutation for an Include.
+
+    Parameters
+    ----------
+    target : Include
+        The include which exists in `current` which is used as the
+        key value for inserting `objects` into the permutation.
+
+    objects : list
+        The list of Objects which should be inserted after `target`.
+        There must be no duplicates in this list.
+
+    obj_set : set
+        The set of `objects`.
+
+    current : list
+        The current list of objects on which the permutation is
+        computed. There must be no duplicates in this list.
+
+    """
+    indices = {}
+    for idx, curr in enumerate(current):
+        indices[curr] = idx
+    permutation = []
+    for curr in current:
+        if curr is target:
+            permutation.append(indices[curr])
+            for obj in objects:
+                permutation.append(indices[obj])
+        elif curr not in obj_set:
+            permutation.append(indices[curr])
+    return permutation
 
 
 class Include(Declarative):
@@ -26,56 +61,73 @@ class Include(Declarative):
     #: are removed from the parent. The default is True.
     destroy_old = Bool(True)
 
-    #: By default, an Include is not snappable. Since an Include simply
-    #: manages the parenting of objects, there is no need for the client
-    #: UI to know of its existence. This can be temporarily set to True
-    #: when performing debug snapshots to get the full state of the tree.
-    #: XXX provide a snaphshot method!
-    snappable = False
+    def initialize(self):
+        """ A reimplemented initialization method.
 
-    def _init_changed(self):
-        """ A change handler for the `init` event of the Include.
-
-        This handler ensures that the current objects are parented and
-        properly initialized during the initialization pass.
+        This method ensures that the objects are added to the children
+        of the parent and initialized.
 
         """
         parent = self.parent
         if parent is not None:
-            objects = self.objects
-            parent.insert_children(self, objects)
-            # During an initialization pass, the parent is not yet fully
-            # initialized and will therefore not initialize any children
-            # which are being added. It is the repsonsibility of the
-            # Include to initially initialize the objects.
-            for child in objects:
-                child.initialize()
+            # Update the children from within a child event context
+            # to collapse multiple notifications into one.
+            with ChildrenEventContext(parent):
+                objects = self.objects
+                obj_set = set(objects)
+                if len(objects) != len(obj_set):
+                    raise ValueError('Cannot include duplicate objects')
+                # Use the `set_parent` api to ensure child validity. This
+                # loop dwarfs the cost of the other operations when the
+                # number of children is large.
+                for obj in objects:
+                    obj.set_parent(parent)
+                # Compute and apply the permutation for the children to
+                # place them in the correct position. The children are
+                # placed immediately after the include.
+                perm = _permutation(self, objects, obj_set, parent.children)
+                parent.permute_children(perm)
+                for obj in objects:
+                    obj.initialize()
 
-    def _parent_changed(self, parent):
-        """ A change handler for the `parent` of the Include.
+    # def _parent_changed(self, parent):
+    #     """ A change handler for the `parent` of the Include.
 
-        This handler will reparent the list of objects provided that
-        the Include is fully initialized.
+    #     This handler will reparent the list of objects provided that
+    #     the Include is fully initialized.
 
-        """
-        if self.initialized:
-            objects = self.objects
-            if parent is None:
-                for child in objects:
-                    child.set_parent(None)
-            else:
-                parent.insert_children(self, objects)
+    #     """
+    #     if self.initialized:
+    #         objects = self.objects
+    #         if parent is None:
+    #             for child in objects:
+    #                 child.set_parent(None)
+    #         else:
+    #             parent.insert_children(self, objects)
 
     def _objects_changed(self, old, new):
         """ A change handler for the `objects` list of the Include.
 
-        This handler will replace the old children with the new children
-        in a single atomic operation.
-
         """
         parent = self.parent
         if parent is not None:
-            parent.replace_children(old, self, new, self.destroy_old)
+            old_set = set(old)
+            new_set = set(new)
+            if len(new) != len(new_set):
+                raise ValueError('Cannot include duplicate objects')
+            add = new_set - old_set
+            remove = old_set - new_set
+            with ChildrenEventContext(parent):
+                if self.destroy_old:
+                    for obj in remove:
+                        obj.destroy()
+                else:
+                    for obj in remove:
+                        obj.set_parent(None)
+                for obj in add:
+                    obj.set_parent(parent)
+                perm = _permutation(self, new, new_set, parent.children)
+                parent.permute_children(perm)
 
     def _objects_items_changed(self, event):
         """ Handle the `objects` list changing in-place.
@@ -86,6 +138,23 @@ class Include(Declarative):
         """
         parent = self.parent
         if parent is not None:
-            old = event.removed
-            parent.replace_children(old, self, self.objects, self.destroy_old)
+            new = self.objects
+            new_set = set(new)
+            if len(new) != len(new_set):
+                raise ValueError('Cannot include duplicate objects')
+            removed_set = set(event.removed)
+            added_set = set(event.added)
+            add = added_set - removed_set
+            remove = removed_set - added_set
+            with ChildrenEventContext(parent):
+                if self.destroy_old:
+                    for obj in remove:
+                        obj.destroy()
+                else:
+                    for obj in remove:
+                        obj.set_parent(None)
+                for obj in add:
+                    obj.set_parent(parent)
+                perm = _permutation(self, new, new_set, parent.children)
+                parent.permute_children(perm)
 
