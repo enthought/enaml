736295918ac4b252d55887bcc0885546dbf64285
Chris Colbert <sccolbert@gmail.com>
2012-12-06 15:47:24 -0500

    Remove the noncomponents dir

--- enaml/noncomponents/image/__init__.py
+++
@@ -1,8 +0,0 @@
-#------------------------------------------------------------------------------
-#  Copyright (c) 2012, Enthought, Inc.
-#  All rights reserved.
-#------------------------------------------------------------------------------
-
-from image_from_file import ImageFromFile
-from image_from_pil import ImageFromPIL
-from image_from_array import ImageFromArray
