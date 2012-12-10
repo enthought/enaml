446001f8168ba3150cdacc39d6690d4d8b490ea1
Chris Colbert <sccolbert@gmail.com>
2012-12-10 12:09:47 -0500

    Add a backwards compatibility import

---
+++ enaml/widgets/include.py
@@ -0,0 +1,7 @@
+#------------------------------------------------------------------------------
+#  Copyright (c) 2012, Enthought, Inc.
+#  All rights reserved.
+#------------------------------------------------------------------------------
+# Backwards compatibility import
+from enaml.core.include import Include
+
