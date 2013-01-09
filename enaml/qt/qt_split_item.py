c2272cf1eff3e667c6ea1d255cc9c13c14745872
Chris Colbert <sccolbert@gmail.com>
2013-01-09 18:57:35 -0500

    Update the splitter implementation to support a stretch factor and a collapse flag.

--- enaml/qt/qt_split_item.py
+++ enaml/qt/qt_split_item.py
@@ -2,29 +2,30 @@
 #  Copyright (c) 2012, Enthought, Inc.
 #  All rights reserved.
 #------------------------------------------------------------------------------
-from .qt.QtGui import QFrame
+from .qt.QtGui import QFrame, QSplitter, QLayout
 from .q_single_widget_layout import QSingleWidgetLayout
 from .qt_container import QtContainer
 from .qt_widget import QtWidget
 
 
 class QSplitItem(QFrame):
-    """ A QFrame subclass which acts as an item QSplitter.
+    """ A QFrame subclass which acts as an item in a QSplitter.
 
     """
-    def __init__(self, *args, **kwargs):
+    def __init__(self, parent=None):
         """ Initialize a QSplitItem.
 
         Parameters
         ----------
-        *args, **kwargs
-            The position and keyword arguments required to initialize
-            a QWidget.
+        parent : QWidget, optional
+            The parent widget of the split item, or None if it has no
+            parent.
 
         """
-        super(QSplitItem, self).__init__(*args, **kwargs)
+        super(QSplitItem, self).__init__(parent)
         self._split_widget = None
         self.setLayout(QSingleWidgetLayout())
+        self.layout().setSizeConstraint(QLayout.SetMinAndMaxSize)
 
     def splitWidget(self):
         """ Get the split widget for this split item.
@@ -49,6 +50,62 @@ class QSplitItem(QFrame):
         self._split_widget = widget
         self.layout().setWidget(widget)
 
+    def stretch(self):
+        """ Get the stretch factor for this split item.
+
+        Returns
+        -------
+        result : int
+            The stretch factor for this split item.
+
+        """
+        # horizontal and vertical stretch are set to be the same,
+        # so which one is returned here is irrelevant.
+        return self.sizePolicy().horizontalStretch()
+
+    def setStretch(self, stretch):
+        """ Set the stretch factor for this split item.
+
+        Parameters
+        ----------
+        stretch : int
+            The stretch factor to use for this split item.
+
+        """
+        stretch = max(0, stretch)
+        policy = self.sizePolicy()
+        policy.setHorizontalStretch(stretch)
+        policy.setVerticalStretch(stretch)
+        self.setSizePolicy(policy)
+
+    def collapsible(self):
+        """ Get whether or not this widget is collapsible.
+
+        Returns
+        -------
+        result : bool
+            Whether or not this item can be collapsed to zero size.
+
+        """
+        parent = self.parentWidget()
+        if isinstance(parent, QSplitter):
+            return parent.isCollapsible(parent.indexOf(self))
+        return False
+
+    def setCollapsible(self, collapsible):
+        """ Set whether or not this widget is collapsible.
+
+        Parameters
+        ----------
+        collapsible : bool
+            Whether or not this item can be collapsed to zero size.
+            This holds regardless of the minimum size of the item.
+
+        """
+        parent = self.parentWidget()
+        if isinstance(parent, QSplitter):
+            return parent.setCollapsible(parent.indexOf(self), collapsible)
+
 
 class QtSplitItem(QtWidget):
     """ A Qt implementation of an Enaml SplitItem.
@@ -68,7 +125,8 @@ class QtSplitItem(QtWidget):
 
         """
         super(QtSplitItem, self).create(tree)
-        self.set_preferred_size(tree['preferred_size'])
+        self.set_stretch(tree['stretch'])
+        self.set_collapsible(tree['collapsible'])
 
     def init_layout(self):
         """ Initialize the layout for the underyling widget.
@@ -116,19 +174,30 @@ class QtSplitItem(QtWidget):
     #--------------------------------------------------------------------------
     # Message Handlers
     #--------------------------------------------------------------------------
-    def on_action_set_preferred_size(self, content):
-        """ Handle the 'set_preferred_size' action from the Enaml widget.
+    def on_action_set_stretch(self, content):
+        """ Handle the 'set_stretch' action from the Enaml widget.
 
         """
-        self.set_preferred_size(content['preferred_size'])
+        self.set_stretch(content['stretch'])
+
+    def on_action_set_collapsible(self, content):
+        """ Handle the 'set_collapsible' action from the Enaml widget.
+
+        """
+        self.set_collapsible(content['collapsible'])
 
     #--------------------------------------------------------------------------
     # Widget Update Methods
     #--------------------------------------------------------------------------
-    def set_preferred_size(self, size):
-        """ Set the preferred size for this item in the splitter.
+    def set_stretch(self, stretch):
+        """ Set the stretch factor for the underlying widget.
 
         """
-        # XXX implement me
-        pass
+        self.widget().setStretch(stretch)
+
+    def set_collapsible(self, collapsible):
+        """ Set the collapsible flag for the underlying widget.
+
+        """
+        self.widget().setCollapsible(collapsible)
 
