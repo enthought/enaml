9f4b937c4d96d1879842aa89ef32719b262912de
Chris Colbert <sccolbert@gmail.com>
2013-01-28 12:22:27 -0500

    Update the looper to keep a tuple of tuples as items, instead of a flat tuple

--- enaml/core/looper.py
+++ enaml/core/looper.py
@@ -32,6 +32,8 @@ class Looper(Templated):
 
     #: A read-only property which returns the tuple of items created
     #: by the looper when it passes over the objects in the iterable.
+    #: Each item in the tuple represents one iteration of the loop and
+    #: is a tuple of the items generated during that iteration.
     items = Property(fget=lambda self: self._items, depends_on='_items')
 
     #: Private storage for the `items` property.
@@ -59,9 +61,10 @@ class Looper(Templated):
         """
         super(Looper, self).post_destroy()
         if len(self._items) > 0:
-            for item in self._items:
-                if not item.is_destroyed:
-                    item.destroy()
+            for iteration in self._items:
+                for item in iteration:
+                    if not item.is_destroyed:
+                        item.destroy()
         self._items = ()
         self.iterable = None
 
@@ -95,6 +98,7 @@ class Looper(Templated):
                 # list of description dicts. There will only typically be
                 # one template, but more can exist if the looper was
                 # subclassed via enamldef to provided default children.
+                iteration = []
                 for identifiers, f_globals, descriptions in templates:
                     # Each iteration of the loop gets a new scope which
                     # is the union of the existing scope and the loop
@@ -109,18 +113,21 @@ class Looper(Templated):
                         name = descr['type']
                         cls = self._lookup_name(name, f_globals, descr)
                         item = cls._construct(None, descr, scope, f_globals)
-                        items.append(item)
+                        iteration.append(item)
+                items.append(tuple(iteration))
 
         old_items = self._items
         self._items = items = tuple(items)
         if len(old_items) > 0 or len(items) > 0:
             with self.parent.children_event_context():
                 if len(old_items) > 0:
-                    for old in old_items:
-                        if not old.is_destroyed:
-                            old.destroy()
+                    for iteration in old_items:
+                        for old in iteration:
+                            if not old.is_destroyed:
+                                old.destroy()
                 if len(items) > 0:
-                    self.parent.insert_children(self, items)
-                    for item in items:
+                    flat = sum(items, ())
+                    self.parent.insert_children(self, flat)
+                    for item in flat:
                         item.initialize()
 
