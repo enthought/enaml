feda8d2cb427e499574e4893a7dd0613aaba7dd7
S. Chris Colbert <sccolbert@gmail.com>
2013-02-19 00:33:42 -0500

    Update the DeclarativeProperty definition.
    Provide a class decorator which makes it easier to wrap members
    as declarative properties.

--- enaml/core/declarative.py
+++ enaml/core/declarative.py
@@ -4,41 +4,12 @@
 #------------------------------------------------------------------------------
 from abc import ABCMeta, abstractmethod, abstractproperty
 
-from atom.api import Member, ReadOnly, UserMember, Value, Str, null
+from atom.api import Member, ReadOnly, Value, null, USER_DEFAULT
 
-from .dynamic_scope import DynamicAttributeError
+#from .dynamic_scope import DynamicAttributeError
 from .exceptions import DeclarativeNameError, OperatorLookupError
 from .object import Object
 from .operator_context import OperatorContext
-
-
-def scope_lookup(name, scope, description):
-    """ A function which retrieves a name from a scope.
-
-    If the lookup fails, a DeclarativeNameError is raised. This can
-    be used to lookup names for a description dict from a global scope
-    with decent error reporting when the lookup fails.
-
-    Parameters
-    ----------
-    name : str
-        The name to retreive from the scope.
-
-    scope : mapping
-        A mapping object.
-
-    description : dict
-        The description dictionary associated with the lookup.
-
-    """
-    try:
-        item = scope[name]
-    except KeyError:
-        lineno = description['lineno']
-        filename = description['filename']
-        block = description['block']
-        raise DeclarativeNameError(name, filename, lineno, block)
-    return item
 
 
 class DeclarativeExpression(object):
@@ -70,53 +41,84 @@ class DeclarativeExpression(object):
         raise NotImplementedError
 
 
-class DeclarativeProperty(UserMember):
-    """ An atom `Member` which enables data binding in Enaml.
+class DeclarativeProperty(Member):
+    """ A custom atom member which enables data binding in Enaml.
 
     A declarative property is used to wrap another member on an Atom
-    subclass to enable that member to be bound by Enaml syntax.
+    subclass to enable that member to be bound by Enaml syntax. The
+    declarative property ensures that a bound expression is used to
+    provide the default value for the member. All other work is
+    delegated to the wrapped member.
 
     """
     __slots__ = 'member'
 
-    def __init__(self, member=Member()):
+    def __init__(self, member):
         """ Initialize a DeclarativeProperty.
 
         Parameters
         ----------
-        member : Member, optional
-            The atom member to use for validation and default value
-            handling. The default is a `Member` member.
+        member : Member
+            The atom member to wrap with this property.
 
         """
         assert isinstance(member, Member), "member must be an atom 'Member'"
-        super(DeclarativeProperty, self).__init__()
         self.member = member
+        super(DeclarativeProperty, self).set_default_kind(USER_DEFAULT, None)
 
-    def _set_member_name(self, name):
+    def set_member_name(self, name):
         """ Assign the name to this member.
 
-        This method is called by the Atom metaclass when a class is
-        created. This makes sure the name of the internal member is
-        also updated.
+        This method keeps the name of the internal member in sync.
 
         """
-        super(DeclarativeProperty, self)._set_member_name(name)
-        self.member._set_member_name(name)
+        super(DeclarativeProperty, self).set_member_name(name)
+        self.member.set_member_name(name)
 
-    def _set_member_index(self, index):
+    def set_member_index(self, index):
         """ Assign the index to this member.
 
-        This method is called by the Atom metaclass when a class is
-        created. This makes sure the index of the internal member is
-        also updated.
+        This method keeps the index of the internal member in sync.
 
         """
-        super(DeclarativeProperty, self)._set_member_index(index)
-        self.member._set_member_index(index)
+        super(DeclarativeProperty, self).set_member_index(index)
+        self.member.set_member_index(index)
+
+    def set_default_kind(self, kind, context):
+        """ Set the default kind for the member.
+
+        The default kind for a DeclarativeProperty cannot be changed,
+        but the kind is applied to the internal member so that it will
+        properly handle the default when no expression is bound for the
+        property.
+
+        """
+        self.member.set_default_kind(kind, context)
+
+    def set_validate_kind(self, kind, context):
+        """ Set the validate kind for the member.
+
+        The validate kind for a DeclarativeProperty cannot be changed,
+        but the kind is applied to the internal member so that it will
+        properly handle the validation for the member.
+
+        """
+        self.member.set_validate_kind(kind, context)
+
+    def set_post_validate_kind(self, kind, context):
+        """ Set the post validate kind for the member.
+
+        The post validate kind for a DeclarativeProperty cannot be
+        changed, but the kind is applied to the internal member so that
+        it will properly handle the post validation for the member.
+
+        """
+        self.member.set_post_validate_kind(kind, context)
 
     def clone(self):
         """ Create a clone of the declarative property.
+
+        This method also creates a clone of the internal member.
 
         """
         clone = super(DeclarativeProperty, self).clone()
@@ -138,17 +140,62 @@ class DeclarativeProperty(UserMember):
             value = self.member.do_default(owner)
         return value
 
-    def validate(self, owner, name, old, new):
-        """ Validate the declarative property value.
+    def do_validate(self, owner, old, new):
+        """ Run the validation for the member.
 
-        The validation is delegated to the internal member.
+        A declarative property delegates validation to its internal
+        member.
 
         """
         return self.member.do_validate(owner, old, new)
 
+    def __set__(self, owner, value):
+        """ Set the value for the member.
 
-#: Export DeclarativeProperty as something a bit easier to type
+        A declarative property delegates the work for setting the value
+        to the internal member.
+
+        """
+        self.member.__set__(owner, value)
+
+
+#: Export the DeclarativeProperty class as something easier to type.
 d = DeclarativeProperty
+
+
+def d_properties(*names):
+    """ A class decorator to exports members as declarative properties.
+
+    This decorator is a convenience for exporting Members defined on a
+    Declarative subclass to the Enaml declarative language. It is only
+    possible to bind expressions to members which are instances of
+    `DeclarativeProperty`. This decorator will automatically wrap the
+    specified members in such a property.
+
+    Parameters
+    ----------
+    *name
+        The names of the members on the class which should be exported
+        as declarative properties.
+
+    """
+    def wrapper(cls):
+        members = cls.__atom_members__
+        for name in names:
+            if name not in members:
+                msg = "Cannot export '%s'. It is not a Member on the %s class."
+                raise ValueError(msg % (name, cls.__name__))
+            member = members[name]
+            if isinstance(member, DeclarativeProperty):
+                continue  # only wrap once
+            d = DeclarativeProperty(member)
+            d.set_member_name(member.name)
+            d.set_member_index(member.index)
+            d.copy_static_observers(member)
+            members[name] = d
+            setattr(cls, name, d)
+        return cls
+    return wrapper
 
 
 class ExpressionNotifier(object):
@@ -188,6 +235,36 @@ class ExpressionNotifier(object):
             setattr(owner, name, owner.evaluate_expression(name))
 
 
+def scope_lookup(name, scope, description):
+    """ A function which retrieves a name from a scope.
+
+    If the lookup fails, a DeclarativeNameError is raised. This can
+    be used to lookup names for a description dict from a global scope
+    with decent error reporting when the lookup fails.
+
+    Parameters
+    ----------
+    name : str
+        The name to retreive from the scope.
+
+    scope : mapping
+        A mapping object.
+
+    description : dict
+        The description dictionary associated with the lookup.
+
+    """
+    try:
+        item = scope[name]
+    except KeyError:
+        lineno = description['lineno']
+        filename = description['filename']
+        block = description['block']
+        raise DeclarativeNameError(name, filename, lineno, block)
+    return item
+
+
+@d_properties('name')
 class Declarative(Object):
     """ The most base class of the Enaml declarative objects.
 
@@ -197,9 +274,6 @@ class Declarative(Object):
     visual representation; that functionality is added by subclasses.
 
     """
-    #: Redefine the 'name' attribute on Object as a 'd' property.
-    name = d(Str())
-
     #: The operator context used to build out this instance. This is
     #: assigned during object instantiation.
     operators = ReadOnly()
